cb606ab2265c3cb3289d1a1e605c30987d38fb5f
azumakuniyuki <azumakuniyuki@gmail.com>
2021-08-16 15:29:43 +0900

    Implement func Squeeze() at sisimai/string

--- sisimai/string/lib.go
+++ sisimai/string/lib.go
@@ -1,8 +1,22 @@
-// Copyright (C) 2020 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2020-2021 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
-
-/*
-libsisimai.org/sisimai/string provide utilities for dealing strings
-*/
 package string
+import "strings"
+
+// Squeeze() remove redundant characters
+func Squeeze(argv0 string, chars string) string {
+	// @param    [string] argvs  Email address, name, and other elements
+	// @return   [EmailAddress]    EmailAddress struct when the email address was not valid
+	if len(argv0) == 0 { return ""    }
+	if len(chars) == 0 { return argv0 }
+
+	for _, e := range strings.Split(chars, "") {
+		// Remove redundant characters from "argv1"
+		for strings.Count(argv0, e + e) > 1 {
+			// e + e => e
+			e = strings.ReplaceAll(argv0, e + e, e)
+		}
+	}
+	return argv0
+}
 
