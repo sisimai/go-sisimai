8d27a24b582f7d69ed9a98354e400745edc23d0b
azumakuniyuki <azumakuniyuki@gmail.com>
2024-07-14 20:56:46 +0900

    Implement FindIPv4Address() in sisimai/string

--- sisimai/string/lib.go
+++ sisimai/string/lib.go
@@ -75,12 +75,12 @@ func ContainsOnlyNumbers(argv1 string) bool {
 	return match
 }
 
-// IsIPv4Address() returns true when the given string is an IPv4 address
+// IsIPv4Address() returns "true" when the given string is an IPv4 address
 func IsIPv4Address(argv1 string) bool {
 	// @param    string argv1  IPv4 address like "192.0.2.25"
 	// @return   bool          true:  is an IPv4 address
 	//                         false: is not an IPv4 address
-	if len(argv1) < 8                 { return false }
+	if len(argv1) < 7                 { return false }
 	if strings.Count(argv1, ".") != 3 { return false }
 
 	match := true
@@ -92,6 +92,29 @@ func IsIPv4Address(argv1 string) bool {
 		if v > 255     { match = false; break }
 	}
 	return match
+}
+
+// FindIPv4Address() find IPv4 addresses from the given string
+func FindIPv4Address(argv1 string) []string {
+	// @param    string   argv1  String including an IPv4 address
+	// @return   []string        List of IPv4 addresses
+	// @since    v5.2.0
+	if len(argv1) < 7 { return []string{} }
+
+	// Rewrite: "mx.example.jp[192.0.2.1]" => "mx.example.jp 192.0.2.1"
+	strings.Replace(argv1, "(", " ", 1)
+	strings.Replace(argv1, ")", " ", 1)
+	strings.Replace(argv1, "[", " ", 1)
+	strings.Replace(argv1, "]", " ", 1)
+
+	ipv4a := []string{}
+	for _, e := range strings.Split(argv1, " ") {
+		// Find a string including an IPv4 address
+		if !strings.Contains(e, ".") { continue }	// IPv4 address must include "." character
+		if !IsIPv4Address(e)         { continue }	// The string is an IPv4 address or not
+		ipv4a = append(ipv4a, e)
+	}
+	return ipv4a
 }
 
 // Aligned() checks if each element of the 2nd argument is aligned in the 1st argument or not
