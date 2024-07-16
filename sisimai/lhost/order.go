eccf6d86c1e7115769382a548398150f09b89e28
azumakuniyuki <azumakuniyuki@gmail.com>
2024-07-16 15:58:37 +0900

    Remove codes using "regexp"

--- sisimai/lhost/order.go
+++ sisimai/lhost/order.go
@@ -1,9 +1,7 @@
-// Copyright (C) 2020 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2020,2024 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package lhost
-
 import "strings"
-import "regexp"
 
 // OrderBySubject() returns an MTA Order decided by the first word of the "Subject": header.
 func OrderBySubject(title string) []string {
@@ -12,46 +10,46 @@ func OrderBySubject(title string) []string {
 	if len(title) == 0 { return []string {} }
 
 	table := map[string][]string {
-		"abuse-report":          []string { "ARF" },
-		"auto":                  []string { "RFC3834" },
-		"auto-reply":            []string { "RFC3834" },
-		"automatic-reply":       []string { "RFC3834" },
-		"aws-notification":      []string { "AmazonSES" },
-		"complaint-about":       []string { "ARF" },
-		"delivery-failure":      []string { "Domino", "X2" },
-		"delivery-notification": []string { "MessagingServer" },
-		"delivery-report":       []string { "PowerMTA" },
-		"delivery-status":       []string {
+		"abuse-report":          []string{"ARF"},
+		"auto":                  []string{"RFC3834"},
+		"auto-reply":            []string{"RFC3834"},
+		"automatic-reply":       []string{"RFC3834"},
+		"aws-notification":      []string{"AmazonSES"},
+		"complaint-about":       []string{"ARF"},
+		"delivery-failure":      []string{"Domino", "X2"},
+		"delivery-notification": []string{"MessagingServer" },
+		"delivery-report":       []string{"PowerMTA" },
+		"delivery-status":       []string{
 			"GSuite", "Outlook", "GoogleGroups", "McAfee", "OpenSMTPD", "AmazonSES", "AmazonWorkMail",
 			"ReceivingSES", "Gmail", "X3",
 		},
-		"dmarc-ietf-dmarc": []string { "ARF" },
-		"email-feedback":   []string { "ARF" },
-		"failed-delivery":  []string { "X2" },
-		"failure-delivery": []string { "X2" },
-		"failure-notice":   []string { "Yahoo", "qmail", "mFILTER", "Activehunter", "X4" },
-		"loop-alert":       []string { "FML" },
-		"mail-could":       []string { "InterScanMSS" },
-		"mail-delivery":    []string { "Exim", "MailRu", "GMX", "EinsUndEins", "Zoho", "MessageLabs", "MXLogic" },
-		"mail-failure":     []string { "Exim" },
-		"mail-not":         []string { "X4" },
-		"mail-system":      []string { "EZweb" },
-		"message-delivery": []string { "MailFoundry" },
-		"message-frozen":   []string { "Exim" },
-		"message-you":      []string { "Barracuda" },
-		"non-recapitabile": []string { "Exchange2007" },
-		"non-remis": []string { "Exchange2007" },
-		"notice":    []string { "Courier" },
-		"permanent-delivery": []string { "X4" },
-		"postmaster-notify":  []string { "Sendmail" },
-		"returned-mail": []string { "Sendmail", "Aol", "V5sendmail", "Bigfoot", "Biglobe", "X1" },
-		"sorry-your":    []string { "Facebook" },
-		"there-was":     []string { "X6" },
-		"undeliverable": []string { "Office365", "Exchange2007", "Aol", "Exchange2003" },
-		"undeliverable-mail":    []string { "Amavis", "MailMarshalSMTP", "IMailServer" },
-		"undeliverable-message": []string { "Notes", "Verizon" },
-		"undelivered-mail":      []string { "Postfix", "Aol", "SendGrid", "Zoho" },
-		"warning":               []string { "Sendmail", "Exim" },
+		"dmarc-ietf-dmarc": []string{"ARF"},
+		"email-feedback":   []string{"ARF"},
+		"failed-delivery":  []string{"X2"},
+		"failure-delivery": []string{"X2"},
+		"failure-notice":   []string{"Yahoo", "qmail", "mFILTER", "Activehunter", "X4"},
+		"loop-alert":       []string{"FML"},
+		"mail-could":       []string{"InterScanMSS"},
+		"mail-delivery":    []string{"Exim", "MailRu", "GMX", "EinsUndEins", "Zoho", "MessageLabs", "MXLogic"},
+		"mail-failure":     []string{"Exim"},
+		"mail-not":         []string{"X4"},
+		"mail-system":      []string{"EZweb"},
+		"message-delivery": []string{"MailFoundry"},
+		"message-frozen":   []string{"Exim"},
+		"message-you":      []string{"Barracuda"},
+		"non-recapitabile": []string{"Exchange2007"},
+		"non-remis":        []string{"Exchange2007"},
+		"notice":           []string{"Courier"},
+		"permanent-delivery": []string{"X4"},
+		"postmaster-notify":  []string{"Sendmail"},
+		"returned-mail": []string{"Sendmail", "Aol", "V5sendmail", "Bigfoot", "Biglobe", "X1"},
+		"sorry-your":    []string{"Facebook"},
+		"there-was":     []string{"X6"},
+		"undeliverable": []string{"Office365", "Exchange2007", "Aol", "Exchange2003"},
+		"undeliverable-mail":    []string{"Amavis", "MailMarshalSMTP", "IMailServer"},
+		"undeliverable-message": []string{"Notes", "Verizon"},
+		"undelivered-mail":      []string{"Postfix", "Aol", "SendGrid", "Zoho"},
+		"warning":               []string{"Sendmail", "Exim"},
 	}
 
 	// The following order is decided by the first 2 words of Subject: header
@@ -59,8 +57,9 @@ func OrderBySubject(title string) []string {
 	title = strings.Replace(title, "]", " ", -1)
 	title = strings.Replace(title, "_", " ", -1)
 
-	regex := regexp.MustCompile(`\s+`)          // Regular expression for 1 or more space characters
-	title  = regex.ReplaceAllString(title, " ") // Squeeze duplicated space characters
+	// Squeeze duplicated space characters
+	for strings.Contains(title, "  ") { title = strings.ReplaceAll(title, "  ", " ") }
+
 	title  = strings.TrimSpace(title)           // Remove leading space characters
 	words := strings.SplitN(strings.ToLower(title), " ", 3)
 	first := ""
