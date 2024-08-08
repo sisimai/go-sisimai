85759834622ceb283ea19f3d752652ce93d72885
azumakuniyuki <azumakuniyuki@gmail.com>
2024-08-08 14:14:34 +0900

    ExpandVERP() and ExpandAlias() have been moved to expand.go in sisimai/address

--- sisimai/address/lib.go
+++ sisimai/address/lib.go
@@ -47,36 +47,3 @@ func Final(argv0 string) string {
 	return useris + "@" + hostis
 }
 
-// ExpandVERP() gets the original recipient address from VERP
-func ExpandVERP(email string) string {
-	// @param    [string] email  VERP Address
-	// @return   [string]        Email address
-	if len(email) == 0 { return "" }
-	local := strings.SplitN(email, "@", 2)[0]
-
-	// bounce+neko=example.org@example.org => neko@example.org
-	if strings.Index(local, "+") < 1 { return "" }
-	if strings.Index(local, "=") < 1 { return "" }
-	if strings.Index(local, "+") > len(local) - 1 { return "" }
-	if strings.Index(local, "=") > len(local) - 1 { return "" }
-
-	verp1 := strings.Replace(strings.SplitN(local, "+", 2)[1], "=", "@", 1)
-	if IsEmailAddress(verp1) { return verp1 }
-	return ""
-}
-
-// ExpandAlias() removes string from "+" to "@" at a local part
-func ExpandAlias(email string) string {
-	// @param    [string] email  Email alias string
-	// @return   [string]        Expanded email address
-	if len(email) == 0 { return "" }
-	if !IsEmailAddress(email) { return "" }
-	if !strings.Contains(email, "+") { return "" }
-	if strings.Index(email, "+") < 1 { return "" }
-
-	// neko+straycat@example.org => neko@example.org
-	lpart := email[0:strings.Index(email, "+")]
-	dpart := strings.SplitN(email, "@", 2)[1]
-	return lpart + "@" + dpart
-}
-
