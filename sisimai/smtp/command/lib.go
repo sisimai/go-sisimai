dd44d7002e796c0f424d727f6ec6b794d3345983
azumakuniyuki <azumakuniyuki@gmail.com>
2024-08-09 15:11:15 +0900

    Remove unused function list(), large scale code improvement in Find() at sisimai/smtp/command/lib.go

--- sisimai/smtp/command/lib.go
+++ sisimai/smtp/command/lib.go
@@ -27,21 +27,36 @@ func Test(argv0 string) bool {
 	return false
 }
 
-func list() []string { return []string{"HELO", "EHLO", "AUTH", "MAIL", "RCPT", "DATA", "QUIT"} }
 func Find(argv0 string) string {
 	// @param    string argv0  Text including SMTP command
 	// @return   string        Found SMTP command
-	if len(argv0) == 0 { return "" }
-	found := ""
-	words := strings.Split(argv0, " ")
+	if Test(argv0) == false { return "" }
 
-	seeks: for _, e := range list() {
-		// Find an SMTP command from the string
-		for _, w := range words {
-			// splitted by " "
-			if e == w { found = e; break seeks }
-		}
+	commandset := []string{}
+	commandmap := map[string]string{"STAR": "STARTTLS", "XFOR": "XFORWARD"}
+	detectable := []string{
+		"HELO", "EHLO", "STARTTLS", "AUTH PLAIN", "AUTH LOGIN", "AUTH CRAM-", "AUTH DIGEST-",
+		"MAIL F", "RCPT", "RCPT T", "DATA", "QUIT", "XFORWARD",
 	}
-	return found
+
+	for _, e := range detectable {
+		// Find an SMTP command from the given string
+		if strings.Contains(argv0, e) == false { continue }
+
+		smtpc := e[0:4] // The first 4 characters of SMTP command found in the argument
+		found := false  // There is the same SMTP command in "commandset" or not
+		for _, c := range commandset {
+			// Check that the command found in the argument is already included in "commandset"
+			if strings.HasPrefix(c, smtpc) == false { continue }
+			found = true
+			break
+		}
+		if found { continue } // There is the same SMTP command in "commandset"
+
+		if smtpc == "STAR" || smtpc == "XFOR" { smtpc = commandmap[smtpc] }
+		commandset = append(commandset, smtpc)
+	}
+	if len(commandset) == 0 { return "" }
+	return commandset[len(commandset)-1]
 }
 
