f88d3ed044d839c04aa5ab7a0b66ce40b7af8514
azumakuniyuki <azumakuniyuki@gmail.com>
2024-08-26 12:25:30 +0900

    Tiny comment improvement

--- sisimai/sis/delivery-matter.go
+++ sisimai/sis/delivery-matter.go
@@ -1,7 +1,6 @@
 // Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package sis
-
 //  ____       _ _                      __  __       _   _            
 // |  _ \  ___| (_)_   _____ _ __ _   _|  \/  | __ _| |_| |_ ___ _ __ 
 // | | | |/ _ \ | \ \ / / _ \ '__| | | | |\/| |/ _` | __| __/ _ \ '__|
