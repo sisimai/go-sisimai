a59c09ecdc98d00efcdb49987f8a5a1aa96aacd1
azumakuniyuki <azumakuniyuki@gmail.com>
2024-08-27 23:22:50 +0900

    Rewrite Find() function in sisimai/address

--- sisimai/address/find.go
+++ sisimai/address/find.go
@@ -9,248 +9,250 @@ package address
 import "strings"
 import sisimoji "sisimai/string"
 
-// Find() is an email address parser
-func Find(argv0 string) []string {
-    // @param    string   argv0  String including email address
-    // @return   []string        Email address list
-	if len(argv0) == 0 { return []string{} }
-
-	table := seek(argv0); if len(table) == 0 { return []string{} }
-	addrs := []string{}
-	for _, e := range table {
-		// Pick an email address only from the results of seek()
-		addrs = append(addrs, e[0])
-	}
-	return addrs
-}
-
-// seek() is an email address parser with a name and a comment
-func seek(argv0 string) [][3]string {
-	// @param    string argv0   String including email address
-    // @return   [][3]string    Email address list with a name and a comment
-	if len(argv0) == 0 { return [][3]string{} }
+// Find() is an email address parser with a name and a comment
+func Find(argv1 string) [3]string {
+	// @param    string argv1  String including email address
+	// @return   [][3]string{}         Email address list 
+	if len(argv1) < 5 { return [3]string{} }
+		argv1 = strings.ReplaceAll(argv1, "\r", "") // Remove CR
+		argv1 = strings.ReplaceAll(argv1, "\n", "") // Remove NL
+	if len(argv1) < 5 { return [3]string{} }
 
 	delimiters := `<>(),"`
-	indicators := map[string]uint8 {
+	indicators := map[string]uint8{
 		"email-address": (1 << 0), // <neko@example.org>
 		"quoted-string": (1 << 1), // "Neko, Nyaan"
 		"comment-block": (1 << 2), // (nekochan)
 	}
 
-	emailtable :=   [3]string{ "", "", "" } // Address, Name, Comment
-	addrtables := [][3]string{}
-	readbuffer := [][3]string{emailtable}
+	readcursor := uint8(0)              // Points the current cursor position
+	readbuffer := [3]string{"", "", ""} // Read buffer for emailtable
+	emailtable := [3]string{"", "", ""} // [0]Address, [1]Name, [2]Comment
+	p          := uint8(0)              // Current position: 0=undefined, 1=address, 2=name, 3=comment
 
-	var readcursor uint8 = 0    // Points the current cursor position
-	var p int8 = -1             // Current position: 0 = address, 1 = name, or 2 = comment
-	var v *[3]string = &readbuffer[0]
-
-	argv0 = strings.ReplaceAll(argv0, "\r", "") // Remove CR
-	argv0 = strings.ReplaceAll(argv0, "\n", "") // Remove NL
-
-	for _, e := range(strings.Split(argv0, "")) {
+	for _, e := range strings.Split(argv1,"") {
 		// Check each character
-		if strings.Index(e, delimiters) > -1 {
+		if strings.Contains(delimiters, e) {
 			// The character is a delimiter
 			if e == "," {
 				// The "," is a email address separator or a character in a "name"
-				if strings.HasPrefix((*v)[0], "<") &&
-				   strings.Contains( (*v)[0], "@") &&
-				   strings.HasSuffix((*v)[0], ">") {
-					// An email address has already been picked
+				if IsIncluded(readbuffer[0]) {
+					// The email address has already been picked
 					if readcursor & indicators["comment-block"] > 0 {
 						// The cursor is in the comment block (Neko, Nyaan)
-						(*v)[2] += e
+						readbuffer[2] += e
 
 					} else if readcursor & indicators["quoted-string"] > 0 {
 						// "Neko, Nyaan"
-						(*v)[1] += e
+						readbuffer[1] += e
 
 					} else {
 						// The cursor is not in neither the quoted-string nor the comment block
-						readcursor = 0 // Reset the cursor position
-						readbuffer = append(readbuffer, emailtable)
-						v = &readbuffer[len(readbuffer) - 1]
+						readcursor = 0 // Reset the current position
+						p = 0
+						// TODO: break ?
 					}
 				} else {
+					// "," is in the display name or the quoted local part of the email address
 					// "Neko, Nyaan" <neko@nyaan.example.org> OR <"neko,nyaan"@example.org>
-					if p > -1 { (*v)[p] += e } else { (*v)[1] += e }
-				}
-				continue
-			} // END OF if ","
-
-			if e == "<" {
-				// "<": The beginning of an email address or not
-				if len((*v)[0]) > 0 {
-					if p > -1 { (*v)[p] += e } else { (*v)[1] += e }
-
-				} else {
-					// <neko@nyaan.example.org>
-					readcursor |= indicators["email-address"]
-					(*v)[0] += e
-					p = 0
-				}
-				continue
-			} // END OF if "<"
-
-			if e == ">" {
-				// ">": The end of an email address or not
-				if readcursor & indicators["email-address"] > 0 {
-					// <neko@example.org>
-					readcursor &= ^indicators["email-address"]
-					(*v)[0] += e
-					p = -1
-
-				} else {
-					// a comment block or a display name
-					if p > -1 { (*v)[2] += e } else { (*v)[1] += e }
-				}
-				continue
-			} // END OF if ">"
-
-			if e == "(" {
-				// The beginning of a comment block or not
-				if readcursor & indicators["email-address"] > 0 {
-					// <"neko(nyaan)"@example.org> or <neko(nyaan)@example.org>
-					if strings.Contains((*v)[0], `"`) {
-						// Quoted local part: <"neko(nyaan)"@example.org>
-						(*v)[0] += e
+					if p == 0 || p == 2 {
+						// Deal as a character of the display name
+						readbuffer[1]   += e
 
 					} else {
-						// Comment: <neko(nyaan)@example.org>
+						// Append "e" to "address" readbuffer[0] or "comment" readbuffer[2]
+						readbuffer[p-1] += e
+					}
+				}
+				continue
+			}   // End of if(",")
+
+			if e == "<" {
+				// "<": The beginning of an email address or a character in the display name or the comment
+				if len(readbuffer[0]) == 0 {
+					// The 1st character of the email address: <neko@nyaan.example.jp>
+					readcursor |= indicators["email-address"]
+					readbuffer[0] = e; p = 1
+
+				} else {
+					// Check that readbuffer[0] already has a valid email address or not
+					if IsIncluded(readbuffer[0]) {
+						// The value of readbuffer[0] is a valid email address
+						// "e" is a part of the display name or the comment
+						if IsComment(readbuffer[2]) { readbuffer[2] += e } else { readbuffer[1] += e }
+					}
+				}
+				continue
+			}   // End of if("<")
+
+			if e == ">" {
+				// ">": The end of an email address or a character in the display name or the comment
+				if readcursor & indicators["email-address"] > 0 {
+					// The email address in readbuffer[0] has been successfully constructed
+					readcursor &= ^indicators["email-address"]
+					readbuffer[0] += e; p = 0
+
+				} else {
+					// ">" is a part of the comment block or the display name
+					if IsComment(readbuffer[2]) { readbuffer[2] += e } else { readbuffer[1] += e }
+				}
+				continue
+			}   // End of if(">")
+
+			if e == "(" {
+				// "(": The beginning of a comment block or a character in the display name or the comment
+				if readcursor & indicators["email-address"] > 0 {
+					// An email address including a comment like the followings:
+					// <"neko(nyaan)"@example.org> or <neko(nyaan)@example.org>
+					if strings.Contains(readbuffer[0], `"`) {
+						// Quoted local part in the email address like <"neko(nyaan)"@example.org>
+						readbuffer[0] += e
+
+					} else {
+						// A comment in the email address like <neko(nyaan)@example.org>
 						readcursor |= indicators["comment-block"]
-						if strings.HasSuffix((*v)[2], ")") { (*v)[2] += " " }
-						(*v)[2] += e
-						p = 2
+						if strings.HasSuffix(readbuffer[2], ")") { readbuffer[2] += " " }
+						readbuffer[2] += e; p = 2
 					}
 				} else if readcursor & indicators["comment-block"] > 0 {
 					// Comment at the outside of an email address (...(...)
-					if strings.HasSuffix((*v)[2], ")") { (*v)[2] += " " }
-					(*v)[2] += e
+					if strings.HasSuffix(readbuffer[2], ")") { readbuffer[2] += " " }
+					readbuffer[2] += e
 
 				} else if readcursor & indicators["quoted-string"] > 0 {
 					// "Neko, Nyaan(cat)", Deal as a display name
-					(*v)[1] += e
+					readbuffer[1] += e
 
 				} else {
-					// The beginning of a comment block
+					// The beginning of the comment block
 					readcursor |= indicators["comment-block"]
-					if strings.HasSuffix((*v)[2], ")") { (*v)[2] += " " }
-					(*v)[2] += e
-					p = 2
+					if strings.HasSuffix(readbuffer[2], ")") { readbuffer[2] += " " }
+					readbuffer[2] += e; p = 2
 				}
 				continue
-			} // END OF if "("
+			}   // End of if("(")
 
 			if e == ")" {
-				// The end of a comment block or not
+				// "(": The end of a comment block or a character in the display name or the comment
 				if readcursor & indicators["email-address"] > 0 {
-					// <"neko(nyaan)"@example.org> OR <neko(nyaan)@example.org>
-					if strings.Contains((*v)[0], `"`) {
-						// Quoted string in the local part: <"neko(nyaan)"@example.org>
-						(*v)[0] += e
+					// An email address including a comment like the followings:
+					// <"neko(nyaan)"@example.org> or <neko(nyaan)@example.org>
+					if strings.Contains(readbuffer[0], `"`) {
+						// Quoted local part in the email address like <"neko(nyaan)"@example.org>
+						readbuffer[0] += e
 
 					} else {
-						// Comment: <neko(nyaan)@example.org>
+						// A comment in the email address like <neko(nyaan)@example.org>
 						readcursor &= ^indicators["comment-block"]
-						(*v)[2] += e
-						p = 0
+						readbuffer[2] += e; p = 1
 					}
 				} else if readcursor & indicators["comment-block"] > 0 {
-					// Comment at the outside of an email address (...(...) 
+					// Comment at the outside of an email address (...(...)
 					readcursor &= ^indicators["comment-block"]
-					(*v)[2] += e
-					p = -1
+					readbuffer[2] += e; p = 0
 
 				} else {
 					// Deal as a display name
-					readcursor &= ^indicators["comment-block"]
-					(*v)[1] += e
-					p = -1
+					readbuffer[1] += e; p = 0
 				}
 				continue
-			} // END OF if ")"
+			}   // End of if(")")
 
 			if e == `"` {
 				// The beginning or the end of a quoted-string
-				if p > -1 {
-					// email-address or comment-block
-					(*v)[p] += e
+				if p > 0 {
+					// A part of the email address or the comment block
+					readbuffer[p - 1] += e
 
 				} else {
-					// Display name like "Neko, Nyaan"
-					(*v)[1] += e
+					// The display name lke "Neko, Nyaan"
+					readbuffer[1] += e
 					if readcursor & indicators["quoted-string"] == 0 { continue }
-					if strings.HasSuffix((*v)[1], `\"`)              { continue } // "Neko, Nyaan \"...
-					readcursor &= ^indicators["quoted-string"]
-					p = -1
+					if strings.HasSuffix(readbuffer[1], `\"`)        { continue } // "Neko, Nyaan \"...
+					readcursor &= ^indicators["quoted-string"];p = 0
 				}
 				continue
-			} // END of if `"`
+			}   // End of if(`"`)
 		} else {
 			// The character is not a delimiter
-			if p > -1 { (*v)[p] += e } else { (*v)[1] += e }
+			if p == 0 || p == 2 {
+				// Deal as a character of the display name
+				readbuffer[1]   += e
+
+			} else {
+				// Append "e" to "address" readbuffer[0] or "comment" readbuffer[2]
+				readbuffer[p-1] += e
+			}
 			continue
+		}
+	} // End of the loop(for)
+
+	if len(readbuffer[0]) == 0 {
+		// There is no email address
+		if IsIncluded(readbuffer[1]) {
+			// Try to use the string like an email address in the display name
+			for _, e := range strings.Split(readbuffer[1], " ") {
+				// Find an email address
+				if IsEmailAddress(e) == false { continue }
+				readbuffer[0] = e; break
+			}
+		} else if IsMailerDaemon(readbuffer[1]) {
+			// Allow if the string is MAILER-DAEMON
+			readbuffer[0] = readbuffer[1]
 		}
 	}
 
-	for _, e := range(readbuffer) {
-		// Check the value of each email address
-		if len(e[0]) == 0 {
-			// The email address part is empty
-			if IsEmailAddress(e[1]) {
-				// Try to use the value of name as an email address
-				token   := strings.Split(e[1], "@")
-				token[0] = strings.Trim(token[0], " ")
-				token[1] = strings.Trim(token[1], " ")
-				e[0] = token[0] + "@" + token[1]
+	for sisimoji.Aligned(readbuffer[0], []string{"(", ")"}) {
+		// Remove the comment block from the email address
+		// - (nyaan)nekochan@example.org
+		// - nekochan(nyaan)cat@example.org
+		// - nekochan(nyaan)@example.org
+		p1 := strings.Index(readbuffer[0], "(")
+		p2 := strings.Index(readbuffer[0], ")")
+		ce := readbuffer[0][p1:p2 + 1]
 
-			} else if IsMailerDaemon(e[1]) {
-				// Allow Mailer-Daemon
-				e[0] = e[1]
+		readbuffer[0] = strings.Replace(readbuffer[0], ce, "", 1)
+		if len(readbuffer[2]) == 0 { readbuffer[2] = ce } else { readbuffer[2] += " " + ce }
+	}
 
-			} else {
-				// There is no valid email address stirng in the argument
-				continue
-			}
-		}
+	for {
+		// The email address must not include any character except from 0x20 to 0x7e.
+		if IsIncluded(readbuffer[0]) == false && IsMailerDaemon(readbuffer[0]) == false { break }
 
-		// Remove the comment from the email address
-		if strings.Count(e[0], "(") == 1 && strings.Count(e[0], ")") == 1 {
-			// (nyaan)nekochan@example.org, nekochan(nyaan)cat@example.org or nekochan(nyaan)@example.org
-			p1 := strings.Index(e[0], "(")
-			p2 := strings.Index(e[0], ")")
-			p3 := strings.Index(e[0], "@")
-
-			e[0] = e[0][0:p1] + e[0][p2 + 1:p3] + "@" + e[0][p3 + 1:len(e[0])]
-			e[2] = e[0][p1 + 1:p2]
-
-		} else {
-			// TODO: neko(nyaan)kijitora(nyaan)cat@example.org
-			continue
-		}
-
-		// The email address should be a valid email address
-		if IsMailerDaemon(e[0]) && IsEmailAddress(e[0]) == false { continue }
-
+		// Remove angle brackets, other brackets, and quotations: []<>{}'` except a domain part is
+		// an IP address like neko@[192.0.2.222]
 		// Remove angle brackets, other brackets, and quotations: ()[]<>{}'`;. and `"`
-		e[0] = strings.Trim(e[0], "<>{}()[]`';.")
-		if IsQuotedAddress(e[0]) == false { e[0] = strings.Trim(e[0], `"`) }
+		readbuffer[0] = strings.Trim(readbuffer[0], "<>{}()[]`';.")
+		if IsQuotedAddress(readbuffer[0]) == false { readbuffer[0] = strings.Trim(readbuffer[0], `"`) }
 
-		// Remove trailing spaces at the value of 1.name and 2.comment
-		e[1] = strings.TrimSpace(e[1])
-		e[2] = strings.TrimSpace(e[2])
+		emailtable[0] = readbuffer[0]
+		break
+	}
 
-		// Remove the value of 2.comment when the value do not include "(" and ")"
-		if !strings.HasPrefix(e[2], "(") && !strings.HasSuffix(e[2], ")") { e[2] = "" }
+	for len(emailtable[0]) > 0 {
+		// Remove trailing spaces at the display name and the comment block
+		if len(readbuffer[1]) == 0 { break }
+		readbuffer[1] = strings.TrimSpace(readbuffer[1])
 
-		// Remove redundant spaces when tha value of 1.name do not include `"`
-		if !strings.HasPrefix(e[1], `"`) && !strings.HasSuffix(e[1], `"`) { e[1] = sisimoji.Squeeze(e[1], " ") }
-		if IsQuotedAddress(e[1]) == false                                 { e[1] = strings.Trim(e[1], `"`) }
+		if strings.HasPrefix(readbuffer[1], `"`) == false &&
+		   strings.HasSuffix(readbuffer[1], `"`) == false {
+			// Remove redundant spaces from the display name when the value is not a "quoted-string"
+		   readbuffer[1] = sisimoji.Squeeze(readbuffer[1], " ")
+		}
+		if IsQuotedAddress(readbuffer[1]) == false { readbuffer[1] = strings.Trim(readbuffer[1], `"`) }	
+		emailtable[1] = readbuffer[1]
+		break
+	}
 
-		addrtables = append(addrtables, e)
-	} // END OF for(readbuffer)
+	for len(emailtable[0]) > 0 {
+		// Check and tidy up the comment block
+		if len(readbuffer[2])       == 0     { break }
+		if IsComment(readbuffer[2]) == false { break }
 
-	return addrtables
+		emailtable[2] = strings.TrimSpace(readbuffer[2])
+		break
+	}
+
+	return emailtable
 }
 
