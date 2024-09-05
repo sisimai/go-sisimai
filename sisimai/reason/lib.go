bbfb600084b7d6f3aed9d7cdc32f531b9e761ed9
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-05 17:39:24 +0900

    Update sisimai/reason/Index() (Add a comment to each reason)

--- sisimai/reason/lib.go
+++ sisimai/reason/lib.go
@@ -1,6 +1,11 @@
 // Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package reason
+//                                 
+//  _ __ ___  __ _ ___  ___  _ __  
+// | '__/ _ \/ _` / __|/ _ \| '_ \ 
+// | | |  __/ (_| \__ \ (_) | | | |
+// |_|  \___|\__,_|___/\___/|_| |_|
 
 // Retry() returns the table of reason list which should be checked again
 func Retry() map[string]bool {
@@ -13,11 +18,40 @@ func Retry() map[string]bool {
 // Index() returns the list of all the reasons sisimai supoort
 func Index() []string {
 	return []string{
-        "AuthFailure", "BadReputation", "Blocked", "ContentError", "ExceedLimit", "Expired", "Filtered",
-		"HasMoved", "HostUnknown", "MailboxFull", "MailerError", "MesgTooBig", "NetworkError", "NotAccept",
-		"NotCompliantRFC", "OnHold", "Rejected", "NoRelaying", "SpamDetected", "VirusDetected", "PolicyViolation",
-		"SecurityError", "Speeding", "Suspend", "RequirePTR", "SystemError", "SystemFull", "TooManyConn",
-		"UserUnknown", "SyntaxError",
+		"AuthFailure",      // Email rejected due to SPF, DKIM, DMARC failure
+		"BadReputation",    // Email rejected due to an IP address reputation
+		"Blocked",          // Email rejected due to client IP address or a hostname
+		"ContentError",     // Email rejected due to a header format of the email
+		// "Delivered",     // Email delivered successfully
+		"ExceedLimit",      // Email rejected due to an email exceeded the limit
+		"Expired",          // Delivery time has expired due to a connection failure
+		"Feedback",         // Email forwarded to the sender as a complaint message from your mailbox provider
+		"Filtered",         // Email rejected due to a header content after SMTP DATA command
+		"HasMoved",         // Email rejected due to users mailbox has moved and is not forwarded automatically
+		"HostUnknown",      // Delivery failed due to a domain part of a recipients email address does not exist
+		"MailboxFull",      // Email rejected due to a recipients mailbox is full
+		"MailerError",      // Email returned due to a mailer program has not exited successfully
+		"MesgTooBig",       // Email rejected due to an email size is too big for a destination mail server
+		"NetworkError",     // SMTP connection failed due to DNS look up failure or other network problems
+		"NoRelaying",       // Email rejected with error message "Relaying Denied
+		"NotAccept",        // Delivery failed due to a destination mail server does not accept any email
+		"NotCompliantRFC",  // Email rejected due to non-compliance with RFC
+		"OnHold",           // Sisimai could not decided the reason due to there is no (or less) detailed information for judging the reason
+		"PolicyViolation",  // Email rejected due to policy violation on a destination host
+		"Rejected",         // Email rejected due to a senders email address (envelope from)
+		"RequirePTR",       // Email rejected due to missing PTR record or having invalid PTR record
+		"SecurityError",    // Email rejected due to security violation was detected on a destination host
+		"SpamDetected",     // Email rejected by spam filter running on the remote host
+		"Speeding",         // Rejected due to exceeding a rate limit or sending too fast
+		"Suspend",          // Email rejected due to a recipient account is being suspended
+		"SyntaxError",      // Email rejected due to syntax error at sent commands in SMTP session
+		"SystemError",      // Email returned due to system error on the remote host
+		"SystemFull",       // Email rejected due to a destination mail servers disk is full
+		"TooManyConn",      // SMTP connection rejected temporarily due to too many concurrency connections to the remote host
+		// "Undefined",     // Sisimai could not detect an error reason
+		"UserUnknown",      // Email rejected due to a local part of a recipients email address does not exist
+		//"Vacation",       // Email replied automatically due to a recipient is out of office
+		"VirusDetected",    // Email rejected due to a virus scanner on a destination host
     }
 }
 
