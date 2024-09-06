a8fc5d6f5c328dea6ffb6280910f278b40ad02b6
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-06 12:35:58 +0900

    Fix bug in FindIPv4Address

--- sisimai/string/ipv4.go
+++ sisimai/string/ipv4.go
@@ -1,6 +1,13 @@
 // Copyright (C) 2020-2021,2024 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package string
+
+//      _        _                ___            _  _   
+//  ___| |_ _ __(_)_ __   __ _   / (_)_ ____   _| || |  
+// / __| __| '__| | '_ \ / _` | / /| | '_ \ \ / / || |_ 
+// \__ \ |_| |  | | | | | (_| |/ / | | |_) \ V /|__   _|
+// |___/\__|_|  |_|_| |_|\__, /_/  |_| .__/ \_/    |_|  
+//                       |___/       |_|                
 import "strings"
 import "strconv"
 
@@ -31,17 +38,18 @@ func FindIPv4Address(argv1 string) []string {
 	if len(argv1) < 7 { return []string{} }
 
 	// Rewrite: "mx.example.jp[192.0.2.1]" => "mx.example.jp 192.0.2.1"
-	strings.Replace(argv1, "(", " ", 1)
-	strings.Replace(argv1, ")", " ", 1)
-	strings.Replace(argv1, "[", " ", 1)
-	strings.Replace(argv1, "]", " ", 1)
-
+	argv1  = strings.Replace(argv1, "(", " ", 1)
+	argv1  = strings.Replace(argv1, ")", " ", 1)
+	argv1  = strings.Replace(argv1, "[", " ", 1)
+	argv1  = strings.Replace(argv1, "]", " ", 1)
 	ipv4a := []string{}
+
 	for _, e := range strings.Split(argv1, " ") {
 		// Find a string including an IPv4 address
-		if !strings.Contains(e, ".") { continue }	// IPv4 address must include "." character
-		if !IsIPv4Address(e)         { continue }	// The string is an IPv4 address or not
+		if !strings.Contains(e, ".") { continue }   // IPv4 address must include "." character
+		if !IsIPv4Address(e)         { continue }   // The string is an IPv4 address or not
 		ipv4a = append(ipv4a, e)
 	}
 	return ipv4a
 }
+
