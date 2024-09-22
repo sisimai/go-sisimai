f39073d1d1c982b1f0047170b2502abc044088ba
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-22 21:53:19 +0900

    Implement sisimai/lhost/fml

--- sisimai/lhost/fml.go
+++ sisimai/lhost/fml.go
@@ -2,23 +2,110 @@
 // This software is distributed under The BSD 2-Clause License.
 package lhost
 
+//       _               _      _______ __  __ _     
+//  _ __| |__   ___  ___| |_   / /  ___|  \/  | |    
+// | '__| '_ \ / _ \/ __| __| / /| |_  | |\/| | |    
+// | |  | | | | (_) \__ \ |_ / / |  _| | |  | | |___ 
+// |_|  |_| |_|\___/|___/\__/_/  |_|   |_|  |_|_____|
+import "strings"
 import "sisimai/sis"
+import "sisimai/rfc5322"
+import sisimoji "sisimai/string"
 
 func init() {
-	// Decode bounce messages from
+	// Decode bounce messages from fml mailing list server/manager: https://www.fml.org
 	InquireFor["FML"] = func(bf *sis.BeforeFact) sis.RisingUnderway {
 		// @param    *sis.BeforeFact bf  Message body of a bounce email
 		// @return   RisingUnderway      RisingUnderway structure
 		if len(bf.Head)            == 0 { return sis.RisingUnderway{} }
 		if len(bf.Body)            == 0 { return sis.RisingUnderway{} }
 
-        return sis.RisingUnderway{}
+		if len(bf.Head["x-mlserver"])                     == 0 { return sis.RisingUnderway{} }
+		if strings.Index(bf.Head["from"][0], "-admin@")    < 1 { return sis.RisingUnderway{} }
+		if strings.Index(bf.Head["message-id"][0], ".FML") < 2 { return sis.RisingUnderway{} }
+
+		boundaries := []string{"Original mail as follows:"}
+		errortitle := map[string][]string{
+			"rejected": []string{
+				" are not member",
+				"NOT MEMBER article from ",
+				"reject mail ",
+				"Spam mail from a spammer is rejected",
+			},
+			"systemerror": []string{
+				"fml system error message",
+				"Loop Alert: ",
+				"Loop Back Warning: ",
+				"WARNING: UNIX FROM Loop",
+			},
+			"securityerror": []string{"Security Alert"},
+		}
+		errortable := map[string][]string{
+			"rejected": []string{
+				" header may cause mail loop",
+				"NOT MEMBER article from ",
+				"reject mail from ",
+				"reject spammers:",
+				"You are not a member of this mailing list",
+			},
+			"systemerror": []string{
+				" has detected a loop condition so that",
+				"Duplicated Message-ID",
+				"Loop Back Warning:",
+			},
+			"securityerror": []string{"Security alert:"},
+		}
+		dscontents := []sis.DeliveryMatter{{}}
+		emailparts := rfc5322.Part(&bf.Body, boundaries, false)
+		recipients := uint8(0)            // The number of 'Final-Recipient' header
+		v          := &(dscontents[len(dscontents) - 1])
+
+		for _, e := range(strings.Split(emailparts[0], "\n")) {
+			// Read error messages and delivery status lines from the head of the email to the
+			// previous line of the beginning of the original message.
+			if len(e) == 0 { continue }
+
+			p1 := strings.Index(e, "<")
+			p2 := strings.Index(e, ">")
+
+			if p1 > 0 && p2 > 0 {
+				// You are not a member of this mailing list <neko-nyaan@example.org>.
+				if len(v.Recipient) > 0 {
+					// There are multiple recipient addresses in the message body.
+					dscontents = append(dscontents, sis.DeliveryMatter{})
+					v = &(dscontents[len(dscontents) - 1])
+				}
+				v.Recipient = e[p1:p2]
+				v.Diagnosis = e
+				recipients += 1
+
+			} else {
+				// If you know the general guide of this list, please send mail with the mail body
+				v.Diagnosis += e
+			}
+		}
+		if recipients == 0 { return sis.RisingUnderway{} }
+
+		for j, _ := range dscontents {
+			// Set default values stored in "permessage" if each value in "dscontents" is empty.
+			e := &(dscontents[j])
+			e.Diagnosis = sisimoji.Sweep(e.Diagnosis)
+
+			for f := range errortable {
+				// The key is a bounce reason name
+				if sisimoji.ContainsAny(e.Diagnosis, errortable[f]) == false { continue }
+				e.Reason = f; break
+			}
+			if e.Reason != "" { continue }
+
+			for f := range errortitle {
+				// The key is a bounce reason name
+				if sisimoji.ContainsAny(bf.Head["subject"][0], errortitle[f]) == false { continue }
+				e.Reason = f; break
+			}
+		}
+
+		return sis.RisingUnderway{ Digest: dscontents, RFC822: emailparts[1] }
     }
 }
 
-//       _               _      _______ __  __ _     
-//  _ __| |__   ___  ___| |_   / /  ___|  \/  | |    
-// | '__| '_ \ / _ \/ __| __| / /| |_  | |\/| | |    
-// | |  | | | | (_) \__ \ |_ / / |  _| | |  | | |___ 
-// |_|  |_| |_|\___/|___/\__/_/  |_|   |_|  |_|_____|
-//                                                   
