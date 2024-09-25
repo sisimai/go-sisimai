b82abee477cac210fe4c1683992a82eaecd13fb1
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-26 03:16:56 +0900

    Tiny code and comment improvement

--- sisimai/rfc5322/part.go
+++ sisimai/rfc5322/part.go
@@ -1,6 +1,12 @@
 // Copyright (C) 2020,2024 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package rfc5322
+
+//  ____  _____ ____ ____ _________  ____  
+// |  _ \|  ___/ ___| ___|___ /___ \|___ \ 
+// | |_) | |_ | |   |___ \ |_ \ __) | __) |
+// |  _ <|  _|| |___ ___) |__) / __/ / __/ 
+// |_| \_\_|   \____|____/____/_____|_____|
 import "strings"
 
 // Part() split the entire message body given as the 1st argument into error message lines and the
@@ -11,27 +17,23 @@ func Part(email *string, cutby []string, keeps bool) [2]string {
 	// @param    bool     keeps    Flag for keeping strings after "\n\n"
 	// @return   []string          { "Error message lines", "The original message" }
 	// @since    v5.0.0
-	if len(*email) == 0 { return [2]string{ "", "" } }
-	if len(cutby)  == 0 { return [2]string{ "", "" } }
+	if len(*email) == 0 { return [2]string{"", ""} }
+	if len(cutby)  == 0 { return [2]string{"", ""} }
 
-	boundaryor := ""	// A boundary string divides the error message part and the original message part
-	positionor := -1	// A position of the boudary string
-	formerpart := ""	// The error message part
-	latterpart := ""	// The original message part
+	positionor := -1 // A position of the boudary string
+	formerpart := "" // The error message part
+	latterpart := "" // The original message part
 
 	for _, e := range cutby {
-		// Find a boundary string(2nd argument) from the 1st argument
-		positionor = strings.Index(*email, e)
-		if positionor == -1 { continue }
-		boundaryor = e
+		// Find a boundary string(2nd argument)] from the 1st argument
+		positionor = strings.Index(*email, e); if positionor == -1 { continue }
 		break
 	}
 
 	if positionor > 0 {
 		// There is the boundary string in the message body
-		formersize := positionor + len(boundaryor)
-		formerpart  = (*email)[0:positionor]
-		latterpart  = (*email)[formersize + 1:len(*email) - formersize]
+		formerpart  = (*email)[:positionor]
+		latterpart  = (*email)[positionor:]
 
 	} else {
 		// Substitute the entire message to the former part when the boundary string is not included the *email
@@ -62,11 +64,11 @@ func Part(email *string, cutby []string, keeps bool) [2]string {
 			latterpart = latterpart[0:strings.Index(latterpart, "\n\n") + 1]
 		}
 
-		if !strings.HasSuffix(latterpart, "\n") {
+		if strings.HasSuffix(latterpart, "\n") == false {
 			// Append "\n" at the end of the original message
 			latterpart += "\n"
 		}
 	}
-	return [2]string{ formerpart, latterpart }
+	return [2]string{formerpart, latterpart}
 }
 
