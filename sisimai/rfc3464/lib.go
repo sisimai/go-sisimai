a0809db9a735c365c7fea43529ac53aa7194bd43
azumakuniyuki <azumakuniyuki@gmail.com>
2024-09-27 22:27:15 +0900

    Code improvement

--- sisimai/rfc3464/lib.go
+++ sisimai/rfc3464/lib.go
@@ -7,13 +7,15 @@ package rfc3464
 // | |_) | |_ | |     |_ \| || |_| '_ \| || |_ 
 // |  _ <|  _|| |___ ___) |__   _| (_) |__   _|
 // |_| \_\_|   \____|____/   |_|  \___/   |_|  
-import "fmt"
-import "slices"
 import "strings"
 import "sisimai/sis"
 import "sisimai/lhost"
 import "sisimai/rfc1894"
+import "sisimai/rfc2045"
 import "sisimai/rfc5322"
+import "sisimai/smtp/reply"
+import "sisimai/smtp/status"
+import "sisimai/smtp/command"
 import sisimoji "sisimai/string"
 
 // Inquire() decodes a bounce message which have fields defined in RFC3464
@@ -25,17 +27,23 @@ func Inquire(bf *sis.BeforeFact) sis.RisingUnderway {
 	if len(bf.Body) == 0 { return sis.RisingUnderway{} }
 
 	indicators := lhost.INDICATORS()
-	boundaries := []string{"Content-Type: message/rfc822"}
+	boundaries := []string{
+		"Content-Type: message/rfc822",
+		"Content-Type: text/rfc822-headers",
+		"Content-Disposition: inline", // See lhost-facebook-*.eml
+	}
 	startingof := map[string][]string{"message": []string{"Content-Type: message/delivery-status"}}
-
 	fieldtable := rfc1894.FIELDTABLE()
 	permessage := map[string]string{} // Store values of each Per-Message field
 	keystrings := []string{}          // Key list of permessage
 	dscontents := []sis.DeliveryMatter{{}}
+	alternates := sis.DeliveryMatter{}
 	emailparts := rfc5322.Part(&bf.Body, boundaries, false)
 	readcursor := uint8(0)            // Points the current cursor position
 	readslices := []string{""}        // Copy each line for later reference
 	recipients := uint8(0)            // The number of 'Final-Recipient' header
+	beforemesg := ""                  // String before startingof["message"]
+	ctboundary := rfc2045.Boundary(bf.Head["content-type"][0], 0)
 	v          := &(dscontents[len(dscontents) - 1])
 
 	for j, e := range(strings.Split(emailparts[0], "\n")) {
@@ -46,6 +54,18 @@ func Inquire(bf *sis.BeforeFact) sis.RisingUnderway {
 		if readcursor == 0 {
 			// Beginning of the bounce message or message/delivery-status part
 			if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= indicators["deliverystatus"] }
+
+			for {
+				// Append each string before startingof["message"][0] except the following patterns
+				// for the later reference
+				if e == ""                                 { break } // Blank line
+				if strings.HasPrefix(e, ctboundary)        { break } // boundary="----------=_1351676802-30315-116783"
+				if strings.HasPrefix(e, "Content-")        { break } // Content-Type:, Content-Disposition
+				if strings.HasPrefix(e, "This is a MIME")  { break } // This is a MIME-formatted message.
+				if strings.HasPrefix(e, "This is a multi") { break } // This is a multipart message in MIME format
+				if strings.HasPrefix(e, "This is an auto") { break } // This is an automatically generated ...
+				beforemesg += e + " "; break
+			}
 			continue
 		}
 		if readcursor & indicators["deliverystatus"] == 0 { continue }
@@ -83,29 +103,46 @@ func Inquire(bf *sis.BeforeFact) sis.RisingUnderway {
 				// Other DSN fields defined in RFC3464
 				v.Set(o[0], o[2]); if f != 1 { continue }
 
-				// Copy the lower-cased member name of DeliveryMatter{} for "permessage"
+				// Copy the lower-cased member name of sis.DeliveryMatter{} for "permessage" for
+				// the later reference
 				permessage[z] = o[2]
-				if slices.Contains(keystrings, z) == false { keystrings = append(keystrings, z) }
+				if sisimoji.EqualsAny(z, keystrings) == false { keystrings = append(keystrings, z) }
 			}
 		} else {
 			// Continued line of the value of Diagnostic-Code: field
 			if strings.HasPrefix(readslices[j], "Diagnostic-Code:") == false { continue }
 			if strings.HasPrefix(e, " ")                            == false { continue }
-			v.Diagnosis += fmt.Sprintf(" %s", sisimoji.Sweep(e))
+			v.Diagnosis += " " + sisimoji.Sweep(e)
 		}
-
 	}
 	if recipients == 0 { return sis.RisingUnderway{} }
 
+	if beforemesg != "" {
+		// Pick some values of []sis.DeliveryMatte{} from the string before startingof["message"]
+		beforemesg           = sisimoji.Sweep(beforemesg)
+		alternates.Command   = command.Find(beforemesg)
+		alternates.ReplyCode = reply.Find(beforemesg, dscontents[0].Status)
+		alternates.Status    = status.Find(beforemesg, alternates.ReplyCode)
+	}
+	issuedcode := strings.ToLower(beforemesg)
+
 	for j, _ := range dscontents {
 		// Set default values stored in "permessage" if each value in "dscontents" is empty.
-		e := &(dscontents[j])
-		for _, z := range keystrings {
-			// Do not set an empty string into each member of DeliveryMatter{}
+		e := &(dscontents[j]); for _, z := range keystrings {
+			// Do not set an empty string into each member of sis.DeliveryMatter{}
 			if len(v.Get(z))       > 0 { continue }
 			if len(permessage[z]) == 0 { continue }
 			e.Set(z, permessage[z])
 		}
+
+		// "beforemesg" contains the entire strings of e.Diagnosis
+		e.Diagnosis = sisimoji.Sweep(e.Diagnosis)
+		lowercased := strings.ToLower(e.Diagnosis)
+		if strings.Contains(issuedcode, lowercased) == true { e.Diagnosis = beforemesg }
+
+		e.Command   = command.Find(e.Diagnosis);             if e.Command   == "" { e.Command   = alternates.Command   }
+		e.ReplyCode = reply.Find(e.Diagnosis, e.Status);     if e.ReplyCode == "" { e.ReplyCode = alternates.ReplyCode }
+		e.Status    = status.Find(e.Diagnosis, e.ReplyCode); if e.Status    == "" { e.Status    = alternates.Status    }
 	}
 	return sis.RisingUnderway{ Digest: dscontents, RFC822: emailparts[1] }
 }
