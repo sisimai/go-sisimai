14f6c41fa70355a5299c44bb7f48c1d0e2013876
azumakuniyuki <azumakuniyuki@gmail.com>
2024-10-18 21:54:05 +0900

    Implement sisimai/rfc2045.CharacterSet() function: returns a charset name such as "ISO-2022-JP" in "=?iso-2022-jp?B?..."

--- sisimai/rfc2045/mime-decode.go
+++ sisimai/rfc2045/mime-decode.go
@@ -7,12 +7,14 @@ package rfc2045
 // | |_) | |_ | |     __) | | | | || ||___ \ 
 // |  _ <|  _|| |___ / __/| |_| |__   _|__) |
 // |_| \_\_|   \____|_____|\___/   |_||____/ 
+import "io"
 import "fmt"
 import "log"
 import "mime"
 import "strings"
 import "io/ioutil"
 import "mime/quotedprintable"
+import "golang.org/x/net/html/charset"
 
 // IsEncoded() checks that the argument is MIME encoded string or not.
 func IsEncoded(argv0 string) bool {
@@ -37,10 +39,16 @@ func IsEncoded(argv0 string) bool {
 func DecodeH(argv0 string) string {
 	// @param    string    argvs  MIME-Encoded text
 	// @return   string           MIME-Decoded text
-	hasdecoded := ""
+	toreadable := "" // Human readble text (has decoded)
 	stringlist := []string{}
-	decodingif := new(mime.WordDecoder)
-	characters := []string{".", "[", "]"}
+	replacingc := []string{".", "[", "]"}
+	decodingif := new(mime.WordDecoder); if CharacterSet(argv0) != "UTF-8" {
+		// The character set is not UTF-8
+		decodingif.CharsetReader = func(c string, v io.Reader) (io.Reader, error) {
+			eo, _ := charset.Lookup(c)
+			return eo.NewDecoder().Reader(v), nil
+		}
+	}
 
 	if strings.Contains(argv0, " ") {
 		// The argument string include 1 or more space characters
@@ -53,34 +61,33 @@ func DecodeH(argv0 string) string {
 
 	for j, e := range stringlist {
 		// Check and decode each part of the string
-		if IsEncoded(e) {
-			if strings.HasPrefix(e, "=?") == false {
-				// For example, "[=?UTF-8?B?...]"
-				for _, c := range characters { e = strings.Replace(e, c + "=?", "=?", -1) }
-			}
-
-			if strings.HasSuffix(e, "?=") == false {
-				// For example, "=?UTF-8?B?....?=."
-				for _, c := range characters { e = strings.Replace(e, "?=" + c, "?=", -1) }
-			}
-
-			if f, nyaan := decodingif.DecodeHeader(e); nyaan == nil {
-				// Successfully decoded
-				if j > 0 { hasdecoded += " " }
-				hasdecoded += f
-
-			} else {
-				// Failed to decode
-				if j > 0 { hasdecoded += " " }
-				hasdecoded += e
-			}
-		} else {
+		if IsEncoded(e) == false {
 			// Is not MIME-Encoded text part
-			if j > 0 { hasdecoded += " " }
-			hasdecoded += e
+			if j > 0 { toreadable += " " }; toreadable += e
+			continue
+		}
+
+		// MIME-Encoded text part such as "=?UTF-8?B?44OL44Oj44O844Oz?="
+		if strings.HasPrefix(e, "=?") == false {
+			// For example, "[=?UTF-8?B?...]"
+			for _, c := range replacingc { e = strings.Replace(e, c + "=?", "=?", -1) }
+		}
+
+		if strings.HasSuffix(e, "?=") == false {
+			// For example, "=?UTF-8?B?....?=."
+			for _, c := range replacingc { e = strings.Replace(e, "?=" + c, "?=", -1) }
+		}
+
+		if f, nyaan := decodingif.DecodeHeader(e); nyaan == nil {
+			// Successfully decoded
+			if j > 0 { toreadable += " " }; toreadable += f
+
+		} else {
+			// Failed to decode
+			if j > 0 { toreadable += " " }; toreadable += e
 		}
 	}
-	return hasdecoded
+	return toreadable
 }
 
 // DecodeB() decodes Base64 encoded text.
@@ -125,5 +132,19 @@ func DecodeQ(argv0 string) string {
 		plainvalue = string(plain)
 	}
 	return plainvalue
+}
+
+// CharacterSet() returns "ISO-2022-JP" as a character set name from "=?ISO-2022-JP?B?...?="
+func CharacterSet(argv0 string) string {
+	// @param    string argv0  Base64 or Quoted-Printable encoded text
+	// @return   string        A character set name like "iso-2022-jp"
+	if strings.HasPrefix(argv0, "=?") == false { return "" }
+	if strings.HasSuffix(argv0, "?=") == false { return "" }
+
+	argv1 := strings.ToUpper(argv0)
+	index := strings.Index(argv1, "?B?"); if index < 0 { index = strings.Index(argv1, "?Q?") }
+
+	if index < 0 { return "" }
+	return argv1[2:index]
 }
 
