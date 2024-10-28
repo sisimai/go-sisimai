41896765b79c2529844658398aa6dccfaef3a509
azumakuniyuki <azumakuniyuki@gmail.com>
2024-10-28 20:16:41 +0900

    Implement sisimai/lhost/notes

--- sisimai/lhost/notes.go
+++ sisimai/lhost/notes.go
@@ -2,23 +2,114 @@
 // This software is distributed under The BSD 2-Clause License.
 package lhost
 
+//  _ _               _      ___   _       _            
+// | | |__   ___  ___| |_   / / \ | | ___ | |_ ___  ___ 
+// | | '_ \ / _ \/ __| __| / /|  \| |/ _ \| __/ _ \/ __|
+// | | | | | (_) \__ \ |_ / / | |\  | (_) | ||  __/\__ \
+// |_|_| |_|\___/|___/\__/_/  |_| \_|\___/ \__\___||___/
+import "fmt"
+import "strings"
 import "sisimai/sis"
+import "sisimai/rfc2045"
+import "sisimai/rfc5322"
+import sisiaddr "sisimai/address"
+import sisimoji "sisimai/string"
 
 func init() {
-	// Decode bounce messages from
+	// Decode bounce messages from HCL Notes (Formerly IBM Notes(Formerly Lotus Notes))
 	InquireFor["Notes"] = func(bf *sis.BeforeFact) sis.RisingUnderway {
 		// @param    *sis.BeforeFact bf  Message body of a bounce email
 		// @return   RisingUnderway      RisingUnderway structure
-		if len(bf.Head)            == 0 { return sis.RisingUnderway{} }
-		if len(bf.Body)            == 0 { return sis.RisingUnderway{} }
+		if len(bf.Head) == 0 { return sis.RisingUnderway{} }
+		if len(bf.Body) == 0 { return sis.RisingUnderway{} }
+		if strings.HasPrefix(bf.Head["subject"][0], "Undeliverable message") == false { return sis.RisingUnderway{} }
 
-        return sis.RisingUnderway{}
+		indicators := INDICATORS()
+		boundaries := []string{"------- Returned Message --------"}
+		startingof := map[string][]string{"message": []string{"------- Failure Reasons "} }
+		messagesof := map[string][]string{
+			"networkerror": []string{"Message has exceeded maximum hop count"},
+			"userunknown":  []string{
+				"User not listed in public Name & Address Book",
+				"ディレクトリのリストにありません",
+			},
+		}
+		removedmsg := "MULTIBYTE CHARACTERS HAVE BEEN REMOVED"
+		characters := rfc2045.Parameter(bf.Head["content-type"][0], "charset")
+
+		dscontents := []sis.DeliveryMatter{{}}
+		emailparts := rfc5322.Part(&bf.Body, boundaries, false)
+		readcursor := uint8(0)            // Points the current cursor position
+		recipients := uint8(0)            // The number of 'Final-Recipient' header
+		v          := &(dscontents[len(dscontents) - 1])
+
+		for _, e := range(strings.Split(emailparts[0], "\n")) {
+			// Read error messages and delivery status lines from the head of the email to the
+			// previous line of the beginning of the original message.
+			if readcursor == 0 {
+				// Beginning of the bounce message or message/delivery-status part
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= indicators["deliverystatus"] }
+				continue
+			}
+			if readcursor & indicators["deliverystatus"] == 0 { continue }
+			if len(e) == 0                                    { continue }
+
+			// ------- Failure Reasons  --------
+			//
+			// User not listed in public Name & Address Book
+			// kijitora@notes.example.jp
+			//
+			// ------- Returned Message --------
+			if strings.Contains(e, "@") && strings.Contains(e, " ") == false {
+				// kijitora@notes.example.jp
+				if sisiaddr.IsEmailAddress(e) == false { continue }
+
+				if len(v.Recipient) > 0 {
+					// There are multiple recipient addresses in the message body.
+					dscontents = append(dscontents, sis.DeliveryMatter{})
+					v = &(dscontents[len(dscontents) - 1])
+				}
+				v.Recipient = e
+				recipients += 1
+
+			} else {
+				// Get error messages
+				if strings.HasPrefix(e, "-") { continue }
+				if sisimoji.Is8Bit(&e) || characters == "iso-2022-jp" {
+					// The error message is not ISO-8859-1
+					if characters == "" { v.Diagnosis = removedmsg; continue } // No "charset" parameter
+					utf8string, nyaan := sisimoji.ToUTF8([]byte(e), characters)
+					if nyaan != nil {
+						// Failed to convert the string to UTF8
+						v.Diagnosis = removedmsg + fmt.Sprintf(" ***error: %s", nyaan)
+
+					} else {
+						// Successfully converted
+						v.Diagnosis += " " + utf8string
+					}
+				} else {
+					// The error message does not include multi-byte characters
+					v.Diagnosis += " " + e
+				}
+			}
+		}
+		if recipients == 0 { return sis.RisingUnderway{} }
+
+		for j, _ := range dscontents {
+			// Tidy up the error message in e.Diagnosis, Try to detect the bounce reason.
+			e := &(dscontents[j])
+			e.Diagnosis = sisimoji.Sweep(e.Diagnosis)
+
+			FINDREASON: for r := range messagesof {
+				// The key name is a bounce reason name
+				for _, f := range messagesof[r] {
+					// Try to find an error message including lower-cased string listed in messagesof
+					if strings.Contains(e.Diagnosis, f) == false { continue }
+					e.Reason = r; break FINDREASON
+				}
+			}
+		}
+		return sis.RisingUnderway{ Digest: dscontents, RFC822: emailparts[1] }
     }
 }
 
-//       _               _      ___   _       _            
-//  _ __| |__   ___  ___| |_   / / \ | | ___ | |_ ___  ___ 
-// | '__| '_ \ / _ \/ __| __| / /|  \| |/ _ \| __/ _ \/ __|
-// | |  | | | | (_) \__ \ |_ / / | |\  | (_) | ||  __/\__ \
-// |_|  |_| |_|\___/|___/\__/_/  |_| \_|\___/ \__\___||___/
-//                                                         
