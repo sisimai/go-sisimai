bde5ee8e509415f827434c54abcfbf62d8a521b7
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-01 10:06:56 +0900

    sisimai/rfc3464 can decode a bounce mail returned from SurfControl #7

--- sisimai/lhost/lib.go
+++ sisimai/lhost/lib.go
@@ -28,7 +28,7 @@ func INDEX() []string {
 		"Exchange2007", "Exim", "FML", "GMX", "GoogleGroups", "Gmail",
 		"IMailServer", "InterScanMSS", "KDDI", "MailFoundry", "MailMarshalSMTP",
 		"McAfee", "MessagingServer", "Notes", "Office365", "OpenSMTPD",
-		"Postfix", "PowerMTA", "Sendmail", "SurfControl", "V5sendmail",
+		"Postfix", "PowerMTA", "Sendmail", "V5sendmail",
 		"Verizon", "X1", "X2", "X3", "X4", "X5", "X6", "Yahoo", "Zoho", "mFILTER", "qmail",
 	}
 }
