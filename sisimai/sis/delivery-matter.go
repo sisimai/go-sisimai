d69c829a7c6bd4ef0ded9d5cc8dc764529cc0874
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-02 22:16:58 +0900

    Implement sis.DeliveryMatter.Update() function

--- sisimai/sis/delivery-matter.go
+++ sisimai/sis/delivery-matter.go
@@ -9,8 +9,13 @@ package sis
 //                                |___/                               
 import "strings"
 import "sisimai/address"
+import "sisimai/rfc1123"
 import "sisimai/rfc1894"
+import "sisimai/smtp/reply"
 import "sisimai/smtp/status"
+import "sisimai/smtp/command"
+import sisimoji "sisimai/string"
+import sisiaddr "sisimai/address"
 
 var Fields1894 = rfc1894.FIELDTABLE()
 type DeliveryMatter struct {
@@ -51,6 +56,92 @@ func(this *DeliveryMatter) Select(argv0 string) string {
 		case "status":       return this.Status
 		default:             return ""
 	}
+}
+
+// Update() set the argument into the member of sis.DeliveryMatter
+func(this *DeliveryMatter) Update(argv0 string, argv1 string) bool {
+	// @param    string argv0  A lower-cased member name of sis.DeliveryMatter{}
+	// @param    string argv1  The value to be updated
+	// @return   bool          true if it has successfully updated
+	if argv0 == "" { return false } // Member name is missing
+	if argv1 == "" { return false } // Empty value is not allowd in this function
+
+	actionlist := []string{"delayed", "delivered", "expanded", "failed", "relayed"}
+	feedbacklo := []string{"abuse", "dkim", "fraud", "miscategorized", "not-spam", "opt-out", "virus", "other"}
+
+	switch argv0 {
+		default: return false
+		case "action":
+			// Only valid values are accepted
+			if this.Action == argv1 || sisimoji.EqualsAny(argv1, actionlist) == false { return false }
+			this.Action = argv1
+
+		case "agent":
+			// Any value is accepted
+			if this.Agent == argv1 { return false }
+			this.Agent = argv1
+
+		case "alias":
+			// Only valid email addresses are accepted
+			if this.Alias == argv1 || sisiaddr.IsEmailAddress(argv1) == false { return false }
+			this.Alias = argv1
+
+		case "command":
+			// Only valid values are accepted
+			if this.Command == argv1 || command.Test(argv1) == false { return false }
+			this.Command = argv1
+
+		case "date":
+			// Any value is accepted
+			if this.Date == argv1 { return false }
+			this.Date = argv1
+
+		case "diagnosis":
+			// Any value is accepted
+			if this.Diagnosis == argv1 { return false }
+			this.Diagnosis = argv1
+
+		case "feedbacktype":
+			// Only valid values are accepted
+			if this.FeedbackType == argv1 || sisimoji.EqualsAny(argv1, feedbacklo) == false { return false }
+			this.FeedbackType = argv1
+
+		case "lhost":
+			// Only valid hostnames are accepted
+			if this.Lhost == argv1 || rfc1123.IsValidHostname(argv1) == false { return false }
+			this.Lhost = strings.ToLower(argv1)
+
+		case "reason":
+			// Only valid reason names are accepted
+			if this.Reason == argv1 { return false }
+			this.Reason = strings.ToLower(argv1)
+
+		case "recipient":
+			// Only valid email addresses are accepted
+			if this.Recipient == argv1 || sisiaddr.IsEmailAddress(argv1) == false { return false }
+			this.Recipient = argv1
+
+		case "replycode":
+			// Only valid SMTP reply codes are accepted
+			if this.ReplyCode == argv1 || reply.Test(argv1) == false { return false }
+			this.ReplyCode = argv1
+
+		case "rhost":
+			// Only valid hostnames are accepted
+			if this.Rhost == argv1 || rfc1123.IsValidHostname(argv1) == false { return false }
+			this.Rhost = strings.ToLower(argv1)
+
+		case "spec":
+			// Any value is accepted
+			if this.Spec == argv1 { return false }
+			this.Spec = argv1
+
+		case "status":
+			// Only valid SMTP status codes are accepted
+			if this.Status == argv1 || status.Test(argv1) == false { return false }
+			this.Status = argv1
+	}
+	return true
 }
 
 // Set() substitutes the argv1 as a value into the member related to argv0
