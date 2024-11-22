4eb6966ec027a82f34a34abff64dc93299d4b9d4
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-22 16:56:41 +0900

    #19 Large scale code improvement in sisimai/mail.readMailbox()

--- sisimai/mail/mbox.go
+++ sisimai/mail/mbox.go
@@ -9,102 +9,48 @@ package mail
 // |_| |_| |_|\__,_|_|_/_/ |_| |_| |_|_.__/ \___/_/\_\
 import "io"
 import "os"
+import "bufio"
 import "strings"
-import sisimoji "sisimai/string"
-
-const MboxHeader = "\nFrom "
-const BufferSize = 512
 
 // readMailbox() is a UNIX mbox reader, works as a iterator.
-func(this *Mail) readMailbox() (*string, error) {
+func(this *EmailEntity) readMailbox() (*string, error) {
 	// @return   *string  Contents of the mbox
 	// @return   error    It has reached to the end of the mbox
-
-	// The method has been completed to read the mbox
-	if this.offset >= this.Size { return nil, io.EOF }
+	if this.offset >= this.Size { return nil, io.EOF } // The method has been completed to read the mbox
 	if this.handle == nil {
-		// Open the mbox, and read at the offset position
+		// Open the UNIX mbox, and read at the offset position
 		filehandle, nyaan := os.Open(this.Path); if nyaan != nil { return nil, nyaan }
 		this.handle = filehandle // Successfully opened the mbox
 	}
 
-	var emailblock string
-	var crposition bool
-	for {
-		// Read mbox until the EOF
-		loopbuffer := ""
-		readbuffer := make([]byte, BufferSize)
+	seekoffset := this.offset;                     if this.offset  < 0 { seekoffset = 0 }
+	_, nyaan   := this.handle.Seek(seekoffset, 0); if nyaan != nil { return nil, nyaan  }
+	lineending := 0;                               if this.NewLine > 2 { lineending = 1 }
+	unixmboxio := bufio.NewScanner(this.handle)
+	readbuffer := ""
+	emailblock := ""
+	thisheight := 0
 
-		if by, nyaan := this.handle.ReadAt(readbuffer, this.offset); nyaan == nil {
-			// No error returned at reading the mbox, append the read buffer into the loopbuffer
-			loopbuffer += string(readbuffer[:by])
-
-			if crposition && strings.HasPrefix(loopbuffer, "\n") {
-				// When the last character of the previous loopbuffer is "\r" and the 1st character
-				// of the current loopbuffeer is "\n", Remove "\n" at the loopbuffer[0] to avoid
-				// to be converted "\r\n" to "\n\n".
-				loopbuffer = strings.Replace(loopbuffer, "\n", "", 1)
-			}
-
-			if strings.HasSuffix(loopbuffer, "\r") {
-				// "\r\n" was divided into "\r" and "\n" ?
-				crposition = true
-
-			} else {
-				// "\r\n" was not divided into "\r" and "\n"
-				crposition = false
-			}
-			fromlindex := strings.Index(loopbuffer, MboxHeader)
-
-			if fromlindex > 0 {
-				// From line is included in the read buffer
-				emailblock += loopbuffer[:fromlindex + 1]
-				this.offset = int64(len(emailblock))
-				break
-
-			} else {
-				// There is no "From " string in the loopbuffer, try to find "From " string in the previous
-				// loop and the latest buffer
-				tempbuffer := emailblock + loopbuffer
-				fromlindex  = strings.Index(tempbuffer, MboxHeader)
-
-				if fromlindex > 0 {
-					// "From " string exists across the emailblock and the loopbuffer
-					emailblock  = tempbuffer[:len(tempbuffer) - fromlindex + 1]
-					this.offset = int64(len(emailblock))
-					break
-
-				} else {
-					// "From " string dit not appeared yet
-					emailblock  += loopbuffer
-					this.offset += int64(len(loopbuffer))
-				}
-			}
-		} else {
-			// There is any failure on reading the mbox
-			if nyaan == io.EOF {
-				// Reached to the end of the mbox
-				tempbuffer := string(readbuffer[:by])
-
-				if crposition && strings.HasPrefix(tempbuffer, "\n") {
-					// When the last character of the previous loopbuffer is "\r" and the 1st
-					// character of this tempbuffer is "\n", Remove "\n" at the tempbuffer[0] to
-					// avoid to be converted "\r\n" to "\n\n".
-					tempbuffer = strings.Replace(tempbuffer, "\n", "", 1)
-				}
-
-				emailblock  += tempbuffer
-				this.offset += int64(len(tempbuffer))
-				this.handle.Close()
-				break
-
-			} else {
-				// Something wrong
-				return nil, nyaan
-			}
+	for unixmboxio.Scan() {
+		// Read the UNIX mbox until the EOF
+		e := unixmboxio.Text()
+		if strings.HasPrefix(e, "From ") && readbuffer != "" {
+			// The line is a UNIX From line such as "From MAILER-DAEMON Fri Feb  2 18:30:22 2018"
+			// This UNIX From line is the beginning of the second or later email message
+			emailblock  = readbuffer; readbuffer = ""
+			this.offset += int64(len(emailblock) + (thisheight * lineending))
+			break
 		}
-	} // The end of the loop(for)
-	emailblock = *(sisimoji.ToLF(&emailblock))
+		thisheight += 1
+		readbuffer += e + "\n"
+	}
+
+	if readbuffer != "" {
+		// The last email message in the UNIX mbox
+		emailblock  = readbuffer
+		this.offset += int64(len(readbuffer) + (thisheight * lineending))
+		this.handle.Close()
+	}
 	return &emailblock, nil
 }
 
