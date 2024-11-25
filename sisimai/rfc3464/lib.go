ae3a84240a8768eb45a04fdfc7b6b97426b7eff4
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-25 14:15:59 +0900

    Vartiable "f" is not referred in out of "if" block

--- sisimai/rfc3464/lib.go
+++ sisimai/rfc3464/lib.go
@@ -133,7 +133,7 @@ func Inquire(bf *sis.BeforeFact) sis.RisingUnderway {
 		if readcursor & indicators["deliverystatus"] == 0 { continue }
 		if len(e) == 0                                    { continue }
 
-		f := rfc1894.Match(e); if f > 0 {
+		if f := rfc1894.Match(e); f > 0 {
 			// "e" matched with any field defined in RFC3464
 			o := rfc1894.Field(e); if len(o) == 0 { continue }
 			z := fieldtable[o[0]]
