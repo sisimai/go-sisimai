d8573d644981afb1154f461d39129c1b8d370983
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-01 23:15:54 +0900

    Make "beforemesg" a plain text before calling sisimai/string.Sweep()

--- sisimai/rfc3464/lib.go
+++ sisimai/rfc3464/lib.go
@@ -231,6 +231,7 @@ func Inquire(bf *sis.BeforeFact) sis.RisingUnderway {
 
 	if beforemesg != "" {
 		// Pick some values of []sis.DeliveryMatte{} from the string before startingof["message"]
+		beforemesg           = sisimoji.Sweep(*(sisimoji.ToPlain(&beforemesg)))
 		beforemesg           = sisimoji.Sweep(beforemesg)
 		alternates.Command   = command.Find(beforemesg)
 		alternates.ReplyCode = reply.Find(beforemesg, dscontents[0].Status)
