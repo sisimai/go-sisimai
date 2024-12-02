987bc674aacfada276810d3cfeef9f1bc24505ae
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-02 17:43:18 +0900

    Remove debug print

--- sisimai/string/ipv4.go
+++ sisimai/string/ipv4.go
@@ -49,7 +49,6 @@ func FindIPv4Address(argv1 string) []string {
 	for _, e := range strings.Split(argv1, " ") {
 		// Find a string including an IPv4 address
 		if !strings.Contains(e, ".") { continue }   // IPv4 address must include "." character
-		fmt.Printf("IPV4? = (%s)\n", e)
 		if !IsIPv4Address(e)         { continue }   // The string is an IPv4 address or not
 		ipv4a = append(ipv4a, e)
 	}
