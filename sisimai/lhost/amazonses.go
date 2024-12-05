d314f5e981434fad8c8f1458a135f3b7050ad151
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-05 17:44:38 +0900

    []string is not needed (emailparts)

--- sisimai/lhost/amazonses.go
+++ sisimai/lhost/amazonses.go
@@ -29,7 +29,7 @@ func init() {
 		if len(bf.Body) == 0 { return sis.RisingUnderway{} }
 
 		proceedsto := false
-		emailparts := []string{bf.Body, ""}
+		sespayload := bf.Body
 		for {
 			// Remote the following string begins with "--"
 			// --
@@ -37,9 +37,9 @@ func init() {
 			// https://sns.us-west-2.amazonaws.com/unsubscribe.html?SubscriptionArn=arn:aws:sns:us-west-2:1...
 			nt := "notificationType"
 			p1 := strings.Index(bf.Body, "\n\n--\n")
-			if p1 > 0 { emailparts[0] = bf.Body[:p1] }
-			if strings.Contains(emailparts[0], "!\n ") { emailparts[0] = strings.ReplaceAll(emailparts[0], "!\n ", "") }
-			p2 := strings.Index(emailparts[0], `"Message"`)
+			if p1 > 0 { sespayload = bf.Body[:p1] }
+			if strings.Contains(sespayload, "!\n ") { sespayload = strings.ReplaceAll(sespayload, "!\n ", "") }
+			p2 := strings.Index(sespayload, `"Message"`)
 
 			if p2 > 0 {
 				// The JSON included in the email is a format like the following:
@@ -48,17 +48,17 @@ func init() {
 				//  "MessageId" : "02f86d9b-eecf-573d-b47d-3d1850750c30",
 				//  "TopicArn" : "arn:aws:sns:us-west-2:123456789012:SES-EJ-B",
 				//  "Message" : "{\"notificationType\"...
-				if strings.Contains(emailparts[0], "\\") { emailparts[0] = strings.ReplaceAll(emailparts[0], "\\",   "") }
-				p3 := sisimoji.IndexOnTheWay(emailparts[0], "{",  p2 + 9)
-				p4 := sisimoji.IndexOnTheWay(emailparts[0], "\n", p2 + 9)
-				emailparts[0] = emailparts[0][p3:p4]
-				emailparts[0] = strings.TrimRight(emailparts[0], ",")
-				emailparts[0] = strings.TrimRight(emailparts[0], `"`)
+				if strings.Contains(sespayload, "\\") { sespayload = strings.ReplaceAll(sespayload, "\\",   "") }
+				p3 := sisimoji.IndexOnTheWay(sespayload, "{",  p2 + 9)
+				p4 := sisimoji.IndexOnTheWay(sespayload, "\n", p2 + 9)
+				sespayload = sespayload[p3:p4]
+				sespayload = strings.TrimRight(sespayload, ",")
+				sespayload = strings.TrimRight(sespayload, `"`)
 			}
 
-			if strings.Contains(emailparts[0], nt)   == false { break }
-			if strings.HasPrefix(emailparts[0], "{") == false { break }
-			if strings.HasSuffix(emailparts[0], "}") == false { break }
+			if strings.Contains(sespayload, nt)   == false { break }
+			if strings.HasPrefix(sespayload, "{") == false { break }
+			if strings.HasSuffix(sespayload, "}") == false { break }
 			proceedsto = true; break
 		}
 		if proceedsto == false { return sis.RisingUnderway{} }
@@ -219,14 +219,14 @@ func init() {
 		var notifiedto NotifiedTo  // This instance have 3 types: ReturnedTo, Deliveries, Complained
 		var mailinside *mailObject // The pointer to mailObject struct
 		var jsonerrors error  = errors.New("Invalid JSON format")
-		var jsonstring []byte = []byte(emailparts[0])
+		var jsonstring []byte = []byte(sespayload)
 
 		for json.Valid(jsonstring) == true {
 			// The JSON string should contain one of the followings in "notificationType" field
 			// - "Bounce"
 			// - "Complaint"
 			// - "Delivery"
-			if strings.Contains(emailparts[0], `"notificationType":"Bounce"`) {
+			if strings.Contains(sespayload, `"notificationType":"Bounce"`) {
 				// {"notificationType":"Bounce","bounce":{"bounceType":"Permanent",...
 				var p ReturnedTo
 				jsonerrors = json.Unmarshal(jsonstring, &p); if jsonerrors != nil { break }
@@ -234,7 +234,7 @@ func init() {
 				mailinside = &p.Mail
 				whatnotify = "B"
 
-			} else if strings.Contains(emailparts[0], `"notificationType":"Complaint"`) {
+			} else if strings.Contains(sespayload, `"notificationType":"Complaint"`) {
 				// {"notificationType":"Complaint","complaint":{"complainedRecipients":[{"e...
 				var p Complained
 				jsonerrors = json.Unmarshal(jsonstring, &p); if jsonerrors != nil { break }
@@ -242,7 +242,7 @@ func init() {
 				mailinside = &p.Mail
 				whatnotify = "C"
 
-			} else if strings.Contains(emailparts[0], `"notificationType":"Delivery"`) {
+			} else if strings.Contains(sespayload, `"notificationType":"Delivery"`) {
 				// {"notificationType":"Delivery","mail":{"timestamp":...
 				var p Deliveries
 				jsonerrors = json.Unmarshal(jsonstring, &p); if jsonerrors != nil { break }
@@ -338,9 +338,8 @@ func init() {
 		for _, e := range mailinside.Headers { cv += fmt.Sprintf("%s: %s\n", e.Name, e.Value) }
 		if mailinside.CommonHeaders.Date    != "" { cv += fmt.Sprintf("Date: %s\n", mailinside.CommonHeaders.Date)       }
 		if mailinside.CommonHeaders.Subject != "" { cv += fmt.Sprintf("Subject: %s\n", mailinside.CommonHeaders.Subject) }
-		emailparts[1] = cv
 
-		return sis.RisingUnderway{ Digest: dscontents, RFC822: emailparts[1] }
+		return sis.RisingUnderway{ Digest: dscontents, RFC822: cv }
     }
 }
 
