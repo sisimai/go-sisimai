814a77a54bf5fe59530edceca6e5d208815a83c7
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-20 18:38:38 +0900

    Register sisimai/reason/failedstarttls.go #35

---
+++ sisimai/reason/failedstarttls.go
@@ -0,0 +1,34 @@
+// Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
+// This software is distributed under The BSD 2-Clause License.
+package reason
+
+//  _____     _ _          _ ____ _____  _    ____ _____ _____ _     ____  
+// |  ___|_ _(_) | ___  __| / ___|_   _|/ \  |  _ \_   _|_   _| |   / ___| 
+// | |_ / _` | | |/ _ \/ _` \___ \ | | / _ \ | |_) || |   | | | |   \___ \ 
+// |  _| (_| | | |  __/ (_| |___) || |/ ___ \|  _ < | |   | | | |___ ___) |
+// |_|  \__,_|_|_|\___|\__,_|____/ |_/_/   \_\_| \_\|_|   |_| |_____|____/ 
+import "strings"
+import "sisimai/sis"
+import sisimoji "sisimai/string"
+
+func init() {
+	// Try to check the argument string includes any of the strings in the error message pattern
+	IncludedIn["FailedSTARTTLS"] = func(argv1 string) bool {
+		// @param    string argv1 Does the string include any of the strings listed in the pattern?
+		// @return   bool         true: Included, false: did not include
+		index := []string{
+			"starttls is required to send mail",
+			"tls required but not supported", // SendGrid:the recipient mailserver does not support TLS or have a valid certificate
+		}
+		for _, v := range index { if strings.Contains(argv1, v) { return true }}
+		return false
+	}
+
+	// The bounce reason is "FailedSTARTTLS" or not
+	ProbesInto["FailedSTARTTLS"] = func(fo *sis.Fact) bool {
+		// @param    *sis.Fact fo    Struct to be detected the reason
+		// @return   bool            true: is FailedSTARTTLS, false: is not FailedSTARTTLS
+		return false
+	}
+}
+
