f20615fcec066bbd6479db3cd742aa24d95972ba
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-21 12:52:18 +0900

    IsIPv4Address() and FindIPv4Address() require a *string as the first argument #34

--- sisimai/rfc791/lib.go
+++ sisimai/rfc791/lib.go
@@ -11,43 +11,43 @@ import "strings"
 import "strconv"
 
 // IsIPv4Address() returns "true" when the given string is an IPv4 address
-func IsIPv4Address(argv1 string) bool {
-	// @param    string argv1  IPv4 address like "192.0.2.25"
+func IsIPv4Address(argv1 *string) bool {
+	// @param    *string argv1 IPv4 address like "192.0.2.25"
 	// @return   bool          true:  is an IPv4 address
 	//                         false: is not an IPv4 address
-	if len(argv1) < 7                 { return false }
-	if strings.Count(argv1, ".") != 3 { return false }
+	if argv1 == nil || len(*argv1) < 7 { return false }
+	if strings.Count(*argv1, ".") != 3 { return false }
 
-	match := true
-	for _, e := range strings.Split(argv1, ".") {
+	for _, e := range strings.Split(*argv1, ".") {
 		// Check each octet is between 0 and 255
 		v, nyaan := strconv.Atoi(e)
-		if nyaan != nil { match = false; break }
-		if v < 0        { match = false; break }
-		if v > 255      { match = false; break }
+		if nyaan != nil { return false }
+		if v < 0        { return false }
+		if v > 255      { return false }
 	}
-	return match
+	return true
 }
 
 // FindIPv4Address() find IPv4 addresses from the given string
-func FindIPv4Address(argv1 string) []string {
-	// @param    string   argv1  String including an IPv4 address
+func FindIPv4Address(argv1 *string) []string {
+	// @param    *string   argv1 String including an IPv4 address
 	// @return   []string        List of IPv4 addresses
 	// @since    v5.2.0
-	if len(argv1) < 7 { return []string{} }
+	if argv1 == nil || len(*argv1) < 7 { return []string{} }
 
 	// Rewrite: "mx.example.jp[192.0.2.1]" => "mx.example.jp 192.0.2.1"
-	argv1  = strings.ReplaceAll(argv1, "(", " ",)
-	argv1  = strings.ReplaceAll(argv1, ")", " ",)
-	argv1  = strings.ReplaceAll(argv1, "[", " ",)
-	argv1  = strings.ReplaceAll(argv1, "]", " ",)
-	argv1  = strings.ReplaceAll(argv1, ",", " ",)
+	argv2 := *argv1
+	argv2  = strings.ReplaceAll(argv2, "(", " ",)
+	argv2  = strings.ReplaceAll(argv2, ")", " ",)
+	argv2  = strings.ReplaceAll(argv2, "[", " ",)
+	argv2  = strings.ReplaceAll(argv2, "]", " ",)
+	argv2  = strings.ReplaceAll(argv2, ",", " ",)
 	ipv4a := []string{}
 
-	for _, e := range strings.Split(argv1, " ") {
+	for _, e := range strings.Split(argv2, " ") {
 		// Find a string including an IPv4 address
 		if !strings.Contains(e, ".") { continue }   // IPv4 address must include "." character
-		if !IsIPv4Address(e)         { continue }   // The string is an IPv4 address or not
+		if !IsIPv4Address(&e)        { continue }   // The string is an IPv4 address or not
 		ipv4a = append(ipv4a, e)
 	}
 	return ipv4a
