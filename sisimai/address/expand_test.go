9d72a43e59c8dff2ed27da4cd5602ec7ab861cd5
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-30 16:07:33 +0900

    Test suites for sisimai/address.Expand*() #41

---
+++ sisimai/address/expand_test.go
@@ -0,0 +1,54 @@
+// Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
+// This software is distributed under The BSD 2-Clause License.
+package address
+
+//  _____         _      __        _     _                     _____                            _ 
+// |_   _|__  ___| |_   / /_ _  __| | __| |_ __ ___  ___ ___  | ____|_  ___ __   __ _ _ __   __| |
+//   | |/ _ \/ __| __| / / _` |/ _` |/ _` | '__/ _ \/ __/ __| |  _| \ \/ / '_ \ / _` | '_ \ / _` |
+//   | |  __/\__ \ |_ / / (_| | (_| | (_| | | |  __/\__ \__ \_| |___ >  <| |_) | (_| | | | | (_| |
+//   |_|\___||___/\__/_/ \__,_|\__,_|\__,_|_|  \___||___/___(_)_____/_/\_\ .__/ \__,_|_| |_|\__,_|
+//                                                                       |_|                      
+import "testing"
+
+func TestExpandVERP(t *testing.T) {
+	fn := "ExpandVERP"
+	cx := 0
+	ae := []struct {testname string; argument string; expected string}{
+		{"(cat+neko=example.jp@example.org)",  "cat+neko=example.jp@example.org", "neko@example.jp"},
+		{"('')", "", ""},
+		{"(+neko!example.jp)", "+neko!example.jp", ""},
+		{"(=neko!example.jp)", "=neko!example.jp", ""},
+		{"(neko)", "neko", ""},
+	}
+
+	for _, e := range ae {
+		t.Run(fn + e.testname, func(t *testing.T) {
+			cx += 1
+			cv := ExpandVERP(e.argument)
+			if cv != e.expected { t.Errorf("[%6d]: %s() is (%s) not (%s)", cx, fn, cv, e.expected) }
+		})
+	}
+	t.Logf("The number of tests = %d", cx)
+}
+
+func TestExpandAlias(t *testing.T) {
+	fn := "ExpandAlias"
+	cx := 0
+	ae := []struct{testname string; argument string; expected string}{
+		{"('')", "", ""},
+		{"(<neko>)", "<neko>", "<neko>"},
+		{"(<neko@example.jp>)", "<neko@example.jp>", "neko@example.jp"},
+		{"(<neko@example.jp)",  "<neko@example.jp",  "neko@example.jp"},
+		{"(neko@example.jp>)",  "neko@example.jp>",  "neko@example.jp"},
+	}
+	for _, e := range ae {
+		t.Run(fn + e.testname, func(t *testing.T) {
+			cx += 1
+			cv := ExpandAlias(e.argument)
+			if cv != e.expected { t.Errorf("[%6d]: %s() is (%s) not (%s)", cx, fn, cv, e.expected) }
+		})
+	}
+	t.Logf("The number of tests = %d", cx)
+}
+
+
