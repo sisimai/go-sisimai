cd4e53146a27c4f90500721a9a5e31be86cac091
azumakuniyuki <azumakuniyuki@gmail.com>
2025-01-21 16:39:17 +0900

    the value of "Path" should be set at sisimai/mail.readEmail()

--- sisimai/mail/maildir.go
+++ sisimai/mail/maildir.go
@@ -40,8 +40,11 @@ func(this *EmailEntity) readEmail() (*string, error) {
 
 	for {
 		// Try to read the email file
-		cf := filepath.Clean(filepath.FromSlash(this.Dir + "/" + this.payload[this.offset]))
+		this.File = this.payload[this.offset]
+		this.Path = filepath.Clean(filepath.FromSlash(this.Dir + "/" + this.File))
 		this.offset++
+		cf := this.Path
+
 		buf, nyaan := os.ReadFile(cf); if nyaan != nil || len(buf) == 0 {
 			// Failed to read the email file or the email file is empty
 			if this.offset >= this.Size { return nil, io.EOF }
