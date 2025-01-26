9ef3449233648de9fd7b0b66fca9013cbb4064cc
azumakuniyuki <azumakuniyuki@gmail.com>
2025-01-26 10:37:27 +0900

    #41 Count the number of tests

--- sisimai/sis/not-decoded_test.go
+++ sisimai/sis/not-decoded_test.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2024-2025 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package sis
 
@@ -14,43 +14,51 @@ func TestMakeNotDecoded(t *testing.T) {
 	cc := "NotDecoded"
 	fn := "MakeNotDecoded"
 	cv := MakeNotDecoded("Test message", true)
+	cx := 0
 
-	if cv == nil                { t.Fatalf("%s() = nil", fn) }
-	if cv.EmailFile != ""       { t.Errorf("%s.EmailFile is not empty: %s", cc, cv.EmailFile) }
-	if cv.BecauseOf == ""       { t.Errorf("%s.BecauseOf is empty", cc) }
-	if cv.Timestamp.Unix() == 0 { t.Errorf("%s.Timestamp.Unix() is 0", cc) }
-	if cv.CalledOff == false    { t.Errorf("%s.CalledOff is false", cc) }
-	if cv.DecodedBy != ""       { t.Errorf("%s.DecodedBy is not empty: %s", cc, cv.DecodedBy) }
-	if strings.Contains(cv.WhoCalled, fn) == false { t.Errorf("%s.WhoCalled is %s", cc, cv.WhoCalled) }
+	cx++; if cv == nil                { t.Fatalf("%s() = nil", fn) }
+	cx++; if cv.EmailFile != ""       { t.Errorf("%s.EmailFile is not empty: %s", cc, cv.EmailFile) }
+	cx++; if cv.BecauseOf == ""       { t.Errorf("%s.BecauseOf is empty", cc) }
+	cx++; if cv.Timestamp.Unix() == 0 { t.Errorf("%s.Timestamp.Unix() is 0", cc) }
+	cx++; if cv.CalledOff == false    { t.Errorf("%s.CalledOff is false", cc) }
+	cx++; if cv.DecodedBy != ""       { t.Errorf("%s.DecodedBy is not empty: %s", cc, cv.DecodedBy) }
+	cx++; if strings.Contains(cv.WhoCalled, fn) == false { t.Errorf("%s.WhoCalled is %s", cc, cv.WhoCalled) }
+	t.Logf("The number of tests = %d", cx)
 }
 
 func TestError(t *testing.T) {
 	fn := "Error"
 	cv := &NotDecoded{BecauseOf: "Test for " + fn, EmailFile: "25.eml"}
-	cx := &NotDecoded{BecauseOf: "", EmailFile: "25.eml"}
+	cw := &NotDecoded{BecauseOf: "", EmailFile: "25.eml"}
+	cx := 0
 
-	if cv == nil                { t.Fatalf("%s() = nil", fn) }
-	if cv.Error() == ""         { t.Fatalf("%s() = empty string", fn) }
-	if cx.Error() != ""         { t.Fatalf("%s() = string %s", fn, cx.Error()) }
+	cx++; if cv == nil        { t.Fatalf("%s() = nil", fn) }
+	cx++; if cv.Error() == "" { t.Fatalf("%s() = empty string", fn) }
+	cx++; if cw.Error() != "" { t.Fatalf("%s() = string %s", fn, cw.Error()) }
+	t.Logf("The number of tests = %d", cx)
 }
 
 func TestLabel(t *testing.T) {
 	fn := "Label"
 	cv := &NotDecoded{BecauseOf: "Test for " + fn, CalledOff: true}
-	cx := &NotDecoded{BecauseOf: "Test for " + fn, CalledOff: false}
+	cw := &NotDecoded{BecauseOf: "Test for " + fn, CalledOff: false}
+	cx := 0
 
-	if cv == nil                { t.Fatalf("%s() = nil", fn) }
-	if strings.HasPrefix(cv.Label(), " *****error") == false { t.Errorf("%s() = (%s)", fn, cv.Label()) }
-	if strings.HasPrefix(cx.Label(), " ***warning") == false { t.Errorf("%s() = (%s)", fn, cx.Label()) }
+	cx++; if cv == nil                { t.Fatalf("%s() = nil", fn) }
+	cx++; if strings.HasPrefix(cv.Label(), " *****error") == false { t.Errorf("%s() = (%s)", fn, cv.Label()) }
+	cx++; if strings.HasPrefix(cw.Label(), " ***warning") == false { t.Errorf("%s() = (%s)", fn, cw.Label()) }
+	t.Logf("The number of tests = %d", cx)
 }
 
 func TestEmail(t *testing.T) {
 	fn := "Email"
 	cv := &NotDecoded{BecauseOf: "Test for " + fn}
+	cx := 0
 
-	if cv == nil                { t.Fatalf("%s() = nil", fn) }
-	if cv.Email("22.eml") == "" { t.Errorf("%s() = empty string", fn) }
-	if cv.EmailFile != "22.eml" { t.Errorf("%s() = (%s)", fn, cv.EmailFile) }
-	if cv.Email("25") == "25"   { t.Errorf("%s() = 25", fn) }
+	cx++; if cv == nil                { t.Fatalf("%s() = nil", fn) }
+	cx++; if cv.Email("22.eml") == "" { t.Errorf("%s() = empty string", fn) }
+	cx++; if cv.EmailFile != "22.eml" { t.Errorf("%s() = (%s)", fn, cv.EmailFile) }
+	cx++; if cv.Email("25") == "25"   { t.Errorf("%s() = 25", fn) }
+	t.Logf("The number of tests = %d", cx)
 }
 
