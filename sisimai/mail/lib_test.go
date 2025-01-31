8afd4323144f868f60bf27ad918fd5e5463e8fca
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-01 05:54:58 +0900

    #41 Test suites for sisimai.mail.EmailEntity.Read()

--- sisimai/mail/lib_test.go
+++ sisimai/mail/lib_test.go
@@ -36,11 +36,11 @@ var Maildir = []string{
 
 func TestRise(t *testing.T) {
 	fn := "Rise"
+	cf := ""
 	cx := 0
 
-	for _, e := range Mailbox {
+	cf = "EmailEntity(mailbox)"; for _, e := range Mailbox {
 		cv, ce := Rise(e)
-		cf := "EmailEntity(mailbox)"
 		cx++; if cv == nil { t.Errorf("%s(%s) returns nil", fn, e) }
 		cx++; if ce != nil { t.Errorf("%s(%s) returns error: %s", fn, e, ce) }
 		cx++; if cv.Kind != "mailbox" { t.Errorf("%s.Kind is not mailbox: %s", cf, cv.Kind) }
@@ -53,9 +53,8 @@ func TestRise(t *testing.T) {
 		cx++; if len(cv.payload) > 0  { t.Errorf("%s.payload is not 0: %d", cf, len(cv.payload)) }
 	}
 
-	for _, e := range Maildir {
+	cf = "EmailEntity(maildir)"; for _, e := range Maildir {
 		cv, ce:= Rise(e)
-		cf := "EmailEntity(maildir)"
 		cx++; if cv == nil { t.Errorf("%s(%s) returns nil", fn, e) }
 		cx++; if ce != nil { t.Errorf("%s(%s) returns error: %s", fn, e, ce) }
 		cx++; if cv.Kind != "maildir" { t.Errorf("%s.Kind is not maildir: %s", cf, cv.Kind) }
@@ -68,5 +67,35 @@ func TestRise(t *testing.T) {
 	}
 
 	t.Logf("The number of tests = %d", cx)
+}
+
+func TestRead(t *testing.T) {
+	fn := "Read"
+	cf := ""
+	cx := 0
+
+	cf = "EmailEntity(mailbox)"; for _, e := range Mailbox {
+		if eo, _ := Rise(e); eo != nil {
+			cv, ce := eo.Read()
+			cx++; if ce != nil           { t.Errorf("%s.%s(%s) returns error: %s", cf, fn, e, ce) }
+			cx++; if len(*cv)  == 0      { t.Errorf("%s.%s(%s) returns empty", cf, fn, e) }
+			cx++; if eo.offset == 0      { t.Errorf("%s.offset is 0", cf) }
+			cx++; if eo.handle == nil    { t.Errorf("%s.handle is nil", cf) }
+			cx++; if eo.Size < eo.offset { t.Errorf("%s.offset(%d) is greater than Size(%d)", cf, eo.Size, eo.offset) }
+		}
+	}
+
+	cf = "EmailEntity(maildir)"; for _, e := range Maildir {
+		if eo, _ := Rise(e); eo != nil {
+			cv, ce := eo.Read()
+			cx++; if ce != nil           { t.Errorf("%s.%s(%s) returns error: %s", cf, fn, e, ce) }
+			cx++; if len(*cv)  == 0      { t.Errorf("%s.%s(%s) returns empty", cf, fn, e) }
+			cx++; if eo.offset == 0      { t.Errorf("%s.offset is 0", cf) }
+			cx++; if eo.handle != nil    { t.Errorf("%s.handle is not nil", cf) }
+			cx++; if eo.Size < eo.offset { t.Errorf("%s.offset(%d) is greater than Size(%d)", cf, eo.Size, eo.offset) }
+			cx++; if strings.HasSuffix(eo.Path, ".eml") == false { t.Errorf("%s.Path does not end with .eml", cf) }
+			cx++; if strings.HasSuffix(eo.File, ".eml") == false { t.Errorf("%s.File does not end with .eml", cf) }
+		}
+	}
 }
 
