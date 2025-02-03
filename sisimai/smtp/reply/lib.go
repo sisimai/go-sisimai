91c73c764938dbf54d6383037c0eb79271da9c63
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-04 07:58:03 +0900

    sisimai/smtp/reply.Find() has been moved to sisimai/smtp/reply/lib.go

--- sisimai/smtp/reply/lib.go
+++ sisimai/smtp/reply/lib.go
@@ -78,6 +78,8 @@ package reply
 // 557   draft-moore-email-addrquery-01
 //
 import "strconv"
+import "strings"
+import sisimoji "sisimai/string"
 
 var ReplyCode2 = []string{"211", "214", "220", "221", "235", "250", "251", "252", "253", "354"}
 var ReplyCode4 = []string{"421", "450", "451", "452", "422", "430", "432", "453", "454", "455", "456", "458", "459"}
@@ -115,5 +117,53 @@ func Test(argv0 string) bool {
 		return true
 	}
 	return true
+}
+
+// Find() returns an SMTP reply code found from the given string
+func Find(argv1 string, argv2 string) string {
+	// @param    string argv1  String including SMTP reply code like 550
+	// @param    string argv2  Status code like 5.1.1 or 2 or 4 or 5
+	// @return   string        SMTP reply code or empty if the first argument did not include SMTP Reply Code value
+	if len(argv1) < 3                                     { return "" }
+	if strings.Contains(strings.ToUpper(argv1), "X-UNIX") { return "" }
+	if len(argv2) == 0 { argv2 = "0" }
+
+	statuscode := argv2[0:1]
+	replycodes := []string{}
+
+	if statuscode == "2" || statuscode == "4" || statuscode == "5" {
+		// The first character of the 2nd argument is 2 or 4 or 5
+		replycodes = CodeOfSMTP[statuscode]
+
+	} else {
+		// The first character of the 2nd argument is 0 or other values
+		// TODO: use "slices" package and slices.Concat() avaialble from Go 1.22
+		//       https://pkg.go.dev/slices@master
+		replycodes = append(replycodes, CodeOfSMTP["5"]...)
+		replycodes = append(replycodes, CodeOfSMTP["4"]...)
+		replycodes = append(replycodes, CodeOfSMTP["2"]...)
+	}
+
+	esmtperror := " " + argv1 + " "
+	esmtpreply := ""
+	for _, e := range replycodes {
+		// Try to find an SMTP Reply Code from the given string
+		appearance := strings.Count(esmtperror, e); if appearance == 0 { continue }
+		startingat := 1
+
+		for j := 0; j < appearance; j++ {
+			// Find all the reply codes in the error message
+			replyindex := sisimoji.IndexOnTheWay(esmtperror, e, startingat); if replyindex < 0 { break }
+			formerchar := []byte(esmtperror[replyindex - 1:replyindex])[0]
+			latterchar := []byte(esmtperror[replyindex + 3:replyindex + 4])[0]
+
+			if formerchar > 45 && formerchar < 58 { startingat += replyindex + 3; continue } // '.' => '9'
+			if latterchar > 45 && latterchar < 58 { startingat += replyindex + 3; continue } // '.' => '9'
+			esmtpreply = e
+			break
+		}
+		if esmtpreply != "" { break }
+	}
+	return esmtpreply
 }
 
