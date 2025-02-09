3bff054e6dacc7c1939c6665c6ae6159c203fb5e
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-10 08:57:07 +0900

    Update "loc", "count-test-cases" targets

--- Developers.mk
+++ Developers.mk
@@ -1,4 +1,4 @@
-# go-sisimai/Developers.mk
+# libsisimai.org/sisimai/Developers.mk
 #  ____                 _                                       _    
 # |  _ \  _____   _____| | ___  _ __   ___ _ __ ___   _ __ ___ | | __
 # | | | |/ _ \ \ / / _ \ |/ _ \| '_ \ / _ \ '__/ __| | '_ ` _ \| |/ /
@@ -13,15 +13,23 @@ MKDIR := mkdir -p
 LS    := ls -1
 CP    := cp
 
+LIBSISIMAI := libsisimai.org
+SISIMAIDIR := address arf fact lda lhost mail message reason rfc1123 rfc1894 rfc2045 rfc3464 \
+			  rfc3834 rfc5322 rfc5965 rfc791 rhost sis smtp string
+COVERAGETO := coverage.txt
+
 # -------------------------------------------------------------------------------------------------
 .PHONY: clean
 
+list-test-files:
+	@ find $(SISIMAIDIR) -type f -name '*_test.go'
+
 count-test-cases:
-	@ go test -v `find sisimai -type f -name '*_test.go' | xargs dirname | sort | uniq` | \
+	@ go test -v `find $(SISIMAIDIR) -type f -name '*_test.go' | xargs dirname | sort | uniq | sed -e 's|^|./|g'` | \
 		grep 'The number of ' | awk '{ cx += $$7 } END { print cx }'
 
 loc:
-	@ find . -type f -name '*.go' | grep -vE '/(tmp|sbin|internal)/' | grep -v '_test.go' | \
+	@ find libsisimai.go $(SISIMAIDIR) -type f -name '*.go' -not -name '*_test.go' | \
 		xargs grep -vE '(^$$|^//|/[*]|[*]/|^ |^--)' | grep -vE "\t+//" | wc -l
 
 clean:
