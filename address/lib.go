f64544ab200ca2f83a0334017bf2d68fb87163dd
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-13 04:35:00 +0900

    Update comments, banners for the page generated by godoc #53

--- address/lib.go
+++ address/lib.go
@@ -1,12 +1,13 @@
 // Copyright (C) 2020-2021,2024-2025 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
-package address
-
 //            _     _                   
 //   __ _  __| | __| |_ __ ___  ___ ___ 
 //  / _` |/ _` |/ _` | '__/ _ \/ __/ __|
 // | (_| | (_| | (_| | | |  __/\__ \__ \
 //  \__,_|\__,_|\__,_|_|  \___||___/___/
+
+// Package "address" provide functions related to an email address
+package address
 import "strings"
 import "libsisimai.org/sisimai/rfc5322"
 
