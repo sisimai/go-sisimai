e188d6b6de23979ded9925203e0e91bdace176f4
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-13 04:26:09 +0900

    Update comments for the page generated by `godoc` #53

--- libsisimai.go
+++ libsisimai.go
@@ -1,17 +1,17 @@
 // Copyright (C) 2020-2025 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
-package sisimai
-
 //  _ _ _         _     _                 _                  
 // | (_) |__  ___(_)___(_)_ __ ___   __ _(_)  ___  _ __ __ _ 
 // | | | '_ \/ __| / __| | '_ ` _ \ / _` | | / _ \| '__/ _` |
 // | | | |_) \__ \ \__ \ | | | | | | (_| | || (_) | | | (_| |
 // |_|_|_.__/|___/_|___/_|_| |_| |_|\__,_|_(_)___/|_|  \__, |
 // https://libsisimai.org/                              |___/
-//
+
 // sisimai is a library that decodes complex and diverse bounce emails and outputs the results of the
 // delivery failure, such as the reason for the bounce and the recipient email address, in structured
 // data. It is also possible to output in JSON format.
+package sisimai
+
 import "io"
 import "fmt"
 import "errors"
