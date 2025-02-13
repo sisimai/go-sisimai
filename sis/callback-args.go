73da37add12a6ebda5da6b56074e9868cbec3a4a
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-13 12:29:35 +0900

    Add comments for each field of sis.CallbackArgs #53

--- sis/callback-args.go
+++ sis/callback-args.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2024-2025 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 //   ____      _ _ _                _        _                  
 //  / ___|__ _| | | |__   __ _  ___| | __   / \   _ __ __ _ ___ 
@@ -12,7 +12,7 @@ package sis
 // CallbackArgs is an argument of the callback functions that are called at sisimai.Rise() and
 // message.sift(). It is aliased to sisimai.CallbackArgs at the libsisimai.go
 type CallbackArgs struct {
-	Headers map[string][]string
-	Payload *string
+	Headers map[string][]string // Email headers of the bounce mail
+	Payload *string             // Entire message body of the bounce mail
 }
 
