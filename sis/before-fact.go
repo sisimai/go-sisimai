6094fd986c3befda927505a3e85ccc086427016f
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-13 12:30:16 +0900

    Add comments for each function, update some comments for each field of sis.BeforeFact #53

--- sis/before-fact.go
+++ sis/before-fact.go
@@ -11,20 +11,22 @@ package sis
 // sisimai/message.Rise() returns BeforeFact{}
 type BeforeFact struct {
 	Sender  string              // Unix FROM line ("From ")
-	Headers map[string][]string // Email headers
-	Payload string              // Email body
+	Headers map[string][]string // Email headers of the bounce mail
+	Payload string              // Entire message body of the bounce mail
 	RFC822  map[string][]string // Email headers of the original message
-	Digest  []DeliveryMatter    // Decoded results returned from sisimai/lhost/*
-	Catch   interface{}         // Any data structure returned by the callback function
+	Digest  []DeliveryMatter    // Decoded results returned from lhost/via-*.go
+	Catch   interface{}         // Any data structure returned by the callback function [0]
 	Errors  []NotDecoded        // All the errors and warnings
 }
 
+// Empty() returns true when Headers or body is empty
 func(this *BeforeFact) Empty() bool {
 	if len(this.Headers) == 0  { return true }
 	if this.Payload      == "" { return true }
 	return false
 }
 
+// Void() returns true when Digest or RFC822 is empty
 func(this *BeforeFact) Void() bool {
 	// @param    NONE
 	// @return   bool   Returns true if BeforeFact.Digest or RFC822 is empty
