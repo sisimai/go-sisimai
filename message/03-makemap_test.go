3eb89f320f4bfd88600e79283fcd597a864c42a8
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-22 07:58:51 +0900

    #41 Test code improvements for message.makemap()

--- message/03-makemap_test.go
+++ message/03-makemap_test.go
@@ -16,24 +16,29 @@ import "os"
 func TestMakeMap(t *testing.T) {
 	fn := "sisimai/message.makemap"
 	cx := 0
-	ae := "../set-of-emails/maildir/bsd/lhost-opensmtpd-17.eml"
-	bx, _ := os.ReadFile(ae); if len(bx) == 0 {
-		cx++; t.Fatalf("os.ReadFile(%s) returns an empty string", ae)
+	ae := []string{
+		"../set-of-emails/maildir/bsd/lhost-opensmtpd-17.eml",
+		"../set-of-emails/maildir/bsd/lhost-postfix-78.eml",
 	}
-	et := string(bx); em, _ := mail.ReadMessage(strings.NewReader(et))
-	cv := makemap(&em.Header, false)
+	for _, ef := range ae {
+		bx, _ := os.ReadFile(ef); if len(bx) == 0 {
+			cx++; t.Fatalf("os.ReadFile(%s) returns an empty string", ef)
+		}
+		et := string(bx); em, _ := mail.ReadMessage(strings.NewReader(et))
+		cv := makemap(&em.Header, false)
 
-	cx++; if len(cv) == 0 { t.Errorf("%s() returns empty", fn) }
-	for e := range cv {
-		cx++; if e == ""          { t.Errorf("%s() returns an empty key", fn) }
-		cx++; if len(cv[e]) == 0  { t.Errorf("%s()[%s] have no elements", fn, e) }
-		cx++; if cv[e][0]   == "" { t.Errorf("%s()[%s][0] is empty", fn, e) }
-	}
-	for _, e := range []string{"from", "received", "message-id", "content-type", "subject"} {
-		cx++; if cv[e][0]   == "" { t.Errorf("%s()[%s][0] is empty", fn, e) }
-	}
-	for _, e := range []string{"dkim-signature", "authentication-results", "received-spf"} {
-		cx++; if len(cv[e]) != 0  { t.Errorf("%s()[%s] is not empty", fn, e) }
+		cx++; if len(cv) == 0 { t.Errorf("%s() returns empty", fn) }
+		for e := range cv {
+			cx++; if e == ""          { t.Errorf("%s() returns an empty key", fn) }
+			cx++; if len(cv[e]) == 0  { t.Errorf("%s()[%s] have no elements", fn, e) }
+			cx++; if cv[e][0]   == "" { t.Errorf("%s()[%s][0] is empty", fn, e) }
+		}
+		for _, e := range []string{"from", "received", "message-id", "content-type", "subject"} {
+			cx++; if cv[e][0]   == "" { t.Errorf("%s()[%s][0] is empty", fn, e) }
+		}
+		for _, e := range []string{"dkim-signature", "authentication-results", "received-spf"} {
+			cx++; if len(cv[e]) != 0  { t.Errorf("%s()[%s] is not empty", fn, e) }
+		}
 	}
 
 	t.Logf("The number of tests = %d", cx)
