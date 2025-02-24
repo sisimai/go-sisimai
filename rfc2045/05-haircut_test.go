1b9641bf46d23033be78ace53fd69501163cb8a8
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-24 13:36:57 +0900

    #41 #62 Add test cases with a string including "boundary=" for rfc2045.haircut()

--- rfc2045/05-haircut_test.go
+++ rfc2045/05-haircut_test.go
@@ -8,6 +8,7 @@ package rfc2045
 //   | |  __/\__ \ |_ / / |  _ <|  _|| |___ / __/| |_| |__   _|__) |
 //   |_|\___||___/\__/_/  |_| \_\_|   \____|_____|\___/   |_||____/ 
 import "testing"
+import "strings"
 
 func TestHairCut(t *testing.T) {
 	fn := "sisimai/rfc2045.haircut"
@@ -29,6 +30,23 @@ Reason: 550 neko@dest.example.net... No such user`
 	cx++; if cv[0] != `text/plain; charset="utf-8"` { t.Errorf("%s(%s)[0] returns %s", fn, ae[:20], cv[0]) }
 	cx++; if cv[1] != "quoted-printable"            { t.Errorf("%s(%s)[1] returns %s", fn, ae[:20], cv[1]) }
 	cx++; if cv[2] == ""                            { t.Errorf("%s(%s)[2] returns empty", fn, ae[:20])     }
+
+	ae  = `Content-Description: "error-message"
+Content-Type: text/plain; charset="UTF-8"; boundary="nekochan"
+Content-Transfer-Encoding: quoted-printable
+
+This is the mail delivery agent at messagelabs.com.
+
+I was unable to deliver your message to the following addresses:
+
+neko@dest.example.net
+
+Reason: 550 neko@dest.example.net... No such user`
+	cv  = haircut(&ae, false)
+	cx++; if len(cv) == 0 { t.Errorf("%s(%s) returns an empty list", fn, ae[:20]) }
+	cx++; if strings.Contains(cv[0], "nekochan") == false { t.Errorf("%s(%s)[0] returns %s", fn, ae[:20], cv[0]) }
+	cx++; if cv[1] != "quoted-printable"                  { t.Errorf("%s(%s)[1] returns %s", fn, ae[:20], cv[1]) }
+	cx++; if cv[2] == ""                                  { t.Errorf("%s(%s)[2] returns empty", fn, ae[:20])     }
 
 	cw := "nekochan"
 	cv  = haircut(&cw, false)
