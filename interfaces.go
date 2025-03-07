fa54f4549b67310fb388af2d248804634e4c8c9c
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-08 05:10:13 +0900

    Implement sisimai.Reason() function for returning reason.Availables #71

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,15 @@
 // Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
+//  _ _ _         _     _                 _                     ___  __ 
+// | (_) |__  ___(_)___(_)_ __ ___   __ _(_)  ___  _ __ __ _   / (_)/ _|
+// | | | '_ \/ __| / __| | '_ ` _ \ / _` | | / _ \| '__/ _` | / /| | |_ 
+// | | | |_) \__ \ \__ \ | | | | | | (_| | || (_) | | | (_| |/ / | |  _|
+// |_|_|_.__/|___/_|___/_|_| |_| |_|\__,_|_(_)___/|_|  \__, /_/  |_|_|  
+// https://libsisimai.org/                             |___/            
 
 package sisimai
+import "libsisimai.org/sisimai/reason"
+
+// Reason() returns the list of bounce reasons sisimai can detect
+func Reason() map[string]string { return reason.Availables }
 
