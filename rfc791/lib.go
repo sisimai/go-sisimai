42f0c555dd1724be1ad5ebb9841ce70efd56ddeb
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-09 05:09:14 +0900

    #72 Initialize the ipv4a([]string) using make()

--- rfc791/lib.go
+++ rfc791/lib.go
@@ -37,7 +37,7 @@ func FindIPv4Address(argv1 *string) []string {
 	argv2 := *argv1
 	for _, e := range []string{"(", ")", "[", "]", ","} { argv2 = strings.ReplaceAll(argv2, e, " ") }
 
-	ipv4a := []string{}; for _, e := range strings.Split(argv2, " ") {
+	ipv4a := make([]string, 0, 4); for _, e := range strings.Split(argv2, " ") {
 		// Find a string including an IPv4 address
 		if strings.IndexByte(e, '.') < 0 { continue } // IPv4 address must include "." character
 		if IsIPv4Address(e) == false     { continue } // The string is an IPv4 address or not
