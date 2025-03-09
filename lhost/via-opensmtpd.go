2f25040fad855304a7a73969e1f14573cae4e5ab
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-10 05:30:04 +0900

    #72 Refer lhost.Indicators instead of calling lhost.INDICATORS()

--- lhost/via-opensmtpd.go
+++ lhost/via-opensmtpd.go
@@ -36,7 +36,6 @@ func init() {
 		}
 		if proceedsto == 0 { return sis.RisingUnderway{} }
 
-		indicators := INDICATORS()
 		boundaries := []string{"    Below is a copy of the original message:"}
 		startingof := map[string][]string{
 			// http://www.openbsd.org/cgi-bin/man.cgi?query=smtpd&sektion=8
@@ -108,10 +107,10 @@ func init() {
 			// previous line of the beginning of the original message.
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= indicators["deliverystatus"] }
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
 				continue
 			}
-			if readcursor & indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
 
 			//    Hi!
 			//
