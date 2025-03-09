0d819249e4f0bc573deebc0d005cd49b34df0afb
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-10 04:59:25 +0900

    #72 lhost.INDICATORS() has been removed and the Indicators was defined as map[string]uint8

--- lhost/lib.go
+++ lhost/lib.go
@@ -13,12 +13,5 @@ import "libsisimai.org/sisimai/sis"
 
 // Keep each function for decoding a bounce mail: % grep '^func init' ./lhost/via-*.go | wc -l
 var InquireFor = make(map[string]func(*sis.BeforeFact) sis.RisingUnderway, 36)
-
-// INDICATORS() returns flags for position variables used at MTA functions in sisimai/lhost.
-func INDICATORS() map[string]uint8 {
-	return map[string]uint8 {
-		"deliverystatus": (1 << 1),
-		"message-rfc822": (1 << 2),
-	}
-}
+var Indicators = map[string]uint8 {"deliverystatus": (1 << 1), "message-rfc822": (1 << 2)}
 
