ef01f0038c840dc732bf510d35b57b4c80d2e2d6
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-10 05:16:54 +0900

    #72 Refer lhost.Indicators instead of calling lhost.INDICATORS()

--- lhost/via-einsundeins.go
+++ lhost/via-einsundeins.go
@@ -24,7 +24,6 @@ func init() {
 		if strings.HasPrefix(bf.Headers["from"][0], `"Mail Delivery System"`) == false     { return sis.RisingUnderway{} }
 		if bf.Headers["subject"][0] != "Mail delivery failed: returning message to sender" { return sis.RisingUnderway{} }
 
-		indicators := INDICATORS()
 		boundaries := []string{"--- The header of the original message is following. ---"}
 		startingof := map[string][]string{
 			"message": []string{"This message was created automatically by mail delivery software"},
@@ -44,10 +43,10 @@ func init() {
 			// previous line of the beginning of the original message.
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= indicators["deliverystatus"] }
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
 				continue
 			}
-			if readcursor & indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
 
 			// The following address failed:
 			//
