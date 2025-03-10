79bd3cd4b02ead20fc7c399c92af38afb316bff7
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-10 16:19:30 +0900

    #74 Function comment improved

--- interfaces.go
+++ interfaces.go
@@ -8,8 +8,9 @@
 // https://libsisimai.org/                             |___/            
 
 package sisimai
+import "libsisimai.org/sisimai/sis"
 import "libsisimai.org/sisimai/reason"
 
-// Reason() returns the list of bounce reasons sisimai can detect
+// Reason returns the list of bounce reasons sisimai can detect.
 func Reason() map[string]string { return reason.Availables }
 
