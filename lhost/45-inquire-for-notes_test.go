27fdba709254dd361f784e43e22ab20bf870ff00
azumakuniyuki <azumakuniyuki@gmail.com>
2025-03-16 04:23:03 +0900

    #81 Update for the remove sis.RisingUnderway.Void() method

--- lhost/45-inquire-for-notes_test.go
+++ lhost/45-inquire-for-notes_test.go
@@ -35,7 +35,7 @@ func TestInquire45(t *testing.T) {
 		}
 
 		cv = InquireFor[en](bf)
-		cx++; if cv.Void() == true            { t.Errorf("%s(%s).Void() returns true", fn, e) }
+		cx++; if cv == nil                    { t.Errorf("%s(%s) returns nil", fn, e) }
 		cx++; if len(cv.Digest) < 1           { t.Errorf("%s(%s).Digest is empty", fn, e) }
 		cx++; if cv.Digest[0].Agent     != "" { t.Errorf("%s(%s).Digest.Agent is not empty", fn, e) }
 		cx++; if cv.Digest[0].Recipient == "" { t.Errorf("%s(%s).Digest.Recipient is empty", fn, e) }
