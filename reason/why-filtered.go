0765f60f405e04402a58ab329c85791fd7fdf23c
azumakuniyuki <azumakuniyuki@gmail.com>
2025-04-05 19:02:41 +0900

    Call smtp/command.ExeceptDATA for checking "Command" value

--- reason/why-filtered.go
+++ reason/why-filtered.go
@@ -11,6 +11,7 @@ import "strings"
 import "libsisimai.org/sisimai/v5/sis"
 import "libsisimai.org/sisimai/v5/moji"
 import "libsisimai.org/sisimai/v5/smtp/status"
+import "libsisimai.org/sisimai/v5/smtp/command"
 
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
@@ -64,8 +65,8 @@ func init() {
 			// The value of "Reason" is not "filtered" when the value of "fo.Command" is an SMTP
 			// command to be sent before the SMTP DATA command because all the MTAs read the headers
 			// and the entire message body after the DATA command.
-			if moji.EqualsAny(fo.Command, []string{"CONN", "EHLO", "HELO", "MAIL", "RCPT"}) { return false }
-			if IncludedIn["Filtered"](issuedcode) || IncludedIn["UserUnknown"](issuedcode)  { return true  }
+			if moji.EqualsAny(fo.Command, command.ExceptDATA) { return false }
+			if IncludedIn["Filtered"](issuedcode) || IncludedIn["UserUnknown"](issuedcode) { return true }
 		}
 		return false
 	}
