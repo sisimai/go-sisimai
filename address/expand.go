406f96b5e4dd89a8dab05e624d9d57d861a50bd5
azumakuniyuki <azumakuniyuki@gmail.com>
2025-05-03 07:14:08 +0900

    #105 Use moji.Select() for expanding a VERP address, an aliaas

--- address/expand.go
+++ address/expand.go
@@ -8,6 +8,7 @@
 
 package address
 import "strings"
+import "libsisimai.org/sisimai/v5/moji"
 import "libsisimai.org/sisimai/v5/rfc5322"
 
 // ExpandVERP gets the original recipient address from a VERP address.
@@ -20,13 +21,8 @@ func ExpandVERP(email string) string {
 	if strings.IndexByte(email, '@')  == -1   { return "" }
 	if rfc5322.IsQuotedAddress(email) == true { return "" } // Do not expand "neko+cat=example.jp"@example.org
 
-	local := strings.SplitN(email, "@", 2)[0]
-	pluss := strings.IndexByte(local, '+'); if pluss < 1                  { return "" }
-	equal := strings.IndexByte(local, '='); if equal < 1 || pluss > equal { return "" }
-	lsize := len(local);      if pluss >= lsize - 1 || equal >= lsize - 1 { return "" }
-	xverp := strings.Replace(strings.SplitN(local, "+", 2)[1], "=", "@", 1)
-
-	if rfc5322.IsEmailAddress(xverp) { return xverp }
+	cv := moji.Select(email, "+", "@", 0);  if cv == ""                   { return "" }
+	cw := strings.Replace(cv, "=", "@", 1); if rfc5322.IsEmailAddress(cw) { return cw }
 	return ""
 }
 
@@ -39,6 +35,6 @@ func ExpandAlias(email string) string {
 	if email == "" || strings.IndexByte(email, '+') < 1 { return "" }
 	if rfc5322.IsEmailAddress(email)  == false          { return "" }
 	if rfc5322.IsQuotedAddress(email) == true           { return "" } // Do not expand "neko+cat"@example.org
-	return email[0:strings.IndexByte(email, '+')] + "@" + strings.SplitN(email, "@", 2)[1]
+	return moji.Select(moji.LHS + email, "", "+", 0) + "@" + moji.Select(email + moji.RHS, "@", "", 1)
 }
 
