520f585e64036e3085e4c5a6bb6ea7e10788ae33
azumakuniyuki <azumakuniyuki@gmail.com>
2025-05-25 05:44:53 +0900

    #123 Use slices.Lines() instead of strings.Split()

--- lhost/via-exchange2003.go
+++ lhost/via-exchange2003.go
@@ -97,9 +97,10 @@ func init() {
 		anotherone := []string{""}  // Keeping another error messages
 		msexchange := []bool{false} // Flag, true if "MSEXCH:" text has been appeared
 
-		for _, e := range(strings.Split(emailparts[0], "\n")) {
+		for e := range strings.Lines(emailparts[0]) {
 			// Read error messages and delivery status lines from the head of the email to the
 			// previous line of the beginning of the original message.
+			e  = strings.TrimRight(e, "\n\r")
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
 				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
