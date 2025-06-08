f148a5d12514a1b22f3e5d3535378a15e350be3f
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-09 05:36:37 +0900

    Refer HereIsDeliveryStatus to check the indicator value #131

--- lhost/via-qmail.go
+++ lhost/via-qmail.go
@@ -171,10 +171,10 @@ func init() {
 			e  = strings.TrimRight(e, "\n\r")
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if moji.ContainsAny(e, startingof["message"]) { readcursor |= Indicators["deliverystatus"] }
+				if moji.ContainsAny(e, startingof["message"]) { readcursor |= HereIsDeliveryStatus }
 				continue
 			}
-			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & HereIsDeliveryStatus == 0 || e == "" { continue }
 
 			// <kijitora@example.jp>:
 			// 192.0.2.153 does not like recipient.
