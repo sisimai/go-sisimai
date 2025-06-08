451b392631b98768ae0e90f6bf7734bb6538aa94
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-09 05:36:34 +0900

    Refer HereIsDeliveryStatus to check the indicator value #131

--- lhost/via-activehunter.go
+++ lhost/via-activehunter.go
@@ -37,10 +37,10 @@ func init() {
 			// previous line of the beginning of the original message.
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= HereIsDeliveryStatus }
 				continue
 			}
-			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & HereIsDeliveryStatus == 0 || e == "" { continue }
 
 			//  ----- The following addresses had permanent fatal errors -----
 			//
