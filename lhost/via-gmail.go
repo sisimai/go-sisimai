b0b50050e811871f65290ccf724ab22fe13a2073
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-09 05:36:35 +0900

    Refer HereIsDeliveryStatus to check the indicator value #131

--- lhost/via-gmail.go
+++ lhost/via-gmail.go
@@ -174,9 +174,9 @@ func init() {
 			e  = strings.TrimRight(e, "\n\r")
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= HereIsDeliveryStatus }
 			}
-			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & HereIsDeliveryStatus == 0 || e == "" { continue }
 
 			// Technical details of permanent failure:=20
 			// Google tried to deliver your message, but it was rejected by the recipient =
