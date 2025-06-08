9a04666ec7f5592b89466f207d926dc542c03039
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-09 05:36:38 +0900

    Refer HereIsDeliveryStatus to check the indicator value #131

--- lhost/via-x6.go
+++ lhost/via-x6.go
@@ -41,9 +41,9 @@ func init() {
 			e  = strings.TrimRight(e, "\n\r")
 			if readcursor == 0 {
 				// Beginning of the bounce message or message/delivery-status part
-				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= Indicators["deliverystatus"] }
+				if strings.HasPrefix(e, startingof["message"][0]) { readcursor |= HereIsDeliveryStatus }
 			}
-			if readcursor & Indicators["deliverystatus"] == 0 || e == "" { continue }
+			if readcursor & HereIsDeliveryStatus == 0 || e == "" { continue }
 
 			// We had trouble delivering your message. Full details follow:
 			//
