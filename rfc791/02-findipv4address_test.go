f1b4b8dc8fc39af55c0c3858a6416322f6fbac6f
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-14 19:11:15 +0900

    rfc791.FIndIPv4Address() receives the 1st argument as a string

--- rfc791/02-findipv4address_test.go
+++ rfc791/02-findipv4address_test.go
@@ -21,16 +21,16 @@ func TestFindIPv4Address(t *testing.T) {
 	ce := []string{"365.31.7.1", "a.b.c.d", ""}
 
 	for _, e := range ae {
-		cv := FindIPv4Address(&e.text)
+		cv := FindIPv4Address(e.text)
 		cx++; if len(cv) == 0          { t.Errorf("%s(%s) returns empty", fn, e.text)     } 
 		cx++; if cv[0] != e.expected   { t.Errorf("%s(%s) returns %s", fn, e.text, cv[0]) }
 		cx++; if !IsIPv4Address(cv[0]) { t.Errorf("IsIPv4Address(%s) is false", cv[0])    }
 	}
 	for _, e := range ce {
-		cv := FindIPv4Address(&e)
+		cv := FindIPv4Address(e)
 		cx++; if len(cv) != 0 { t.Errorf("%s(%s) returns %v", fn, e, cv)  } 
 	}
-	cx++; if cv := FindIPv4Address(nil); len(cv) > 0 { t.Errorf("%s(nil) returns %v", fn, cv) }
+	cx++; if cv := FindIPv4Address(""); len(cv) > 0 { t.Errorf("%s(nil) returns %v", fn, cv) }
 
 	t.Logf("The number of tests = %d", cx)
 }
