8d492b8e5ec7431c42f7a5c06855c53a71f0f680
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-16 06:58:34 +0900

    Call moji.AlignedAny() to check error message patterns #136

--- reason/why-toomanyconn.go
+++ reason/why-toomanyconn.go
@@ -10,6 +10,7 @@
 package reason
 import "strings"
 import "libsisimai.org/sisimai/v5/sis"
+import "libsisimai.org/sisimai/v5/moji"
 import "libsisimai.org/sisimai/v5/smtp/status"
 
 func init() {
@@ -37,9 +38,7 @@ func init() {
 			"trop de connexions, ",
 			"we have already made numerous attempts to deliver this message",
 		}
-
-		for _, v := range index { if strings.Contains(argv1, v) { return true }}
-		return false
+		return moji.ContainsAny(argv1, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
