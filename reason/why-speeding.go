0df18cc070cfd73162876d014e3a84a9cae68fa4
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-16 06:58:33 +0900

    Call moji.AlignedAny() to check error message patterns #136

--- reason/why-speeding.go
+++ reason/why-speeding.go
@@ -10,6 +10,7 @@
 package reason
 import "strings"
 import "libsisimai.org/sisimai/v5/sis"
+import "libsisimai.org/sisimai/v5/moji"
 
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
@@ -25,9 +26,7 @@ func init() {
 			"please try again slower",
 			"receiving mail at a rate that prevents additional messages from being delivered",
 		}
-
-		for _, v := range index { if strings.Contains(argv1, v) { return true }}
-		return false
+		return moji.ContainsAny(argv1, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
