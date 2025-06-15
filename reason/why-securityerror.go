df35e44809288738df34264ee7ace64a178d6468
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-16 06:58:33 +0900

    Call moji.AlignedAny() to check error message patterns #136

--- reason/why-securityerror.go
+++ reason/why-securityerror.go
@@ -8,7 +8,6 @@
 //                                   |___/                           
 
 package reason
-import "strings"
 import "libsisimai.org/sisimai/v5/sis"
 import "libsisimai.org/sisimai/v5/moji"
 
@@ -42,10 +41,7 @@ func init() {
 			[]string{"domain ", " is a dead domain"},
 			[]string{"user ", " is not authorized to perform ses:sendrawemail on resource"},
 		}
-
-		for _, v := range index { if strings.Contains(argv1, v) { return true }}
-		for _, v := range pairs { if moji.Aligned(argv1, v)     { return true }}
-		return false
+		return moji.ContainsAny(argv1, index) || moji.AlignedAny(argv1, pairs)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
