dd0af965655c4d78488a83d886f1079cdf866649
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-16 06:58:30 +0900

    Call moji.AlignedAny() to check error message patterns #136

--- reason/why-contenterror.go
+++ reason/why-contenterror.go
@@ -9,6 +9,7 @@
 package reason
 import "strings"
 import "libsisimai.org/sisimai/v5/sis"
+import "libsisimai.org/sisimai/v5/moji"
 import "libsisimai.org/sisimai/v5/smtp/status"
 
 func init() {
@@ -31,9 +32,7 @@ func init() {
 			"this message contain improperly-formatted binary content",
 			"this message contain text that uses unnecessary base64 encoding",
 		}
-
-		for _, v := range index { if strings.Contains(argv1, v) { return true }}
-		return false
+		return moji.ContainsAny(argv1, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
