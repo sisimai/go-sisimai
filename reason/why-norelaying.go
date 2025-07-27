3fadf01cd9f661ae8b1619ba59e4e8cf413d86cc
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:52:57 +0900

    [Document] Function comments in reason/why-norelaying.go improved

--- reason/why-norelaying.go
+++ reason/why-norelaying.go
@@ -18,7 +18,7 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["NoRelaying"] = func(argv1 string) bool {
 		if argv1 == "" { return false }
 
@@ -50,9 +50,9 @@ func init() {
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["NoRelaying"] = func(fo *sis.Fact) bool {
 		if fo         == nil          { return false }
 		if fo.Reason  == "norelaying" { return true  }
