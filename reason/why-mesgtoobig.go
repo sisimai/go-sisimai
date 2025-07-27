6581ef9a54d29a6d584ebeb906d05a4da52c37c3
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:50:27 +0900

    [Document] Function comments in reason/why-mesgtoobig.go improved

--- reason/why-mesgtoobig.go
+++ reason/why-mesgtoobig.go
@@ -18,7 +18,7 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["MesgTooBig"] = func(argv1 string) bool {
 		if argv1 == "" { return false }
 
@@ -42,9 +42,9 @@ func init() {
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["MesgTooBig"] = func(fo *sis.Fact) bool {
 		// Delivery status code points "mesgtoobig".
 		// Status: 5.3.4
