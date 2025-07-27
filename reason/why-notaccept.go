02303963eafedd7b53f95030c861aa5048450b4c
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:53:48 +0900

    [Document] Function comments in reason/why-notaccept.go improved

--- reason/why-notaccept.go
+++ reason/why-notaccept.go
@@ -18,7 +18,7 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["NotAccept"] = func(argv1 string) bool {
 		if argv1 == "" { return false }
 
@@ -37,9 +37,9 @@ func init() {
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["NotAccept"] = func(fo *sis.Fact) bool {
 		if fo        == nil                                      { return false }
 		if fo.Reason == "notaccept"                              { return true  }
