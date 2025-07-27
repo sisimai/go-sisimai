e6991e3de2f5b7aa8f191471f65c38bbf756f204
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:38:16 +0900

    [Document] Function comments in reason/why-exceedlimit.go improved

--- reason/why-exceedlimit.go
+++ reason/why-exceedlimit.go
@@ -17,7 +17,7 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["ExceedLimit"] = func(argv1 string) bool {
 		if argv1 == "" { return false }
 		index := []string{"message header size exceeds limit", "message too large"}
@@ -26,9 +26,9 @@ func init() {
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["ExceedLimit"] = func(fo *sis.Fact) bool {
 		// Status: 5.2.3
 		// Diagnostic-Code: SMTP; 552 5.2.3 Message size exceeds fixed maximum message size
