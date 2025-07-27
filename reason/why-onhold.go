aa79dd0d1ecc9e0bf5e2abe40daaca7fb1dac300
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:55:23 +0900

    [Document] Function comments in reason/why-onhold.go improved

--- reason/why-onhold.go
+++ reason/why-onhold.go
@@ -15,14 +15,14 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["OnHold"] = func(argv1 string) bool    { return false }
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["OnHold"] = func(fo *sis.Fact) bool {
 		if fo        == nil                           { return false }
 		if fo.Reason == "onhold"                      { return true  }
