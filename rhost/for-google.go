0068fc805a409e5f3d2797f7a7f000387d473c33
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 06:27:21 +0900

    [Document] Function comments in rhost/for-google.go improved

--- rhost/for-google.go
+++ rhost/for-google.go
@@ -16,9 +16,9 @@ import "libsisimai.org/sisimai/v5/smtp/status"
 func init() {
 	// ReturnedBy[*] detects the reason of the bounce returned by this email service.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (string):       Bounce reason name or an empty string
+	//     - (string): Bounce reason name or an empty string.
 	ReturnedBy["Google"] = func(fo *sis.Fact) string {
 		// - https://support.google.com/a/answer/3726730?hl=en
 		if fo == nil || fo.DiagnosticCode == ""    { return "" }
