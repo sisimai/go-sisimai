8d196f937bbaf4e106f9e57652a6b49f1040724a
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 05:59:48 +0900

    [Document] Function comments in reason/why-speeding.go improved

--- reason/why-speeding.go
+++ reason/why-speeding.go
@@ -17,7 +17,7 @@ func init() {
 	//   Arguments:
 	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
-	//     - (bool):         true if the argument includes one or more error message pattern
+	//     - (bool): true if the argument includes one or more error message pattern.
 	IncludedIn["Speeding"] = func(argv1 string) bool {
 		if argv1 == "" { return false }
 
@@ -31,9 +31,9 @@ func init() {
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (bool):         true if a reason is the reason defined in this file
+	//     - (bool): true if a reason is the reason defined in this file.
 	ProbesInto["Speeding"] = func(fo *sis.Fact) bool {
 		// Action: failed
 		// Status: 4.7.1
