56157251d33ce634f970b5830a553a19f36a56b3
azumakuniyuki <azumakuniyuki@gmail.com>
2025-07-28 06:31:32 +0900

    [Document] Function comments in rhost/for-messagelabs.go improved

--- rhost/for-messagelabs.go
+++ rhost/for-messagelabs.go
@@ -14,9 +14,9 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// ReturnedBy[*] detects the reason of the bounce returned by this email service.
 	//   Arguments:
-	//     - fo (*sis.Fact): Decoded data in progress
+	//     - fo (*sis.Fact): Decoded data in progress.
 	//   Returns:
-	//     - (string):       Bounce reason name or an empty string
+	//     - (string): Bounce reason name or an empty string.
 	ReturnedBy["MessageLabs"] = func(fo *sis.Fact) string {
 		// - https://www.broadcom.com/products/cybersecurity/email
 		if fo == nil || fo.DiagnosticCode == "" { return "" }
