f775153ed5f67c741f21e55acac334d8f7c166d6
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-06 17:15:48 +0900

    #130 Change the receiver name in mail.readMailbox(): this => ee

--- mail/mbox.go
+++ mail/mbox.go
@@ -16,29 +16,29 @@ import "strings"
 //   Returns:
 //     - (*string): Contents of each email in the UNIX mbox one by one.
 //     - (error):   Occurred error.
-func(this *EmailEntity) readMailbox() (*string, error) {
-	if this.offset >= this.Size { return nil, io.EOF } // The method has been completed to read the mbox
-	if this.handle == nil {
+func(ee *EmailEntity) readMailbox() (*string, error) {
+	if ee.offset >= ee.Size { return nil, io.EOF } // The method has been completed to read the mbox
+	if ee.handle == nil {
 		// Open the UNIX mbox, and read at the offset position
-		filehandle, nyaan := os.Open(this.Path); if nyaan != nil { return nil, nyaan }
-		this.handle = filehandle // Successfully opened the mbox
+		filehandle, nyaan := os.Open(ee.Path); if nyaan != nil { return nil, nyaan }
+		ee.handle = filehandle // Successfully opened the mbox
 	}
 
-	seekoffset := int64(this.offset);              if this.offset  < 0 { seekoffset = 0 }
-	_, nyaan   := this.handle.Seek(seekoffset, 0); if nyaan != nil { return nil, nyaan  }
-	lineending := 0;                               if this.newline > 2 { lineending = 1 }
+	seekoffset := int64(ee.offset);              if ee.offset  < 0 { seekoffset = 0 }
+	_, nyaan   := ee.handle.Seek(seekoffset, 0); if nyaan != nil { return nil, nyaan  }
+	lineending := 0;                             if ee.newline > 2 { lineending = 1 }
 	readbuffer := strings.Builder{}; readbuffer.Grow(4096)
 	emailblock := ""
 	thisheight := 0
 
-	unixmboxio := bufio.NewScanner(this.handle); for unixmboxio.Scan() {
+	unixmboxio := bufio.NewScanner(ee.handle); for unixmboxio.Scan() {
 		// Read the UNIX mbox until the EOF
 		e := unixmboxio.Text()
 		if strings.HasPrefix(e, "From ") && readbuffer.Len() > 0 {
 			// - The line is a UNIX From line such as "From MAILER-DAEMON Fri Feb  2 18:30:22 2018"
 			// - This UNIX From line is the beginning of the second or later email message
-			emailblock   = readbuffer.String(); readbuffer.Reset()
-			this.offset += len(emailblock) + (thisheight * lineending)
+			emailblock = readbuffer.String(); readbuffer.Reset()
+			ee.offset += len(emailblock) + (thisheight * lineending)
 			break
 		}
 		thisheight += 1
@@ -47,9 +47,9 @@ func(this *EmailEntity) readMailbox() (*string, error) {
 
 	if readbuffer.Len() > 0 {
 		// The last email message in the UNIX mbox
-		emailblock   = readbuffer.String()
-		this.offset += readbuffer.Len() + (thisheight * lineending)
-		if nyaan := this.handle.Close(); nyaan != nil { return nil, nyaan }
+		emailblock = readbuffer.String()
+		ee.offset += readbuffer.Len() + (thisheight * lineending)
+		if nyaan  := ee.handle.Close(); nyaan != nil { return nil, nyaan }
 	}
 	return &emailblock, nil
 }
