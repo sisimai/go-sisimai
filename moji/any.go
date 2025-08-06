40f749206bf3713a7e519d12fbdb9ac7e8021d24
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-07 02:24:54 +0900

    #138 Change the argument names in moji/any.go

--- moji/any.go
+++ moji/any.go
@@ -10,43 +10,43 @@
 package moji
 import "strings"
 
-// ContainsAny checks whether any element in argv2 is included in argv1 or not. (argv1 > argv2)
+// ContainsAny checks whether any element in "list" is included in "text" or not. (text > list)
 //   Arguments:
-//     - argv1 (string):   String containing any substring listed in argv2.
-//     - argv2 ([]string): List of strings to find in argv1.
+//     - text (string):   String containing any substring listed in "list".
+//     - list ([]string): List of strings to find in "text".
 //   Returns:
-//     - (bool): true if one or more string in argv2 was found in argv1.
-func ContainsAny(argv1 string, argv2 []string) bool {
-	if argv1 == "" || len(argv2) == 0 { return false }
+//     - (bool): true if one or more string in "list" was found in "text".
+func ContainsAny(text string, list []string) bool {
+	if text == "" || len(list) == 0 { return false }
 
 	// It works like `grep { index($e, $_) > -1 } @list` in Perl
-	for _, e := range argv2 { if strings.Contains(argv1, e) { return true } }
+	for _, e := range list { if strings.Contains(text, e) { return true } }
 	return false
 }
 
-// HasPrefixAny checks whether any alement in argv2 starts with the argv1 or not.
+// HasPrefixAny checks whether any alement in "list" starts with the "text" or not.
 //   Arguments:
-//     - argv1 (string):   String containing any substring listed in argv2.
-//     - argv2 ([]string): List of strings to find in argv1.
+//     - text (string):   String containing any substring listed in "list".
+//     - list ([]string): List of strings to find in "text".
 //   Returns:
-//     - (bool): true if the string in argv1 starts with any string listed in argv2.
-func HasPrefixAny(argv1 string, argv2 []string) bool {
-	if argv1 == "" || len(argv2) == 0 { return false }
+//     - (bool): true if the string in "text" starts with any string listed in "list".
+func HasPrefixAny(text string, list []string) bool {
+	if text == "" || len(list) == 0 { return false }
 
 	// It works like `grep { index($e, $_) == 0 } @list` in Perl
-	for _, e := range argv2 { if strings.HasPrefix(argv1, e) { return true } }
+	for _, e := range list { if strings.HasPrefix(text, e) { return true } }
 	return false
 }
 
 // AlignedAny checks if each slice of the 2nd argument is aligned in the 1st argument or not.
 //   Arguments:
-//     - argv1 (string):     String to be checked such as "I am a cat. I have, as yet, no name.".
-//     - argv2 ([][]string): List including the ordered strings such as `[][]string{[]striing{"cat", "yet"}}`.
+//     - text (string):     String to be checked such as "I am a cat. I have, as yet, no name.".
+//     - list ([][]string): List including the ordered strings such as `[][]string{[]striing{"cat", "yet"}}`.
 //   Returns:
-//     - (bool): true if the all strings are ordered in argv1, false otherwise.
-func AlignedAny(argv1 string, argv2 [][]string) bool {
-	if argv1 == "" || len(argv2) == 0 { return false }
-	for _, e := range argv2 { if p := Aligned(argv1, e); p == true { return true } }
+//     - (bool): true if the all strings are ordered in "text", false otherwise.
+func AlignedAny(text string, list [][]string) bool {
+	if text == "" || len(list) == 0 { return false }
+	for _, e := range list { if p := Aligned(text, e); p == true { return true } }
 	return false
 }
 
