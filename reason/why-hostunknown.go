5348cd494313bea42ac04d14c1c1dfdb07246a42
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-08 07:29:43 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-hostunknown.go
+++ reason/why-hostunknown.go
@@ -15,11 +15,11 @@ import "libsisimai.org/sisimai/v5/smtp/status"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["HostUnknown"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["HostUnknown"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			"domain does not exist",
@@ -41,7 +41,7 @@ func init() {
 		pairs := [][]string{
 			[]string{"553 ", " does not exist"},
 		}
-		return moji.ContainsAny(argv1, index) || moji.AlignedAny(argv1, pairs)
+		return moji.ContainsAny(mesg, index) || moji.AlignedAny(mesg, pairs)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
