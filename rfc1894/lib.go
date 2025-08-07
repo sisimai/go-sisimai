696fd6391cc506cbda511ae9f44505de4c7d305e
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-08 07:35:41 +0900

    "line" is the better 40fcf0b

--- rfc1894/lib.go
+++ rfc1894/lib.go
@@ -37,14 +37,14 @@ var ActionList = map[string]bool{
 
 // Label returns a lower-cased field name.
 //   Arguments:
-//     - text (string): A line including field and value defined in RFC3464.
+//     - line (string): A line including field and value defined in RFC3464.
 //   Returns:
 //     - (string): Lower-cased field name as a label.
 //   See:
 //     - https://datatracker.ietf.org/doc/html/rfc3464
-func Label(text string) string {
-	if text == "" || strings.IndexByte(text, ':') < 0 { return "" }
-	return strings.ToLower(moji.Select(moji.LHS + text, "", ":", 0))
+func Label(line string) string {
+	if line == "" || strings.IndexByte(line, ':') < 0 { return "" }
+	return strings.ToLower(moji.Select(moji.LHS + line, "", ":", 0))
 }
 
 // Match checks that the argument matches with a field defined in RFC3464 or not.
