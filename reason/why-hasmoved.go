83f0538b4ef9afa11f327ec2d6da7133843387da
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-08 07:29:25 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-hasmoved.go
+++ reason/why-hasmoved.go
@@ -14,13 +14,13 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern
-	IncludedIn["HasMoved"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["HasMoved"] = func(mesg string) bool {
+		if mesg == "" { return false }
 		index := []string{" has been replaced by "}
-		return moji.ContainsAny(argv1, index)
+		return moji.ContainsAny(mesg, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
