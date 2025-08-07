37ac3bdaf6d86d6b88042ddb38706cce0077aac1
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-08 07:26:46 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-delivered.go
+++ reason/why-delivered.go
@@ -10,7 +10,7 @@ package reason
 import "libsisimai.org/sisimai/v5/sis"
 
 func init() {
-	IncludedIn["Delivered"] = func(argv1 string) bool { return false }
+	IncludedIn["Delivered"] = func(mesg string)  bool { return false }
 	ProbesInto["Delivered"] = func(fo *sis.Fact) bool { return false }
 }
 
