2a91d3816efb8dcece929071256c37742e10fd1d
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-09 07:18:11 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-mesgtoobig.go
+++ reason/why-mesgtoobig.go
@@ -16,11 +16,11 @@ import "libsisimai.org/sisimai/v5/smtp/status"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["MesgTooBig"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["MesgTooBig"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			"exceeded maximum inbound message size",
@@ -37,7 +37,7 @@ func init() {
 			"size limit",
 			"taille limite du message atteinte",
 		}
-		return moji.ContainsAny(argv1, index)
+		return moji.ContainsAny(mesg, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
