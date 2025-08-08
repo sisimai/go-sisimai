a253967d92fc5610fcb85dee8a15b337c5f1e706
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-09 07:23:06 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-systemfull.go
+++ reason/why-systemfull.go
@@ -14,17 +14,17 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["SystemFull"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["SystemFull"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			"mail system full",
 			"requested mail action aborted: exceeded storage allocation", // MS Exchange
 		}
-		return moji.ContainsAny(argv1, index)
+		return moji.ContainsAny(mesg, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
