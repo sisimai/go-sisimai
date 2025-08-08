516d654f4ee80a75402f1a79daabc644f4694507
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-09 07:21:18 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-securityerror.go
+++ reason/why-securityerror.go
@@ -14,11 +14,11 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["SecurityError"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["SecurityError"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			"account not subscribed to ses",
@@ -41,7 +41,7 @@ func init() {
 			[]string{"domain ", " is a dead domain"},
 			[]string{"user ", " is not authorized to perform ses:sendrawemail on resource"},
 		}
-		return moji.ContainsAny(argv1, index) || moji.AlignedAny(argv1, pairs)
+		return moji.ContainsAny(mesg, index) || moji.AlignedAny(mesg, pairs)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
