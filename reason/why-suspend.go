a8baad772c7bf71fadf386c0cba4c8758d15b1cd
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-09 07:22:30 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-suspend.go
+++ reason/why-suspend.go
@@ -15,11 +15,11 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["Suspend"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["Suspend"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			" is currently suspended",
@@ -46,7 +46,7 @@ func init() {
 			"user suspended", // http://mail.163.com/help/help_spam_16.htm
 			"vdelivermail: account is locked email bounced",
 		}
-		return moji.ContainsAny(argv1, index)
+		return moji.ContainsAny(mesg, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
