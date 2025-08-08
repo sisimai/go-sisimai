f1a3923e598b1800b8fd1cf434b6a09d862b2264
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-09 07:18:52 +0900

    #138 The argument name of IncludedIn[*]() function has been changed: argv1 => mesg

--- reason/why-norelaying.go
+++ reason/why-norelaying.go
@@ -16,11 +16,11 @@ import "libsisimai.org/sisimai/v5/moji"
 func init() {
 	// IncludedIn[*] Try to check the argument string includes any of the strings in the error message pattern.
 	//   Arguments:
-	//     - argv1 (string): Does the string include any of the strings listed in the pattern?
+	//     - mesg (string): Does the string include any of the strings listed in the pattern?
 	//   Returns:
 	//     - (bool): true if the argument includes one or more error message pattern.
-	IncludedIn["NoRelaying"] = func(argv1 string) bool {
-		if argv1 == "" { return false }
+	IncludedIn["NoRelaying"] = func(mesg string) bool {
+		if mesg == "" { return false }
 
 		index := []string{
 			"as a relay",
@@ -45,7 +45,7 @@ func init() {
 			"unable to relay for",
 			"we don't handle mail for",
 		}
-		return moji.ContainsAny(argv1, index)
+		return moji.ContainsAny(mesg, index)
 	}
 
 	// ProbesInto[*] checks the bounce reason is the reason defined in this file or not.
