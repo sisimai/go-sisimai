4f65148ab32de95b389f2263445db49d00b9c01f
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-16 05:51:29 +0900

    c5b8c4e Remove the 2nd argument from the test code calling rfc5322.Headers()

--- lhost/32-inquire-for-googlegroups_test.go
+++ lhost/32-inquire-for-googlegroups_test.go
@@ -33,7 +33,7 @@ func TestInquire32(t *testing.T) {
 		eo, _ := mail.ReadMessage(strings.NewReader(ee))
 		bo, _ := io.ReadAll(eo.Body)
 		bf    := &sis.BeforeFact{
-			Headers: rfc5322.Headers(&eo.Header, false),
+			Headers: rfc5322.Headers(&eo.Header),
 			Payload: string(bo),
 		}
 
