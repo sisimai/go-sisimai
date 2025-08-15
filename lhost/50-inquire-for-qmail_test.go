eae38ff40fa9e2ae28dbb3a5b6d02c3df1135b85
azumakuniyuki <azumakuniyuki@gmail.com>
2025-08-16 05:53:31 +0900

    c5b8c4e Remove the 2nd argument from the test code calling rfc5322.Headers()

--- lhost/50-inquire-for-qmail_test.go
+++ lhost/50-inquire-for-qmail_test.go
@@ -34,7 +34,7 @@ func TestInquire50(t *testing.T) {
 		eo, _ := mail.ReadMessage(strings.NewReader(ee))
 		bo, _ := io.ReadAll(eo.Body)
 		bf    := &sis.BeforeFact{
-			Headers: rfc5322.Headers(&eo.Header, false),
+			Headers: rfc5322.Headers(&eo.Header),
 			Payload: string(bo),
 		}
 
