ea92022a23dc86068b71d1d46a1b3635ee620aac
azumakuniyuki <azumakuniyuki@gmail.com>
2025-10-06 07:41:36 +0900

    Implement test code for lhost/via-mimecast.go #153 #200

---
+++ lhost/19-inquire-for-mimecast_test.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// This software is distributed under The BSD 2-Clause License.
+package lhost
+
+//  _____         _      ___ _               _   
+// |_   _|__  ___| |_   / / | |__   ___  ___| |_ 
+//   | |/ _ \/ __| __| / /| | '_ \ / _ \/ __| __|
+//   | |  __/\__ \ |_ / / | | | | | (_) \__ \ |_ 
+//   |_|\___||___/\__/_/  |_|_| |_|\___/|___/\__|
+import "testing"
+import "os"
+import "io"
+import "strings"
+import "net/mail"
+import "path/filepath"
+import "libsisimai.org/sisimai/v5/sis"
+import "libsisimai.org/sisimai/v5/rfc5322"
+
+func TestInquire19(t *testing.T) {
+	en := "Mimecast"
+	fn := "lhost.InquireFor[" + en + "]"
+	ae := []string{"lhost-mimecast-01"}
+	cv := InquireFor[en](nil) 
+	cx := 0
+	cx++; if cv != nil { t.Errorf("%s(nil) did not return nil", fn) }
+
+	for _, e := range ae {
+		ef    := filepath.Join("..", "set-of-emails", "maildir", "bsd", e); ef += ".eml"
+		eb, _ := os.ReadFile(ef); ee := string(eb)
+		eo, _ := mail.ReadMessage(strings.NewReader(ee))
+		bo, _ := io.ReadAll(eo.Body)
+		bf    := &sis.BeforeFact{
+			Headers: rfc5322.Headers(&eo.Header),
+			Payload: string(bo),
+		}
+
+		cv = InquireFor[en](bf)
+		cx++; if cv == nil                    { t.Fatalf("%s(%s) returns nil", fn, e) }
+		cx++; if len(cv.Digest) < 1           { t.Errorf("%s(%s).Digest is empty", fn, e) }
+		cx++; if cv.Digest[0].Agent     != "" { t.Errorf("%s(%s).Digest.Agent is not empty", fn, e) }
+		cx++; if cv.Digest[0].Recipient == "" { t.Errorf("%s(%s).Digest.Recipient is empty", fn, e) }
+	}
+
+	t.Logf("The number of tests = %d", cx)
+}
+
