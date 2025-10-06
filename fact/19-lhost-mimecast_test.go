01c014db2fe021e9055aa2d41814c9102207c161
azumakuniyuki <azumakuniyuki@gmail.com>
2025-10-07 06:58:46 +0900

    #153 #200 Register lhost-mimecast/1006 as a private sample

--- fact/19-lhost-mimecast_test.go
+++ fact/19-lhost-mimecast_test.go
@@ -23,6 +23,7 @@ func TestLhostMimecast(t *testing.T) {
 		{{"1003", 1, "5.1.1",   "",    "userunknown",      true, ""}},
 		{{"1004", 1, "5.4.14",  "554", "networkerror",    false, ""}},
 		{{"1005", 1, "5.1.1",   "550", "userunknown",      true, ""}},
+		{{"1006", 1, "5.7.54",  "550", "norelaying",      false, ""}},
 	}; EngineTest(t, "Mimecast", secretlist, false)
 }
 
