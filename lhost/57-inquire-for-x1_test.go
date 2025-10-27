4d692f4d71b61ddaaffad5a7cbc2c79e8165f2c1
azumakuniyuki <azumakuniyuki@gmail.com>
2025-10-28 05:30:23 +0900

    #223 Follow #215, Add 2 new public samples for lhost/x1

--- lhost/57-inquire-for-x1_test.go
+++ lhost/57-inquire-for-x1_test.go
@@ -20,7 +20,7 @@ func TestInquire57(t *testing.T) {
 	en := "X1"
 	fn := "lhost.InquireFor[" + en + "]"
 	ae := []string{
-		"lhost-x1-01", "lhost-x1-02", 
+		"lhost-x1-01", "lhost-x1-02", "lhost-x1-03", "lhost-x1-04", 
 	}
 	cv := InquireFor[en](nil) 
 	cx := 0
@@ -41,7 +41,6 @@ func TestInquire57(t *testing.T) {
 		cx++; if len(cv.Digest) < 1           { t.Errorf("%s(%s).Digest is empty", fn, e) }
 		cx++; if cv.Digest[0].Agent     != "" { t.Errorf("%s(%s).Digest.Agent is not empty", fn, e) }
 		cx++; if cv.Digest[0].Recipient == "" { t.Errorf("%s(%s).Digest.Recipient is empty", fn, e) }
-		cx++; if cv.RFC822 == ""              { t.Errorf("%s(%s).RFC822 is empty (%d)", fn, e, len(cv.RFC822)) }
 	}
 
 	t.Logf("The number of tests = %d", cx)
