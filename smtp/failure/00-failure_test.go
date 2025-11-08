69320ec873f65468b5aba50238cca13a9aee3406
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-09 05:18:16 +0900

    #244 eb.ReREJE has been renamed to eb.ReFROM

--- smtp/failure/00-failure_test.go
+++ smtp/failure/00-failure_test.go
@@ -13,7 +13,7 @@ import "libsisimai.org/sisimai/v5/eb"
 
 var SoftBounce = []string{
 	eb.ReAUTH, eb.ReREPU, eb.ReBLOC, eb.ReBODY, eb.ReXLIM, eb.ReEXPR, eb.ReTTLS, eb.ReFILT, eb.ReFULL,
-	eb.ReUNIX, eb.ReSIZE, eb.ReNETW, eb.ReNRFC, eb.RePOLI, eb.ReRELA, eb.ReREJE, eb.ReQPTR, eb.ReSECU,
+	eb.ReUNIX, eb.ReSIZE, eb.ReNETW, eb.ReNRFC, eb.RePOLI, eb.ReRELA, eb.ReFROM, eb.ReQPTR, eb.ReSECU,
 	eb.ReSPAM, eb.ReFAST, eb.ReSUPP, eb.ReQUIT, eb.ReSYNT, eb.ReSYSE, eb.ReSYSF, eb.ReCONN, eb.ReEXEC,
 	eb.Re___0, eb.Re___1,
 }
