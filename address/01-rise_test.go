485070360845d2d10b70477e033b7c05c6b8267a
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-13 14:37:16 +0900

    #261 Try to increase the coverage of address/find.go: 68.94%

--- address/01-rise_test.go
+++ address/01-rise_test.go
@@ -32,6 +32,8 @@ var TestEmailAddrs = []struct {testname string; argument string; expected string
 	{"", "mari <mari@example.jp> mari@host.int", "mari@example.jp", "mari mari@host.int", ""},
 	{"", "8suke@example.gov (Mayuge-Neko)", "8suke@example.gov", "8suke@example.gov", "(Mayuge-Neko)"},
 	{"", "Shibainu, Hachibe. (Harima-no-kami) 8be@example.gov", "8be@example.gov", "Shibainu, Hachibe. 8be@example.gov", "(Harima-no-kami)"},
+	{"", "neko(nyaan.jp)chan@example.jp", "nekochan@example.jp", "nekochan@example.jp", "(nyaan.jp)"},
+	{"", `"neko(nyaan.jp)chan"@example.jp`, `"nekochan"@example.jp`, `"nekochan"@example.jp`, "(nyaan.jp)"},
 	{"", "neko(miaow)chan@example.jp", "nekochan@example.jp", "nekochan@example.jp", "(miaow)"},
 	{"", "(miaow)neko@example.jp", "neko@example.jp", "neko@example.jp", "(miaow)"},
 	{"", "neko(miaow)@example.jp", "neko@example.jp", "neko@example.jp", "(miaow)"},
