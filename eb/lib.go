1e02105abb7a7b3a8dc67c34f81185719ccf158c
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-20 14:14:44 +0900

    Register eb/lib.go for providing the description of the package

---
+++ eb/lib.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// This software is distributed under The BSD 2-Clause License.
+//       _     
+//   ___| |__  
+//  / _ \ '_ \ 
+// |  __/ |_) |
+//  \___|_.__/ 
+
+// Package "eb" provides constants for the email bounce.
+package eb
+
