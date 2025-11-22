73303bd78bb8d8e801b1f197eba3f53f65361e18
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-22 09:45:40 +0900

    #28 Check that the file (*mesg) is a plain text or not in sisimai.Rise() function

--- libsisimai.go
+++ libsisimai.go
@@ -78,14 +78,14 @@ func Rise(path string, args *siba.DecodingArgs) ([]siba.Fact, []siba.NotDecoded)
 			}
 		} else {
 			// Read and decode each email file as a string
-			if emailthing.Size == 0 {
+			if emailthing.Size == 0 || (*mesg)[0] > 127 {
 				// Reason for this check:
 				//   While mail.Rise() already validates the overall size of the input source, this
 				//   specific condition addresses the case where an individual email message extracted
 				//   from a multi-message source (like a UNIX mbox) might be empty.
 				//   This acts as a necessary secondary validation to prevent issues during further
 				//   processing of empty messages.
-				ce := *siba.MakeNotDecoded("the email file is empty", true); ce.Email(emailthing.Path)
+				ce := *siba.MakeNotDecoded("the file may not be a text file", true); ce.Email(emailthing.Path)
 				notdecoded = append(notdecoded, ce)
 				continue
 			}
