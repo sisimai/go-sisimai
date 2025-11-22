0ce7e2fe6069d5d004fe4208d383ba3152aef6d0
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-22 09:45:56 +0900

    #28 Check that the file (*mesg) is a plain text or not in fact.Rise() function

--- fact/rise.go
+++ fact/rise.go
@@ -38,9 +38,9 @@ import "libsisimai.org/sisimai/v5/smtp/failure"
 //     - ([]siba.Fact):       List of successfully decoded bounce messages.
 //     - ([]siba.NotDecoded): List of occurred errors.
 func Rise(email *string, origin string, args *siba.DecodingArgs) ([]siba.Fact, []siba.NotDecoded) {
-	if email == nil || len(*email) < 1 {
-		// The email message is empty
-		ce := *siba.MakeNotDecoded("email file is empty", true); ce.Email(origin)
+	if email == nil || len(*email) < 1 || (*email)[0] > 127 {
+		// The email message is empty or is a binary
+		ce := *siba.MakeNotDecoded("the file may not be a text file", true); ce.Email(origin)
 		return nil, []siba.NotDecoded{ce}
 	}
 
