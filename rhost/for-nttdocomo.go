bc63dd2c5c401cc4c38c70d34a9913a876bb7663
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-24 09:32:29 +0900

    Code and comment improved

--- rhost/for-nttdocomo.go
+++ rhost/for-nttdocomo.go
@@ -27,100 +27,82 @@ func init() {
 			eb.ReCONN: []string{"552 too many recipients"},
 			eb.ReUSER: []string{"550 unknown user"},
 		}
-		statuscode := fo.DeliveryStatus
-		issuedcode := strings.ToLower(fo.DiagnosticCode)
-		reasontext := ""
+		statuscode, issuedcode := fo.DeliveryStatus, strings.ToLower(fo.DiagnosticCode)
 
 		// Check the value of Status: field, an SMTP Reply Code, and the SMTP Command
-		if statuscode == "5.1.1" || statuscode == "5.0.911" {
-			//    ----- Transcript of session follows -----
-			// ... while talking to mfsmax.docomo.ne.jp.:
-			// >>> RCPT To:<***@docomo.ne.jp>
-			// <<< 550 Unknown user ***@docomo.ne.jp
-			// 550 5.1.1 <***@docomo.ne.jp>... User unknown
-			// >>> DATA
-			// <<< 503 Bad sequence of commands
-			reasontext = eb.ReUSER
+		//    ----- Transcript of session follows -----
+		// ... while talking to mfsmax.docomo.ne.jp.:
+		// >>> RCPT To:<***@docomo.ne.jp>
+		// <<< 550 Unknown user ***@docomo.ne.jp
+		// 550 5.1.1 <***@docomo.ne.jp>... User unknown
+		// >>> DATA
+		// <<< 503 Bad sequence of commands
+		// ------------------------------------------------------------------------------------
+		//
+		//    ----- The following addresses had permanent fatal errors -----
+		// <***@docomo.ne.jp>
+		// (reason: 550 Unknown user ***@docomo.ne.jp)
+		// 
+		//    ----- Transcript of session follows -----
+		// ... while talking to mfsmax.docomo.ne.jp.:
+		// >>> DATA
+		// <<< 550 Unknown user ***@docomo.ne.jp
+		// 554 5.0.0 Service unavailable
+		// ...
+		// Final-Recipient: RFC822; ***@docomo.ne.jp
+		// Action: failed
+		// Status: 5.2.0
+		if statuscode == "5.1.1" || statuscode == "5.0.911" { return eb.ReUSER }
+		if statuscode == "5.2.0"                            { return eb.ReFILT }
 
-		} else if statuscode == "5.2.0" {
-			//    ----- The following addresses had permanent fatal errors -----
-			// <***@docomo.ne.jp>
-			// (reason: 550 Unknown user ***@docomo.ne.jp)
-			// 
-			//    ----- Transcript of session follows -----
-			// ... while talking to mfsmax.docomo.ne.jp.:
-			// >>> DATA
-			// <<< 550 Unknown user ***@docomo.ne.jp
-			// 554 5.0.0 Service unavailable
-			// ...
-			// Final-Recipient: RFC822; ***@docomo.ne.jp
-			// Action: failed
-			// Status: 5.2.0
-			reasontext = eb.ReFILT
-
-		} else {
+		for e := range messagesof {
 			// The value of "Diagnostic-Code:" field is not empty
-			for e := range messagesof {
-				// - The key name is a bounce reason name
-				// - https://github.com/sisimai/go-sisimai/issues/64
-				// - After March 12, 2025, if an error message contains "550 Unknown user", the
-				//   bounce reason will be definitively "userunknown". This is because NTT DOCOMO
-				//   no longer rejects emails via SMTP for domain-specific rejection or specified
-				//   reception filters.
-				if moji.ContainsAny(issuedcode, messagesof[e]) { return e }
-			}
+			// - The key name is a bounce reason name
+			// - https://github.com/sisimai/go-sisimai/issues/64
+			// - After March 12, 2025, if an error message contains "550 Unknown user", the
+			//   bounce reason will be definitively "userunknown". This is because NTT DOCOMO
+			//   no longer rejects emails via SMTP for domain-specific rejection or specified
+			//   reception filters.
+			if moji.ContainsAny(issuedcode, messagesof[e]) { return e }
 		}
-		if reasontext != "" { return reasontext }
 
 		// A bounce reason did not decide from the status code, the error message.
 		if statuscode == "5.0.0" {
-			// Status: 5.0.0
-			if fo.Command == eb.CeRCPT {
-				// Your message to the following recipients cannot be delivered:
-				//
-				// <***@docomo.ne.jp>:
-				// mfsmax.docomo.ne.jp [203.138.181.112]:
-				// >>> RCPT TO:<***@docomo.ne.jp>
-				// <<< 550 Unknown user ***@docomo.ne.jp
-				// ...
-				//
-				// Final-Recipient: rfc822; ***@docomo.ne.jp
-				// Action: failed
-				// Status: 5.0.0
-				// Remote-MTA: dns; mfsmax.docomo.ne.jp [203.138.181.112]
-				// Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
-				reasontext = eb.ReUSER
-
-			} else if fo.Command == eb.CeDATA {
-				// <***@docomo.ne.jp>: host mfsmax.docomo.ne.jp[203.138.181.240] said:
-				// 550 Unknown user ***@docomo.ne.jp (in reply to end of DATA
-				// command)
-				// ...
-				// Final-Recipient: rfc822; ***@docomo.ne.jp
-				// Original-Recipient: rfc822;***@docomo.ne.jp
-				// Action: failed
-				// Status: 5.0.0
-				// Remote-MTA: dns; mfsmax.docomo.ne.jp
-				// Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
-				reasontext = eb.ReFROM
-
-			// } else {
-				// Rejected by other SMTP commands: AUTH, MAIL,
-				//   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
-				//   https://github.com/sisimai/go-sisimai/issues からご連絡ねがいます。
-				//
-				//   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
-				//   please open an issue at https://github.com/sisimai/go-sisimai/issues .
-			}
-		} else {
-			// Status: field is neither 5.0.0 nor values defined in code above
-			//   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
-			//   https://github.com/sisimai/go-sisimai/issues からご連絡ねがいます。
+			// Your message to the following recipients cannot be delivered:
 			//
-			//   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
-			//   please open an issue at https://github.com/sisimai/go-sisimai .
+			// <***@docomo.ne.jp>:
+			// mfsmax.docomo.ne.jp [203.138.181.112]:
+			// >>> RCPT TO:<***@docomo.ne.jp>
+			// <<< 550 Unknown user ***@docomo.ne.jp
+			// ...
+			//
+			// Final-Recipient: rfc822; ***@docomo.ne.jp
+			// Action: failed
+			// Status: 5.0.0
+			// Remote-MTA: dns; mfsmax.docomo.ne.jp [203.138.181.112]
+			// Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
+			// ------------------------------------------------------------------------------------
+			// <***@docomo.ne.jp>: host mfsmax.docomo.ne.jp[203.138.181.240] said:
+			// 550 Unknown user ***@docomo.ne.jp (in reply to end of DATA command)
+			// ...
+			// Final-Recipient: rfc822; ***@docomo.ne.jp
+			// Original-Recipient: rfc822;***@docomo.ne.jp
+			// Action: failed
+			// Status: 5.0.0
+			// Remote-MTA: dns; mfsmax.docomo.ne.jp
+			// Diagnostic-Code: smtp; 550 Unknown user ***@docomo.ne.jp
+			if fo.Command == eb.CeRCPT { return eb.ReUSER }
+			if fo.Command == eb.CeDATA { return eb.ReFROM }
 		}
-		return reasontext
+
+		// 1. Rejected by other SMTP commands: AUTH, MAIL, or any SMTP commands except RCPT and DATA.
+		// 2. The Status: field is neither 5.0.0 nor values defined in code above
+		//   もしもこのブロックを通過するNTTドコモからのエラーメッセージを見つけたら
+		//   https://github.com/sisimai/go-sisimai/issues からご連絡ねがいます。
+		//
+		//   If you found a error message from mfsmax.docomo.ne.jp which passes this block,
+		//   please open an issue at https://github.com/sisimai/go-sisimai/issues page.
+		return ""
 	}
 }
 
