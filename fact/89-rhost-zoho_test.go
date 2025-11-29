a825dc92e95905390b004dc4527f14c1182e6e21
azumakuniyuki <azumakuniyuki@gmail.com>
2025-11-30 06:10:24 +0900

    #288 Implement test codes for rhost/for-zoho.go using public samples

---
+++ fact/89-rhost-zoho_test.go
@@ -0,0 +1,25 @@
+// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// This software is distributed under The BSD 2-Clause License.
+package fact
+
+//  _____         _      __    _               _                _           
+// |_   _|__  ___| |_   / / __| |__   ___  ___| |_     _______ | |__   ___  
+//   | |/ _ \/ __| __| / / '__| '_ \ / _ \/ __| __|___|_  / _ \| '_ \ / _ \ 
+//   | |  __/\__ \ |_ / /| |  | | | | (_) \__ \ ||_____/ / (_) | | | | (_) |
+//   |_|\___||___/\__/_/ |_|  |_| |_|\___/|___/\__|   /___\___/|_| |_|\___/ 
+import "testing"
+
+func TestRhostZoho(t *testing.T) {
+	publiclist := [][]IsExpected{
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
+		{{"01",   1, "5.1.1",   "550", "userunknown",      true, ""}},
+		{{"02",   1, "5.7.7",   "554", "policyviolation", false, ""}},
+		{{"03",   1, "5.7.1",   "554", "rejected",        false, ""}},
+		{{"04",   1, "5.4.1",   "",    "rejected",        false, ""}},
+	}; EngineTest(t, "Zoho", publiclist, true)
+
+	secretlist := [][]IsExpected{
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
+	}; EngineTest(t, "Zoho", secretlist, false)
+}
+
