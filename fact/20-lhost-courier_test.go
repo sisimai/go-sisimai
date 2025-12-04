22eb2398a39ad49e646dffc9eb68658b8b6ffde8
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:41:50 +0900

    #299 Add the results of 'Toxic' field for 20-lhost-courier_test.go

--- fact/20-lhost-courier_test.go
+++ fact/20-lhost-courier_test.go
@@ -11,27 +11,27 @@ import "testing"
 
 func TestLhostCourier(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"01",   1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"02",   1, "5.0.0",   "550", "filtered",        false, ""}},
-		{{"03",   1, "5.7.1",   "550", "rejected",        false, ""}},
-		{{"04",   1, "5.0.0",   "",    "hostunknown",      true, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"01",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"02",   1, "5.0.0",   "550", "filtered",        false,  true, ""}},
+		{{"03",   1, "5.7.1",   "550", "rejected",        false, false, ""}},
+		{{"04",   1, "5.0.0",   "",    "hostunknown",      true,  true, ""}},
 	}; EngineTest(t, "Courier", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"1001", 1, "5.0.0",   "550", "userunknown",      true, ""}},
-		{{"1002", 1, "5.0.0",   "550", "userunknown",      true, ""}},
-		{{"1003", 1, "5.7.1",   "550", "rejected",        false, ""}},
-		{{"1004", 1, "5.0.0",   "550", "userunknown",      true, ""}},
-		{{"1005", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1006", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1007", 1, "5.0.0",   "550", "userunknown",      true, ""}},
-		{{"1008", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1009", 1, "5.0.0",   "550", "filtered",        false, ""}},
-		{{"1010", 1, "5.7.1",   "550", "rejected",        false, ""}},
-		{{"1011", 1, "5.0.0",   "",    "hostunknown",      true, ""}},
-		{{"1012", 1, "5.0.0",   "",    "hostunknown",      true, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"1001", 1, "5.0.0",   "550", "userunknown",      true,  true, ""}},
+		{{"1002", 1, "5.0.0",   "550", "userunknown",      true,  true, ""}},
+		{{"1003", 1, "5.7.1",   "550", "rejected",        false, false, ""}},
+		{{"1004", 1, "5.0.0",   "550", "userunknown",      true,  true, ""}},
+		{{"1005", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1006", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1007", 1, "5.0.0",   "550", "userunknown",      true,  true, ""}},
+		{{"1008", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1009", 1, "5.0.0",   "550", "filtered",        false,  true, ""}},
+		{{"1010", 1, "5.7.1",   "550", "rejected",        false, false, ""}},
+		{{"1011", 1, "5.0.0",   "",    "hostunknown",      true,  true, ""}},
+		{{"1012", 1, "5.0.0",   "",    "hostunknown",      true,  true, ""}},
 	}; EngineTest(t, "Courier", secretlist, false)
 }
 
