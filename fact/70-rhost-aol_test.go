3ac9cad62cba82353ad2300c26b0de268f08f160
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:51:47 +0900

    #299 Add the results of 'Toxic' field for 70-rhost-aol_test.go

--- fact/70-rhost-aol_test.go
+++ fact/70-rhost-aol_test.go
@@ -11,18 +11,18 @@ import "testing"
 
 func TestRhostAol(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"01",   1, "5.4.4",   "",    "hostunknown",      true, ""}},
-		{{"02",   1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"03",   1, "5.2.2",   "550", "mailboxfull",     false, ""},
-		 {"03",   2, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"04",   1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"05",   1, "5.4.4",   "",    "hostunknown",      true, ""}},
-		{{"06",   1, "5.4.4",   "",    "notaccept",        true, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"01",   1, "5.4.4",   "",    "hostunknown",      true,  true, ""}},
+		{{"02",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"03",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
+		 {"03",   2, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"04",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"05",   1, "5.4.4",   "",    "hostunknown",      true,  true, ""}},
+		{{"06",   1, "5.4.4",   "",    "notaccept",        true,  true, ""}},
 	}; EngineTest(t, "Aol", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
 	}; EngineTest(t, "Aol", secretlist, false)
 }
 
