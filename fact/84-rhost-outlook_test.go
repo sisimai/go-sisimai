56d3695e204237433f65f71c3f8acdd49e741819
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:56:14 +0900

    #299 Add the results of 'Toxic' field for 84-rhost-outlook_test.go

--- fact/84-rhost-outlook_test.go
+++ fact/84-rhost-outlook_test.go
@@ -11,20 +11,20 @@ import "testing"
 
 func TestRhostOutlook(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"01",   1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"02",   1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"03",   1, "5.5.0",   "554", "hostunknown",      true, ""}},
-		{{"04",   1, "5.1.1",   "550", "userunknown",      true, ""},
-		 {"04",   2, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"06",   1, "4.4.7",   "",    "expired",         false, ""}},
-		{{"07",   1, "4.4.7",   "",    "expired",         false, ""}},
-		{{"08",   1, "5.5.0",   "550", "userunknown",      true, ""}},
-		{{"09",   1, "5.5.0",   "550", "requireptr",      false, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"01",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"02",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"03",   1, "5.5.0",   "554", "hostunknown",      true,  true, ""}},
+		{{"04",   1, "5.1.1",   "550", "userunknown",      true,  true, ""},
+		 {"04",   2, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"06",   1, "4.4.7",   "",    "expired",         false, false, ""}},
+		{{"07",   1, "4.4.7",   "",    "expired",         false, false, ""}},
+		{{"08",   1, "5.5.0",   "550", "userunknown",      true,  true, ""}},
+		{{"09",   1, "5.5.0",   "550", "requireptr",      false, false, ""}},
 	}; EngineTest(t, "Outlook", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
 	}; EngineTest(t, "Outlook", secretlist, false)
 }
 
