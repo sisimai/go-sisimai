4a80055985a6b92532d7c225498fc2069656e7d3
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:40:41 +0900

    #299 Add the results of 'Toxic' field for 14-lhost-aol_test.go

--- fact/14-lhost-aol_test.go
+++ fact/14-lhost-aol_test.go
@@ -11,27 +11,27 @@ import "testing"
 
 func TestLhostAol(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
 	}; EngineTest(t, "Aol", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"1001", 1, "5.4.4",   "",    "hostunknown",      true, ""}},
-		{{"1002", 1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"1003", 1, "5.2.2",   "550", "mailboxfull",     false, ""},
-		 {"1003", 2, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1004", 1, "5.2.2",   "550", "mailboxfull",     false, ""},
-		 {"1004", 2, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1005", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1006", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1007", 1, "5.2.2",   "552", "mailboxfull",     false, ""}},
-		{{"1008", 1, "5.7.1",   "554", "filtered",        false, ""}},
-		{{"1009", 1, "5.7.1",   "554", "policyviolation", false, ""}},
-		{{"1010", 1, "5.7.1",   "554", "filtered",        false, ""}},
-		{{"1011", 1, "5.7.1",   "554", "filtered",        false, ""}},
-		{{"1012", 1, "5.2.2",   "552", "mailboxfull",     false, ""}},
-		{{"1013", 1, "5.2.2",   "552", "mailboxfull",     false, ""}},
-		{{"1014", 1, "5.1.1",   "550", "userunknown",      true, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"1001", 1, "5.4.4",   "",    "hostunknown",      true,  true, ""}},
+		{{"1002", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"1003", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
+		 {"1003", 2, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1004", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
+		 {"1004", 2, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1005", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1006", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1007", 1, "5.2.2",   "552", "mailboxfull",     false,  true, ""}},
+		{{"1008", 1, "5.7.1",   "554", "filtered",        false,  true, ""}},
+		{{"1009", 1, "5.7.1",   "554", "policyviolation", false, false, ""}},
+		{{"1010", 1, "5.7.1",   "554", "filtered",        false,  true, ""}},
+		{{"1011", 1, "5.7.1",   "554", "filtered",        false,  true, ""}},
+		{{"1012", 1, "5.2.2",   "552", "mailboxfull",     false,  true, ""}},
+		{{"1013", 1, "5.2.2",   "552", "mailboxfull",     false,  true, ""}},
+		{{"1014", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
 	}; EngineTest(t, "Aol", secretlist, false)
 }
 
