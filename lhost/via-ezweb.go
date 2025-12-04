779d1d826bd3f568a1491c0cb37bcb26dcd9f623
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-04 17:44:08 +0900

    #299 Set true into DeliveryMatter.Toxic when the Reason is "Filtered"

--- lhost/via-ezweb.go
+++ lhost/via-ezweb.go
@@ -134,6 +134,7 @@ func init() {
 				// Content-Type: text/plain; ..., X-SPASIGN: NG (spamghetti, au by EZweb)
 				// Filtered recipient returns message that include 'X-SPASIGN' header
 				e.Reason = eb.ReFILT
+				e.Toxic  = true
 
 			} else {
 				// There is no X-SPASIGN header or the value of the header is not "NG"
