d44559df8f1114c98d75e5f8154e0ac4281eb62e
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:50:07 +0900

    #299 Add the results of 'Toxic' field for 60-lhost-x4_test.go

--- fact/60-lhost-x4_test.go
+++ fact/60-lhost-x4_test.go
@@ -12,37 +12,37 @@ import "testing"
 
 func TestLhostX4(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"01",   1, "5.0.922", "",    "mailboxfull",     false, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"01",   1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
 	}; EngineTest(t, "X4", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"1001", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1002", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1003", 1, "5.1.2",   "",    "hostunknown",      true, ""}},
-		{{"1004", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1005", 1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"1006", 1, "5.1.1",   "",    "userunknown",      true, ""}},
-		{{"1007", 1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"1008", 1, "5.0.921", "550", "suspend",         false, ""}},
-		{{"1009", 1, "5.1.1",   "",    "userunknown",      true, ""}},
-		{{"1010", 1, "5.1.2",   "",    "hostunknown",      true, ""}},
-		{{"1011", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1012", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1013", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1014", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1015", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1016", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1018", 1, "5.1.1",   "",    "userunknown",      true, ""}},
-		{{"1019", 1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"1020", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1022", 1, "5.1.1",   "",    "userunknown",      true, ""}},
-		{{"1023", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1024", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
-		{{"1025", 1, "5.1.1",   "",    "userunknown",      true, ""}},
-		{{"1026", 1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"1027", 1, "5.0.922", "",    "mailboxfull",     false, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"1001", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1002", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1003", 1, "5.1.2",   "",    "hostunknown",      true,  true, ""}},
+		{{"1004", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1005", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1006", 1, "5.1.1",   "",    "userunknown",      true,  true, ""}},
+		{{"1007", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1008", 1, "5.0.921", "550", "suspend",         false,  true, ""}},
+		{{"1009", 1, "5.1.1",   "",    "userunknown",      true,  true, ""}},
+		{{"1010", 1, "5.1.2",   "",    "hostunknown",      true,  true, ""}},
+		{{"1011", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1012", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1013", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1014", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1015", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1016", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1018", 1, "5.1.1",   "",    "userunknown",      true,  true, ""}},
+		{{"1019", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1020", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1022", 1, "5.1.1",   "",    "userunknown",      true,  true, ""}},
+		{{"1023", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1024", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
+		{{"1025", 1, "5.1.1",   "",    "userunknown",      true,  true, ""}},
+		{{"1026", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1027", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
 	}; EngineTest(t, "X4", secretlist, false)
 }
 
