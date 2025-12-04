8b7e08819bb8ca6b4006bc061b28badbc79c9244
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-05 06:46:25 +0900

    #299 Add the results of 'Toxic' field for 39-lhost-mailru_test.go

--- fact/39-lhost-mailru_test.go
+++ fact/39-lhost-mailru_test.go
@@ -12,34 +12,34 @@ import "testing"
 
 func TestLhostMailRu(t *testing.T) {
 	publiclist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"01",   1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"02",   1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"03",   1, "5.2.2",   "550", "mailboxfull",     false, ""},
-		 {"03",   2, "5.2.1",   "550", "userunknown",      true, ""}},
-		{{"04",   1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"05",   1, "5.0.932", "",    "notaccept",        true, ""}},
-		{{"06",   1, "5.0.912", "",    "hostunknown",      true, ""}},
-		{{"07",   1, "5.0.910", "550", "filtered",        false, ""}},
-		{{"08",   1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"09",   1, "5.1.8",   "501", "rejected",        false, ""}},
-		{{"10",   1, "4.0.947", "",    "expired",         false, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"01",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"02",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"03",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
+		 {"03",   2, "5.2.1",   "550", "userunknown",      true,  true, ""}},
+		{{"04",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"05",   1, "5.0.932", "",    "notaccept",        true,  true, ""}},
+		{{"06",   1, "5.0.912", "",    "hostunknown",      true,  true, ""}},
+		{{"07",   1, "5.0.910", "550", "filtered",        false,  true, ""}},
+		{{"08",   1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"09",   1, "5.1.8",   "501", "rejected",        false, false, ""}},
+		{{"10",   1, "4.0.947", "",    "expired",         false, false, ""}},
 	}; EngineTest(t, "MailRu", publiclist, true)
 
 	secretlist := [][]IsExpected{
-		// Label, Index, Status, ReplyCode, Reason, HardBounce, AnotherOne
-		{{"1001", 1, "5.0.911", "",    "userunknown",      true, ""}},
-		{{"1002", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1003", 1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"1004", 1, "5.2.2",   "550", "mailboxfull",     false, ""},
-		 {"1004", 2, "5.2.1",   "550", "userunknown",      true, ""}},
-		{{"1005", 1, "5.0.910", "",    "filtered",        false, ""}},
-		{{"1006", 1, "5.2.2",   "550", "mailboxfull",     false, ""}},
-		{{"1007", 1, "5.0.911", "",    "userunknown",      true, ""}},
-		{{"1008", 1, "5.1.1",   "550", "userunknown",      true, ""}},
-		{{"1009", 1, "5.0.910", "550", "filtered",        false, ""}},
-		{{"1010", 1, "5.0.911", "550", "userunknown",      true, ""}},
-		{{"1011", 1, "5.1.8",   "501", "rejected",        false, ""}},
+		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
+		{{"1001", 1, "5.0.911", "",    "userunknown",      true,  true, ""}},
+		{{"1002", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1003", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"1004", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
+		 {"1004", 2, "5.2.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1005", 1, "5.0.910", "",    "filtered",        false, false, ""}},
+		{{"1006", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
+		{{"1007", 1, "5.0.911", "",    "userunknown",      true,  true, ""}},
+		{{"1008", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
+		{{"1009", 1, "5.0.910", "550", "filtered",        false,  true, ""}},
+		{{"1010", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1011", 1, "5.1.8",   "501", "rejected",        false, false, ""}},
 	}; EngineTest(t, "MailRu", secretlist, false)
 }
 
