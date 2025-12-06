28d5cc87e60dc5aa53eebf57c56fefcf16b34ed6
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-06 13:35:37 +0900

    Fix

--- reason/find.go
+++ reason/find.go
@@ -67,6 +67,7 @@ func anotherone(fo *siba.Fact) string {
 			// Trying to match with other patterns in reason/why-*.go
 			if IncludedIn[e](issuedcode) == true { return e }
 		}
+		if reasontext != "" { return reasontext }
 
 		if forsubject := ""; len(fo.DeliveryStatus) > 3 {
 			// Check the first 3 characters of fo.DeliveryStatus
