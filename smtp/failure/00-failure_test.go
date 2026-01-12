fcc2bbe03be946ea28e9f8d8a5e632e7a19d1b75
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-12 17:35:57 +0900

    Fix the copyright year: 2025-2026

--- smtp/failure/00-failure_test.go
+++ smtp/failure/00-failure_test.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2026 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2025-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package failure
 
