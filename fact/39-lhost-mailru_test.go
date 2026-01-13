f788cc9e842d63aa2350c2642be32c9004b516b9
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-14 07:52:48 +0900

    #322 Update to the new internal code for mail.ru

--- fact/39-lhost-mailru_test.go
+++ fact/39-lhost-mailru_test.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2025-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package fact
 
@@ -18,27 +18,27 @@ func TestLhostMailRu(t *testing.T) {
 		{{"03",   1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
 		 {"03",   2, "5.2.1",   "550", "userunknown",      true,  true, ""}},
 		{{"04",   1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
-		{{"05",   1, "5.0.932", "",    "notaccept",        true,  true, ""}},
-		{{"06",   1, "5.0.912", "",    "hostunknown",      true,  true, ""}},
-		{{"07",   1, "5.0.910", "550", "filtered",        false,  true, ""}},
-		{{"08",   1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"05",   1, "5.9.215", "",    "notaccept",        true,  true, ""}},
+		{{"06",   1, "5.9.212", "",    "hostunknown",      true,  true, ""}},
+		{{"07",   1, "5.9.210", "550", "filtered",        false,  true, ""}},
+		{{"08",   1, "5.9.213", "550", "userunknown",      true,  true, ""}},
 		{{"09",   1, "5.1.8",   "501", "rejected",        false, false, ""}},
 		{{"10",   1, "4.0.947", "",    "expired",         false, false, ""}},
 	}; EngineTest(t, "MailRu", publiclist, true)
 
 	secretlist := [][]IsExpected{
 		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
-		{{"1001", 1, "5.0.911", "",    "userunknown",      true,  true, ""}},
+		{{"1001", 1, "5.9.213", "",    "userunknown",      true,  true, ""}},
 		{{"1002", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
 		{{"1003", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
 		{{"1004", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""},
 		 {"1004", 2, "5.2.1",   "550", "userunknown",      true,  true, ""}},
-		{{"1005", 1, "5.0.910", "",    "filtered",        false, false, ""}},
+		{{"1005", 1, "5.9.210", "",    "filtered",        false, false, ""}},
 		{{"1006", 1, "5.2.2",   "550", "mailboxfull",     false,  true, ""}},
-		{{"1007", 1, "5.0.911", "",    "userunknown",      true,  true, ""}},
+		{{"1007", 1, "5.9.213", "",    "userunknown",      true,  true, ""}},
 		{{"1008", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
-		{{"1009", 1, "5.0.910", "550", "filtered",        false,  true, ""}},
-		{{"1010", 1, "5.0.911", "550", "userunknown",      true,  true, ""}},
+		{{"1009", 1, "5.9.210", "550", "filtered",        false,  true, ""}},
+		{{"1010", 1, "5.9.213", "550", "userunknown",      true,  true, ""}},
 		{{"1011", 1, "5.1.8",   "501", "rejected",        false, false, ""}},
 	}; EngineTest(t, "MailRu", secretlist, false)
 }
