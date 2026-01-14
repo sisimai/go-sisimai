bd79d29108fa26a70719a1f7a6e6c5b4b4e38fbe
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-15 05:39:31 +0900

    #322 Update to the new internal code for EinsUndEins

--- fact/23-lhost-einsundeins_test.go
+++ fact/23-lhost-einsundeins_test.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2025-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 package fact
 
@@ -12,15 +12,15 @@ import "testing"
 func TestLhostEinsUndEins(t *testing.T) {
 	publiclist := [][]IsExpected{
 		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
-		{{"02",   1, "5.0.934", "",    "emailtoolarge",   false, false, ""}},
+		{{"02",   1, "5.9.161", "",    "emailtoolarge",   false, false, ""}},
 		{{"03",   1, "5.2.0",   "550", "spamdetected",    false, false, ""}},
 	}; EngineTest(t, "EinsUndEins", publiclist, true)
 
 	secretlist := [][]IsExpected{
 		// Label, Index, Status, ReplyCode, Reason, HardBounce, Toxic, AnotherOne
-		{{"1001", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
-		{{"1002", 1, "5.0.922", "",    "mailboxfull",     false, false, ""}},
-		{{"1003", 1, "5.0.934", "",    "emailtoolarge",   false, false, ""}},
+		{{"1001", 1, "5.9.220", "",    "mailboxfull",     false, false, ""}},
+		{{"1002", 1, "5.9.220", "",    "mailboxfull",     false, false, ""}},
+		{{"1003", 1, "5.9.161", "",    "emailtoolarge",   false, false, ""}},
 		{{"1004", 1, "5.1.1",   "550", "userunknown",      true,  true, ""}},
 		{{"1005", 1, "5.4.1",   "550", "userunknown",      true,  true, ""}},
 		{{"1006", 1, "5.4.1",   "550", "userunknown",      true,  true, ""}},
