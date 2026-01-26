91c14434e087873e4fac0f5858cea7d516c75375
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-26 12:54:55 +0900

    #329 Merge error message patterns in the HostUnknown reason

--- reason/why-hostunknown.go
+++ reason/why-hostunknown.go
@@ -23,7 +23,6 @@ func init() {
 		if mesg == "" { return false }
 
 		index := []string{
-			"domain does not exist",
 			"domain is not reachable",
 			"domain must exist",
 			"host or domain name not found",
@@ -33,14 +32,12 @@ func init() {
 			"name or service not known",
 			"no such domain",
 			"recipient address rejected: unknown domain name",
-			"recipient domain must exist",
-			"the account or domain may not exist",
 			"unknown host",
-			"unroutable address",
-			"unrouteable address",
 		}
 		pairs := [][]string{
 			[]string{"553 ", " does not exist"},
+			[]string{"domain ", "not exist"},
+			[]string{"unrout", "able ", "address"},
 		}
 		return moji.ContainsAny(mesg, index) || moji.AlignedAny(mesg, pairs)
 	}
