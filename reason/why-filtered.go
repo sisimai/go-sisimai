4f33ccdc05a8e5bd7a385f69dd1beb90ad8096aa
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-28 11:12:40 +0900

    2026 014413a

--- reason/why-filtered.go
+++ reason/why-filtered.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2024-2025 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2024-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 //  _____ _ _ _                    _ 
 // |  ___(_) | |_ ___ _ __ ___  __| |
