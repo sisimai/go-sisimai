9355f1c6434552bddc7eb58c952ae05f694e596c
azumakuniyuki <azumakuniyuki@gmail.com>
2026-02-01 08:38:42 +0900

    The next version is v5.6.0

--- libsisimai.go
+++ libsisimai.go
@@ -23,7 +23,7 @@ import sisimbox "libsisimai.org/sisimai/v5/mail"
 import sisifact "libsisimai.org/sisimai/v5/fact"
 
 const libname string = "sisimai"
-const version string = "5.5.0"
+const version string = "5.6.0"
 const patchlv uint8  = 0
 type  CallbackArg0 = siba.CallbackArg0
 type  CallbackArg1 = siba.CallbackArg1
