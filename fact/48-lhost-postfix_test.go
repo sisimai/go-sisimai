50008cf468bfc042035ab96157be6425e754a6d2
azumakuniyuki <azumakuniyuki@gmail.com>
2026-02-12 08:55:23 +0900

    41fc68c Register a bounce mail rejected by zoho.jp as a private sample (lhost/postfix-1337)

--- fact/48-lhost-postfix_test.go
+++ fact/48-lhost-postfix_test.go
@@ -432,6 +432,7 @@ func TestLhostPostfix(t *testing.T) {
 		{{"1334", 1, "4.3.0",   "",    "systemerror",     false, false, ""}},
 		{{"1335", 1, "4.5.0",   "451", "systemerror",     false, false, ""}},
 		{{"1336", 1, "5.0.0",   "",    "suspend",         false,  true, ""}},
+		{{"1337", 1, "5.4.1",   "",    "rejected",        false, false, ""}},
 	}; EngineTest(t, "Postfix", secretlist, false)
 }
 
