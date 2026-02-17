6a51f8275e17504534cac4bf8bbf2046d19db4a8
azumakuniyuki <azumakuniyuki@gmail.com>
2026-02-18 05:12:12 +0900

    Fix the commit 41fc68c, Hostname should be ".zoho.jp"

--- rhost/lib.go
+++ rhost/lib.go
@@ -34,7 +34,7 @@ var RhostClass = map[string][]string{
 	"Spectrum":    []string{"charter.net"},
 	"Tencent":     []string{".qq.com"},
 	"YahooInc":    []string{".yahoodns.net"},
-	"Zoho":        []string{".zoho.com", ".zoho.eu", "zoho.jp"},
+	"Zoho":        []string{".zoho.com", ".zoho.eu", ".zoho.jp"},
 }
 
 // Name returns the rhost class name.
