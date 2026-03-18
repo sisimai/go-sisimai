1f3f9131c3094cf77bc95f652d9071bf6f51c8ee
azumakuniyuki <azumakuniyuki@gmail.com>
2026-03-19 05:51:05 +0900

    #375 Remove substitution code for Toxic from fact/lib

--- fact/lib.go
+++ fact/lib.go
@@ -291,7 +291,6 @@ func Rise(email *string, origin string, args *siba.DecodingArgs) ([]siba.Fact, [
 		thing.Timestamp      = clock
 		thing.TimezoneOffset = clock.Format("+0900")
 		thing.Token          = token(as.Address, ar.Address, int(thing.Timestamp.Unix()))
-		thing.Toxic          = e.Toxic
 
 		ALIAS: for thing.Recipient.Address == thing.Alias {
 			// Look up the Envelope-To address from the Received: header in the original message
@@ -388,9 +387,6 @@ func Rise(email *string, origin string, args *siba.DecodingArgs) ([]siba.Fact, [
 
 		// Feedback-ID: 1.us-west-2.QHuyeCQrGtIIMGKQfVdUhP9hCQR2LglVOrRamBc+Prk=:AmazonSES
 		if len(rfc822data["feedback-id"]) > 0 { thing.FeedbackID = rfc822data["feedback-id"][0] }
-
-		// EXPERIMENTAL:
-		if thing.Toxic == 0 { thing.Toxic = thing.IsToxic() }
 
 		// Convert the value of Reason to the lower-cased name such as "mailboxfull".
 		thing.Reason = strings.ToLower(thing.Reason)
