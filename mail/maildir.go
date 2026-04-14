4e3b867bb47504169a7da6484681bb3ae6e0464d
azumakuniyuki <azumakuniyuki@gmail.com>
2026-04-15 05:54:14 +0900

    #392 readMaildir() returns []byte

--- mail/maildir.go
+++ mail/maildir.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2020,2024-2025 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2020,2024-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 //                  _ _    __               _ _     _ _      
 //  _ __ ___   __ _(_) |  / / __ ___   __ _(_) | __| (_)_ __ 
@@ -25,7 +25,7 @@ func(ee *EmailEntity) listMaildir() (int, error) {
 	direntries, nyaan := ee.handle.Readdir(0); if nyaan != nil { return 0, nyaan }
 	for _, e := range direntries {
 		// Read each email file in the Maildir/
-		if e.IsDir() == false || e.Size() > 0 { ee.payload = append(ee.payload, e.Name()) }
+		if e.IsDir() == false || e.Size() > 0 { ee.payload = append(ee.payload, []byte(e.Name())) }
 	}
 	nyaan = ee.handle.Close(); ee.handle = nil
 	return len(ee.payload), nyaan
@@ -33,22 +33,22 @@ func(ee *EmailEntity) listMaildir() (int, error) {
 
 // readMaildir is an email reader in the Maildir/, works like a iterator.
 //   Returns:
-//     - (*string): Contents of each email file in the Maildir/ one by one.
-//     - (error):   Occurred error.
-func(ee *EmailEntity) readMaildir() (*string, error) {
+//     - ([]byte): Contents of each email file in the Maildir/ one by one.
+//     - (error):  Occurred error.
+func(ee *EmailEntity) readMaildir() ([]byte, error) {
 	if ee.Size == 0         { return nil, fmt.Errorf("there is no email file in %s", ee.Dir) }
 	if ee.Size <= ee.offset { return nil, io.EOF }
 
 	for {
 		// Try to read the email file
-		ee.File = ee.payload[ee.offset]; ee.offset++
+		ee.File = string(ee.payload[ee.offset]); ee.offset++
 		ee.Path = filepath.Join(ee.Dir, ee.File)
-		b, nyaan := os.ReadFile(ee.Path); if nyaan != nil || len(b) == 0 {
+		by, nyaan := os.ReadFile(ee.Path); if nyaan != nil || len(by) == 0 {
 			// Failed to read the email file or the email file is empty
 			if ee.offset >= ee.Size { return nil, io.EOF }
 			continue
 		}
-		cv := string(b); return &cv, nil
+		return by, nil
 	}
 }
 
