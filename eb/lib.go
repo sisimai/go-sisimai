cdfaab0eb7176b71dce9bdd88be3acd6bd3757fc
azumakuniyuki <azumakuniyuki@gmail.com>
2026-06-28 07:55:41 +0900

    #423 Define the max depth of MIME parts

--- eb/lib.go
+++ eb/lib.go
@@ -8,6 +8,11 @@
 
 // Package "eb" provides constants for the email bounce.
 package eb
+
+// Max depth of MIME parts
+// - sendmail-8.18.1/sendmail/conf.h:#define MAXMIMENESTING	20	/* max MIME multipart nesting */
+// - postfix-3.7.2/proto/postconf.proto:%PARAM mime_nesting_limit 100
+const XdMIME = 100
 const XeBYTE = 2 * 1024 * 1024 * 1024 // 2GB: The maximum bytes of the email size in mail/lib.go
 const GeFrom = "MAILER-DAEMON Fri Feb  2 18:30:22 2018"
 var FeRFC822 = []string{"Content-Type: message/rfc822", "Content-Type: text/rfc822-headers"}
