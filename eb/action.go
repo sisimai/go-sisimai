589029f3533a620efd49d64b378db285cd51daa7
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-16 12:41:52 +0900

    #428 Replace eb.AeTRAN with eb.AePASS

--- eb/action.go
+++ eb/action.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 azumakuniyuki and sisimai development team, All rights reserved.
+// Copyright (C) 2025-2026 azumakuniyuki and sisimai development team, All rights reserved.
 // This software is distributed under The BSD 2-Clause License.
 //       _       ___        _   _             
 //   ___| |__   / / \   ___| |_(_) ___  _ __  
@@ -47,7 +47,7 @@ const (
 	AeFAIL = "failed"
 	AeSTAY = "delayed"
 	AeSENT = "delivered"
-	AeTRAN = "relayed"
+	AePASS = "relayed"
 	AeEXPN = "expanded"
 )
 
